1c77b5f49d803ba9a73d350de1bf53e777842ca1
Jon Skeet <skeet@pobox.com>
2009-11-08 20:28:00 +0000

    First commit - skeleton of what the ported names might look like, copyright notices etc.

---
+++ src/NodaTime/DateTimeConstants.cs
@@ -0,0 +1,30 @@
+﻿#region Copyright and license information
+// Copyright 2001-2009 Stephen Colebourne
+// Copyright 2009 Jon Skeet
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+namespace NodaTime
+{
+    /// <summary>
+    /// Original name: DateTimeConstants.
+    /// 
+    /// I'm not sure that everything in DateTimeConstants
+    /// really belongs in one file, and it should perhaps be named to reflect its ISO background
+    /// as well. We should consider an Iso8601Month enum etc. Possibly start
+    /// with everything in here, then refactor it out when it's all working.
+    /// </summary>
+    public static class DateTimeConstants
+    {
+    }
+}
---
+++ src/NodaTime/Fields/readme.txt
@@ -0,0 +1,2 @@
+﻿I believe we should be able to use largely the same names here as in Java, so I haven't added the classes yet.
+We can do so when the implementation forces us to, basically.
