ed7d822cba73a749ded3f1f29bcfef51f97629ac
Martinho Fernandes <martinho.fernandes@gmail.com>
2009-11-10 10:42:37 +0000

    Changed set of test times to the one from TestInstant_Basics.java

--- src/NodaTime.Test/InstantTest.cs
+++ src/NodaTime.Test/InstantTest.cs
@@ -38,18 +38,19 @@ namespace NodaTime.Test
                                        365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 +
                                        366 + 365 + 365;
 
-        // 1970-06-09
+        // 2002-06-09
         private const long TestTimeNow =
-            (31L + 28L + 31L + 30L + 31L + 9L - 1L) * DateTimeConstants.MillisecondsPerDay;
-        // 1970-04-05
+            (Y2002Days + 31L + 28L + 31L + 30L + 31L + 9L - 1L) * DateTimeConstants.MillisecondsPerDay;
+
+        // 2002-04-05
         private const long TestTime1 =
-            (31L + 28L + 31L + 6L - 1L) * DateTimeConstants.MillisecondsPerDay
+            (Y2002Days + 31L + 28L + 31L + 5L - 1L) * DateTimeConstants.MillisecondsPerDay
             + 12L * DateTimeConstants.MillisecondsPerHour
             + 24L * DateTimeConstants.MillisecondsPerMinute;
 
-        // 1971-05-06
+        // 2003-05-06
         private const long TestTime2 =
-            (365L + 31L + 28L + 31L + 30L + 7L - 1L) * DateTimeConstants.MillisecondsPerDay
+            (Y2003Days + 31L + 28L + 31L + 30L + 6L - 1L) * DateTimeConstants.MillisecondsPerDay
             + 14L * DateTimeConstants.MillisecondsPerHour
             + 28L * DateTimeConstants.MillisecondsPerMinute;
     }
