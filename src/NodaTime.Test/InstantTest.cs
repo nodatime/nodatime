32d23cf72604f95be97f4091ce68bfacfc691383
Martinho Fernandes <martinho.fernandes@gmail.com>
2009-11-10 10:38:22 +0000

    Added missing copyright notices.

--- src/NodaTime.Test/InstantTest.cs
+++ src/NodaTime.Test/InstantTest.cs
@@ -1,7 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿#region Copyright and license information
+
+// // Copyright 2001-2009 Stephen Colebourne
+// // Copyright 2009 Jon Skeet
+// // 
+// // Licensed under the Apache License, Version 2.0 (the "License");
+// // you may not use this file except in compliance with the License.
+// // You may obtain a copy of the License at
+// //
+// //     http://www.apache.org/licenses/LICENSE-2.0
+// //
+// // Unless required by applicable law or agreed to in writing, software
+// // distributed under the License is distributed on an "AS IS" BASIS,
+// // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// // See the License for the specific language governing permissions and
+// // limitations under the License.
+
+#endregion
+
 using NUnit.Framework;
 
 namespace NodaTime.Test
@@ -15,9 +30,9 @@ namespace NodaTime.Test
         private static readonly DateTimeZone London = DateTimeZone.ForID("Europe/London");
 
         private const long Y2002Days = 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 +
-                               366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 +
-                               365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 +
-                               366 + 365;
+                                       366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 +
+                                       365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 +
+                                       366 + 365;
         private const long Y2003Days = 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 +
                                        366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 +
                                        365 + 365 + 366 + 365 + 365 + 365 + 366 + 365 + 365 + 365 +
