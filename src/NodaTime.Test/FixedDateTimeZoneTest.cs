1ef26839e40ea81a50afbd2db90821db8fbb6859
Jon Skeet <skeet@pobox.com>
2009-11-18 17:27:14 +0000

    FixedDateTimeZone, which will always return the same offset.

---
+++ src/NodaTime.Test/FixedDateTimeZoneTest.cs
@@ -0,0 +1,41 @@
+﻿using NodaTime.TimeZones;
+using NUnit.Framework;
+
+namespace NodaTime.Test
+{
+    [TestFixture]
+    public class FixedDateTimeZoneTest
+    {
+        [Test]
+        public void SimpleProperties_ReturnValuesFromConstructor()
+        {
+            Duration offset = new Duration(1000);
+            FixedDateTimeZone zone = new FixedDateTimeZone("test", offset);
+            Assert.AreEqual("test", zone.Id);
+            // TODO: Use a real LocalDateTime when we've implemented it!
+            Assert.AreEqual(offset, zone.GetOffsetFromLocal(new LocalDateTime()));
+            Assert.AreEqual(offset, zone.GetOffsetFromUtc(Instant.UnixEpoch));
+        }
+
+        [Test]
+        public void IsFixed_ReturnsTrue()
+        {
+            FixedDateTimeZone zone = new FixedDateTimeZone("test", new Duration(1000));
+            Assert.IsTrue(zone.IsFixed);
+        }
+
+        [Test]
+        public void NextTransition_ReturnsNull()
+        {
+            FixedDateTimeZone zone = new FixedDateTimeZone("test", new Duration(1000));
+            Assert.IsNull(zone.NextTransition(Instant.UnixEpoch));
+        }
+
+        [Test]
+        public void PreviousTransition_ReturnsNull()
+        {
+            FixedDateTimeZone zone = new FixedDateTimeZone("test", new Duration(1000));
+            Assert.IsNull(zone.PreviousTransition(Instant.UnixEpoch));
+        }
+    }
+}
