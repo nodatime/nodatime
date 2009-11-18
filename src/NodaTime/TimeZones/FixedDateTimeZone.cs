226a6932689e85f4e5a3023d23dc37eae3372273
Jon Skeet <skeet@pobox.com>
2009-11-18 17:27:14 +0000

    FixedDateTimeZone, which will always return the same offset.

---
+++ src/NodaTime/TimeZones/FixedDateTimeZone.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NodaTime.TimeZones
+{
+    /// <summary>
+    /// Basic <see cref="IDateTimeZone" /> implementation that has a fixed name key and offset.
+    /// </summary>
+    /// <remarks>
+    /// This type is thread-safe and immutable.
+    /// </remarks>
+    public sealed class FixedDateTimeZone : IDateTimeZone
+    {
+        private readonly Duration offset;
+        private readonly string id;
+
+        /// <summary>
+        /// Creates a new fixed time zone.
+        /// </summary>
+        /// <param name="id">The ID of the time zone.</param>
+        /// <param name="standardOffset">The offset from UTC. A positive duration indicates that the local time is later than UTC.</param>
+        public FixedDateTimeZone(string id, Duration offset)
+        {
+            this.id = id;
+            this.offset = offset;
+        }
+
+        public Instant? NextTransition(Instant instant)
+        {
+            return null;
+        }
+
+        public Instant? PreviousTransition(Instant instant)
+        {
+            return null;
+        }
+
+        public Duration GetOffsetFromUtc(Instant instant)
+        {
+            return offset;
+        }
+
+        public Duration GetOffsetFromLocal(LocalDateTime localTime)
+        {
+            return offset;
+        }
+
+        public string Id
+        {
+            get { return id; }
+        }
+
+        public bool IsFixed
+        {
+            get { return true; }
+        }
+    }
+}
