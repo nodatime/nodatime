bc57a1a75f540959ab7754dcc5e99a9e5fae706d
Jon Skeet <skeet@pobox.com>
2009-11-20 06:23:47 +0000

    Stubs preparing for chronology work.

--- src/NodaTime/LocalDateTime.cs
+++ src/NodaTime/LocalDateTime.cs
@@ -15,6 +15,8 @@
 // limitations under the License.
 #endregion
 
+using NodaTime.Calendars;
+
 namespace NodaTime
 {
     /// <summary>
@@ -31,28 +33,90 @@ namespace NodaTime
     /// </para>
     /// </summary>
     public struct LocalDateTime
-    {/*
+    {
+        private readonly LocalInstant localInstant;
+        private ICalendarSystem calendar;
+
+        public LocalDateTime(LocalInstant localInstant)
+            : this(localInstant, CalendarSystems.Iso)
+        {
+        }
+
+        public LocalDateTime(LocalInstant localInstant, ICalendarSystem calendar)
+        {
+            this.localInstant = localInstant;
+            this.calendar = calendar;
+        }
+
         public LocalDateTime(int year, int month, int day,
-                             int minute, int hour, int second,
+                             int hour, int minute)
+            : this(year, month, day, hour, minute, 0, 0, 0, CalendarSystems.Iso)
+        {
+        }
+
+        public LocalDateTime(int year, int month, int day,
+                             int hour, int minute, ICalendarSystem calendar)
+            : this(year, month, day, hour, minute, 0, 0, 0, calendar)
+        {
+        }
+
+        public LocalDateTime(int year, int month, int day,
+                             int hour, int minute, int second)
+            : this(year, month, day, hour, minute, second, 0, 0, CalendarSystems.Iso)
+        {
+        }
+
+        public LocalDateTime(int year, int month, int day,
+                             int hour, int minute, int second,
                              ICalendarSystem calendar)
+            : this(year, month, day, hour, minute, second, 0, 0, calendar)
         {
         }
 
         public LocalDateTime(int year, int month, int day,
-                             int minute, int hour, int second, int millisecond,
+                             int hour, int minute, int second, int millisecond)
+            : this(year, month, day, hour, minute, second, millisecond, 0, CalendarSystems.Iso)
+        {
+        }
+
+        public LocalDateTime(int year, int month, int day,
+                             int hour, int minute, int second, int millisecond,
                              ICalendarSystem calendar)
+            : this(year, month, day, hour, minute, second, millisecond, 0, calendar)
+        {            
+        }
+
+        public LocalDateTime(int year, int month, int day,
+                             int hour, int minute, int second, int millisecond,
+                             int tickWithinMillisecond)
+            : this(year, month, day, hour, minute, second, millisecond, tickWithinMillisecond, CalendarSystems.Iso)
         {
         }
 
         public LocalDateTime(int year, int month, int day,
-                             int minute, int hour, int second)
+                             int hour, int minute, int second, int millisecond,
+                             int tickWithinMillisecond, ICalendarSystem calendar)
         {
+            localInstant = calendar.GetLocalInstant(year, month, day, hour, minute, second, millisecond, tickWithinMillisecond);
+            this.calendar = calendar;
         }
 
-        public LocalDateTime(int year, int month, int day,
-                             int minute, int hour, int second, int millisecond)
-        {
-        }
-        */
+        public int Era { get { return calendar.DateTimeFields[DateTimeFieldType.Era].GetValue(localInstant); } }
+        public int CenturyOfEra { get { return calendar.DateTimeFields[DateTimeFieldType.CenturyOfEra].GetValue(localInstant); } }
+        public int Year { get { return calendar.DateTimeFields[DateTimeFieldType.Year].GetValue(localInstant); } }
+        public int YearOfCentury { get { return calendar.DateTimeFields[DateTimeFieldType.YearOfCentury].GetValue(localInstant); } }
+        public int YearOfEra { get { return calendar.DateTimeFields[DateTimeFieldType.YearOfEra].GetValue(localInstant); } }
+        public int WeekYear { get { return calendar.DateTimeFields[DateTimeFieldType.Weekyear].GetValue(localInstant); } }
+        public int MonthOfYear { get { return calendar.DateTimeFields[DateTimeFieldType.MonthOfYear].GetValue(localInstant); } }
+        public int WeekOfWeekYear { get { return calendar.DateTimeFields[DateTimeFieldType.WeekOfWeekyear].GetValue(localInstant); } }
+        public int DayOfYear { get { return calendar.DateTimeFields[DateTimeFieldType.DayOfYear].GetValue(localInstant); } }
+        public int DayOfMonth { get { return calendar.DateTimeFields[DateTimeFieldType.DayOfMonth].GetValue(localInstant); } }
+        public int DayOfWeek { get { return calendar.DateTimeFields[DateTimeFieldType.DayOfWeek].GetValue(localInstant); } }
+        public int HourOfDay { get { return calendar.DateTimeFields[DateTimeFieldType.HourOfDay].GetValue(localInstant); } }
+        public int MinuteOfHour { get { return calendar.DateTimeFields[DateTimeFieldType.MinuteOfHour].GetValue(localInstant); } }
+        public int SecondOfMinute { get { return calendar.DateTimeFields[DateTimeFieldType.SecondOfMinute].GetValue(localInstant); } }
+        public int MillisecondsOfSecond { get { return calendar.DateTimeFields[DateTimeFieldType.MillisecondOfSecond].GetValue(localInstant); } }
+        public int TicksOfMillicond { get { return calendar.DateTimeFields[DateTimeFieldType.TickOfMillisecond].GetValue(localInstant); } }
+        public int TicksOfDay { get { return calendar.DateTimeFields[DateTimeFieldType.TickOfDay].GetValue(localInstant); } }
     }
 }
---
+++ src/NodaTime/LocalInstant.cs
@@ -0,0 +1,25 @@
+﻿namespace NodaTime
+{
+    /// <summary>
+    /// Represents a local date and time without reference to a calendar system,
+    /// as the number of ticks since the Unix epoch which would represent that time
+    /// of the same date in UTC. You cannot perform arithmetic directly on a local
+    /// instant, as the result would depend on the time zone.
+    /// </summary>
+    public struct LocalInstant
+    {
+        public static readonly LocalInstant LocalUnixEpoch = new LocalInstant(0);
+
+        private readonly long ticks;
+
+        /// <summary>
+        /// Ticks since the Unix epoch.
+        /// </summary>
+        public long Ticks { get { return ticks; } }
+
+        public LocalInstant(long ticks)
+        {
+            this.ticks = ticks;
+        }
+    }
+}
