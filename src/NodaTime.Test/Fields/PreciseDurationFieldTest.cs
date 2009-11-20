f861cfc2e4c569c4fb0240893ce381aa6be597f4
Jon Skeet <skeet@pobox.com>
2009-11-20 23:36:14 +0000

    Added PreciseDurationField and tests.

---
+++ src/NodaTime.Test/Fields/PreciseDurationFieldTest.cs
@@ -0,0 +1,203 @@
+﻿#region Copyright and license information
+// Copyright 2001-2009 Stephen Colebourne
+// Copyright 2009 Jon Skeet
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System;
+using NodaTime.Fields;
+using NUnit.Framework;
+namespace NodaTime.Test.Fields
+{
+    [TestFixture]
+    public class PreciseDurationFieldTest
+    {
+        private PreciseDurationField field;
+
+        [SetUp]
+        public void SetUp()
+        {
+            field = new PreciseDurationField(DurationFieldType.Milliseconds, DateTimeConstants.TicksPerMillisecond);
+        }
+
+        [Test]
+        public void Constructor_WithInvalidType_ThrowsArgumentOutOfRangeException()
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => new PreciseDurationField((DurationFieldType)(-1), 10));
+        }
+
+        [Test]
+        public void FieldType()
+        {
+            Assert.AreEqual(DurationFieldType.Milliseconds, field.FieldType);
+        }
+
+        [Test]
+        public void IsPrecise_ReturnsTrue()
+        {
+            Assert.IsTrue(field.IsPrecise);
+        }
+
+        [Test]
+        public void UnitTicks()
+        {
+            Assert.AreEqual(DateTimeConstants.TicksPerMillisecond, field.UnitTicks);
+        }
+
+        [Test]
+        public void GetValue()
+        {
+            Assert.AreEqual(0, field.GetValue(new Duration(0L)));
+            Assert.AreEqual(12345, field.GetValue(new Duration(123456789L)));
+            Assert.AreEqual(-1, field.GetValue(new Duration(-12345L)));
+            Assert.AreEqual(int.MaxValue, field.GetValue(new Duration(int.MaxValue * 10000L + 9999L)));            
+        }
+
+        [Test]
+        public void GetValue_ThrowsOnOverflow()
+        {
+            Assert.Throws<OverflowException>(() => field.GetValue(new Duration(int.MaxValue * 10000L + 10000L)));
+        }
+
+        [Test]
+        public void GetLongValue()
+        {
+            Assert.AreEqual(0, field.GetLongValue(new Duration(0L)));
+            Assert.AreEqual(12345, field.GetLongValue(new Duration(123456789L)));
+            Assert.AreEqual(-1, field.GetLongValue(new Duration(-12345L)));
+            Assert.AreEqual(int.MaxValue + 1L, field.GetLongValue(new Duration(int.MaxValue * 10000L + 10000L)));
+        }
+
+        [Test]
+        public void GetValue_WithLocalInstant()
+        {
+            LocalInstant when = new LocalInstant(987654321L);
+            Assert.AreEqual(0, field.GetValue(new Duration(0L), when));
+            Assert.AreEqual(12345, field.GetValue(new Duration(123456789L), when));
+            Assert.AreEqual(-1, field.GetValue(new Duration(-12345L), when));
+        }
+
+        [Test]
+        public void GetValue_WithLocalInstant_ThrowsOnOverflow()
+        {
+            LocalInstant when = new LocalInstant(987654321L);
+            Assert.Throws<OverflowException>(() => field.GetValue(new Duration(int.MaxValue * 10000L + 10000L), when));
+        }
+
+        [Test]
+        public void GetLongValue_WithLocalInstant()
+        {
+            LocalInstant when = new LocalInstant(987654321L);
+            Assert.AreEqual(0, field.GetLongValue(new Duration(0L), when));
+            Assert.AreEqual(12345, field.GetLongValue(new Duration(123456789L), when));
+            Assert.AreEqual(-1, field.GetLongValue(new Duration(-12345L), when));
+            Assert.AreEqual(int.MaxValue + 1L, field.GetLongValue(new Duration(int.MaxValue * 10000L + 10000L), when));
+        }
+
+        [Test]
+        public void GetDuration()
+        {
+            Assert.AreEqual(0L, field.GetDuration(0).Ticks);
+            Assert.AreEqual(12340000L, field.GetDuration(1234).Ticks);
+            Assert.AreEqual(-12340000L, field.GetDuration(-1234).Ticks);
+            Assert.AreEqual(int.MaxValue * 10000L, field.GetDuration(int.MaxValue).Ticks);
+        }
+
+        [Test]
+        public void GetDuration_ThrowsOnOverflow()
+        {
+            Assert.Throws<OverflowException>(() => field.GetDuration(long.MaxValue));
+        }
+
+        [Test]
+        public void GetDuration_WithLocalInstant()
+        {
+            LocalInstant when = new LocalInstant(987654321L);
+            Assert.AreEqual(0L, field.GetDuration(0, when).Ticks);
+            Assert.AreEqual(12340000L, field.GetDuration(1234, when).Ticks);
+            Assert.AreEqual(-12340000L, field.GetDuration(-1234, when).Ticks);
+            Assert.AreEqual(int.MaxValue * 10000L, field.GetDuration(int.MaxValue, when).Ticks);
+        }
+
+        [Test]
+        public void GetDuration_WithLocalInstant_ThrowsOnOverflow()
+        {
+            LocalInstant when = new LocalInstant(987654321L);
+            Assert.Throws<OverflowException>(() => field.GetDuration(long.MaxValue, when));
+        }
+
+        [Test]
+        public void Add_WithInt32Value()
+        {
+            LocalInstant when = new LocalInstant(567L);
+            Assert.AreEqual(567L, field.Add(when, 0).Ticks);
+            Assert.AreEqual(567L + 12340000L, field.Add(when, 1234).Ticks);
+            Assert.AreEqual(567L - 12340000L, field.Add(when, -1234).Ticks);
+        }
+
+        [Test]
+        public void Add_WithInt32Value_ThrowsOnOverflow()
+        {
+            Assert.Throws<OverflowException>(() => field.Add(new LocalInstant(long.MaxValue), 1));
+        }
+
+        [Test]
+        public void Add_WithInt64Value()
+        {
+            LocalInstant when = new LocalInstant(567L);
+            Assert.AreEqual(567L, field.Add(when, 0L).Ticks);
+            Assert.AreEqual(567L + 12340000L, field.Add(when, 1234L).Ticks);
+            Assert.AreEqual(567L - 12340000L, field.Add(when, -1234L).Ticks);
+        }
+
+        [Test]
+        public void Add_WithInt64Value_ThrowsOnOverflow()
+        {
+            Assert.Throws<OverflowException>(() => field.Add(new LocalInstant(long.MaxValue), 1));
+            Assert.Throws<OverflowException>(() => field.Add(new LocalInstant(0), long.MaxValue));
+        }
+
+        [Test]
+        public void GetDifference()
+        {
+            Assert.AreEqual(0, field.GetDifference(new LocalInstant(1), new LocalInstant(0)));
+            Assert.AreEqual(567, field.GetDifference(new LocalInstant(5670000L), new LocalInstant(0)));
+            Assert.AreEqual(567 - 1234, field.GetDifference(new LocalInstant(5670000L), new LocalInstant(12340000L)));
+            Assert.AreEqual(567 + 1234, field.GetDifference(new LocalInstant(5670000L), new LocalInstant(-12340000L)));
+        }
+
+        [Test]
+        public void GetDifference_ThrowsOnOverflow()
+        {
+            Assert.Throws<OverflowException>(() => field.GetDifference(new LocalInstant(long.MaxValue), new LocalInstant(-1L)));
+            Assert.Throws<OverflowException>(() => field.GetDifference(new LocalInstant(int.MaxValue * 10000L), new LocalInstant(-10000L)));
+        }
+
+        [Test]
+        public void GetLongDifference()
+        {
+            Assert.AreEqual(0L, field.GetLongDifference(new LocalInstant(1), new LocalInstant(0)));
+            Assert.AreEqual(567L, field.GetLongDifference(new LocalInstant(5670000L), new LocalInstant(0)));
+            Assert.AreEqual(567L - 1234L, field.GetLongDifference(new LocalInstant(5670000L), new LocalInstant(12340000L)));
+            Assert.AreEqual(567L + 1234L, field.GetLongDifference(new LocalInstant(5670000L), new LocalInstant(-12340000L)));
+            Assert.AreEqual(int.MaxValue + 1L, field.GetLongDifference(new LocalInstant(int.MaxValue * 10000L), new LocalInstant(-10000L)));
+        }
+
+        [Test]
+        public void GetLongDifference_ThrowsOnOverflow()
+        {
+            Assert.Throws<OverflowException>(() => field.GetLongDifference(new LocalInstant(long.MaxValue), new LocalInstant(-1L)));
+        }
+    }
+}
---
+++ src/NodaTime/Fields/PreciseDurationField.cs
@@ -0,0 +1,71 @@
+﻿#region Copyright and license information
+// Copyright 2001-2009 Stephen Colebourne
+// Copyright 2009 Jon Skeet
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NodaTime.Fields
+{
+    /// <summary>
+    /// Duration field class representing a field with a fixed unit length.
+    /// </summary>
+    internal sealed class PreciseDurationField : DurationFieldBase
+    {
+        /// <summary>
+        /// The size of the unit, in ticks.
+        /// </summary>
+        private readonly long unitTicks;
+
+        internal PreciseDurationField(DurationFieldType type, long unitTicks) : base(type)
+        {
+            this.unitTicks = unitTicks;
+        }
+
+        /// <summary>
+        /// Always returns true.
+        /// </summary>
+        public override bool IsPrecise { get { return true; } }
+
+        public override long UnitTicks { get { return unitTicks; } }
+
+        public override long GetLongValue(Duration duration, LocalInstant localInstant)
+        {
+            return duration.Ticks / unitTicks;
+        }
+
+        public override Duration GetDuration(long value, LocalInstant localInstant)
+        {
+            return new Duration(checked(value * UnitTicks));
+        }
+
+        public override LocalInstant Add(LocalInstant localInstant, int value)
+        {
+            return new LocalInstant(checked(localInstant.Ticks + value * UnitTicks));
+        }
+
+        public override LocalInstant Add(LocalInstant localInstant, long value)
+        {
+            return new LocalInstant(checked(localInstant.Ticks + value * UnitTicks));
+        }
+
+        public override long GetLongDifference(LocalInstant minuendInstant, LocalInstant subtrahendInstant)
+        {
+            return checked((minuendInstant.Ticks - subtrahendInstant.Ticks) / UnitTicks);
+        }
+    }
+}
