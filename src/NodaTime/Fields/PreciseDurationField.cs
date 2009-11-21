2ab4daba9586aa8cf69a813fb772f0b23d9841cd
Jon Skeet <skeet@pobox.com>
2009-11-21 20:01:06 +0000

    Global "remove and sort usings" (and that's all)

--- src/NodaTime/Fields/PreciseDurationField.cs
+++ src/NodaTime/Fields/PreciseDurationField.cs
@@ -14,10 +14,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 #endregion
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace NodaTime.Fields
 {
