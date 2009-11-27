821da17d3ecdbd0b8cdc72df2844d83376560791
Jon Skeet <skeet@pobox.com>
2009-11-27 19:16:26 +0000

    Applied copyright headers everywhere. This has caused some whitespace only changes, but it's using the Resharper "clean up code" option so it should be stable.

--- src/NodaTime/Periods/Days.cs
+++ src/NodaTime/Periods/Days.cs
@@ -1,5 +1,4 @@
 #region Copyright and license information
-
 // Copyright 2001-2009 Stephen Colebourne
 // Copyright 2009 Jon Skeet
 // 
@@ -14,9 +13,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
 #endregion
-
 using System;
 
 using NodaTime.Fields;
