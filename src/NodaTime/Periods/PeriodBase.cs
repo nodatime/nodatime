b7b6112f5da68463fb4bba7d5abd97f6a4f67000
Dmitry Bullavin <Dmitry Bullavin@localhost>
2009-12-14 19:12:50 +0300

    PeriodType indexing support, NodaDefaults ported from DateTimeUtils, PeriodBase and Period implementation have been started

--- src/NodaTime/Periods/PeriodBase.cs
+++ src/NodaTime/Periods/PeriodBase.cs
@@ -14,7 +14,11 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 #endregion
+
+using System;
+using NodaTime.Calendars;
 using NodaTime.Fields;
+
 namespace NodaTime.Periods
 {
     /// <summary>
@@ -22,27 +26,142 @@ namespace NodaTime.Periods
     /// </summary>
     public abstract class PeriodBase : IPeriod
     {
+        private readonly PeriodType periodType;
+        private int[] fieldValues;
+
+
+        protected PeriodBase( int[] fieldValues, PeriodType periodType)
+        {
+            this.periodType = periodType;
+
+            this.fieldValues = fieldValues;
+        }
+
+        protected PeriodBase(int years, int months, int weeks, int days,
+                         int hours, int minutes, int seconds, int millis,
+                         PeriodType periodType)
+        {
+            this.periodType = NodaDefaults.CheckPeriodType(periodType);
+
+            SetPeriodInternal(years, months, weeks, days, hours, minutes, seconds, millis);
+        }
+
+        protected PeriodBase(Duration duration, ICalendarSystem calendar, PeriodType periodType)
+        {
+            this.periodType = NodaDefaults.CheckPeriodType(periodType);
+
+            calendar = NodaDefaults.CheckCalendarSystem(calendar);
+            this.fieldValues = calendar.GetPeriodValues(this, duration);
+        }
+
+        private void SetPeriodInternal(int years, int months, int weeks, int days,
+                                       int hours, int minutes, int seconds, int millis)
+        {
+            int[] newValues = new int[Size];
+            CheckAndUpdate(DurationFieldType.Years, newValues, years);
+            CheckAndUpdate(DurationFieldType.Months, newValues, months);
+            CheckAndUpdate(DurationFieldType.Weeks, newValues, weeks);
+            CheckAndUpdate(DurationFieldType.Days, newValues, days);
+            CheckAndUpdate(DurationFieldType.Hours, newValues, hours);
+            CheckAndUpdate(DurationFieldType.Minutes, newValues, minutes);
+            CheckAndUpdate(DurationFieldType.Seconds, newValues, seconds);
+            CheckAndUpdate(DurationFieldType.Milliseconds, newValues, millis);
+            fieldValues = newValues;
+        }
+
+        private void CheckAndUpdate(DurationFieldType type, int[] values, int newValue)
+        {
+            int index = PeriodType.IndexOf(type);
+            if (index == -1)
+            {
+                if (newValue != 0)
+                {
+                    throw new  ArgumentException(
+                        "Period does not support field '" + type + "'");
+                }
+            }
+            else
+            {
+                values[index] = newValue;
+            }
+        }
+
+        /// <summary>
+        /// Gets an array of the field types that this period supports.
+        /// </summary>
+        /// <remarks>
+        /// The fields are returned largest to smallest, for example Hours, Minutes, Seconds.
+        /// </remarks>
+        /// <returns>The fields supported in an array that may be altered, largest to smallest</returns>
+        public DurationFieldType[] GetFieldTypes()
+        {
+            DurationFieldType[] result = new DurationFieldType[Size];
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = GetFieldType(i);
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Gets an array of the value of each of the fields that this period supports.
+        /// </summary>
+        /// <remarks>
+        /// The fields are returned largest to smallest, for example Hours, Minutes, Seconds.
+        /// Each value corresponds to the same array index as <code>GetFieldTypes()</code>
+        /// </remarks>
+        /// <returns>The current values of each field in an array that may be altered, largest to smallest</returns>
+        public int[] GetValues()
+        {
+            int[] result = new int[Size];
+            for (int i = 0; i < result.Length; i++)
+            {
+                result[i] = GetValue(i);
+            }
+            return result;
+        }
+
         #region IPeriod Members
 
-        public abstract PeriodType PeriodType
+        public PeriodType PeriodType
         {
-            get;
+            get { return periodType; }
         }
 
-        public abstract int Size
+        public int Size
         {
-            get;
+            get { return periodType.Size; }
         }
 
-        public abstract DurationFieldType GetFieldType(int index);
+        public DurationFieldType GetFieldType(int index)
+        {
+            return periodType.GetFieldType(index);
+        }
 
-        public abstract int GetValue(int index);
+        public bool IsSupported(DurationFieldType field)
+        {
+            return PeriodType.IsSupported(field);
+        }
 
-        public abstract int Get(DurationFieldType field);
+        public int GetValue(int index)
+        {
+            return fieldValues[index];
+        }
 
-        public abstract bool IsSupported(DurationFieldType field);
+        public int Get(DurationFieldType field)
+        {
+            int index = PeriodType.IndexOf(field);
+            if (index == -1)
+            {
+                return 0;
+            }
+            return GetValue(index);
+        }
 
-        public abstract Period ToPeriod();
+        public Period ToPeriod()
+        {
+            throw new NotImplementedException();
+        }
 
         #endregion
 
