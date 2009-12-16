9a86c80302a7aef8bd23a3e0c8d86e11c6afb931
Dmitry Bullavin <Dmitry Bullavin@localhost>
2009-12-16 16:44:19 +0300

    PeriodType Equality support with tests

---
+++ src/NodaTime.Test/Periods/PeriodTypeTest.Equality.cs
@@ -0,0 +1,39 @@
+﻿#region Copyright and license information
+// Copyright 2001-2009 Stephen Colebourne
+// Copyright 2009 Jon Skeet
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using NodaTime.Periods;
+using NUnit.Framework;
+using NodaTime.Fields;
+
+namespace NodaTime.Test.Periods
+{
+    public partial class PeriodTypeTest
+    {
+        [Test]
+        public void Equals_StandartAndStandartNotTime()
+        {
+            TestHelper.TestEqualsClass(PeriodType.Standart, PeriodType.Standart, PeriodType.Time);
+        }
+
+        [Test]
+        public void Equals_YearMonthDayTimeAndStandartWithWeeksRemovedNotYears()
+        {
+            TestHelper.TestEqualsClass(PeriodType.YearMonthDayTime, PeriodType.Standart.WithWeeksRemoved(), PeriodType.Years);
+        }
+
+    }
+}
