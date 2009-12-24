3c824ad416b3c1cfe3d5c08eaff09ec41cf99375
Jon Skeet <skeet@pobox.com>
2009-12-24 19:49:58 +0000

    A couple of bits of refactoring.

--- src/NodaTime/Periods/Days.cs
+++ src/NodaTime/Periods/Days.cs
@@ -40,28 +40,17 @@ namespace NodaTime.Periods
         {
             switch (days)
             {
-                case 0:
-                    return Zero;
-                case 1:
-                    return One;
-                case 2:
-                    return Two;
-                case 3:
-                    return Three;
-                case 4:
-                    return Four;
-                case 5:
-                    return Five;
-                case 6:
-                    return Six;
-                case 7:
-                    return Seven;
-                case Int32.MinValue:
-                    return MinValue;
-                case Int32.MaxValue:
-                    return MaxValue;
-                default:
-                    return new Days(days);
+                case 0: return Zero;
+                case 1: return One;
+                case 2: return Two;
+                case 3: return Three;
+                case 4: return Four;
+                case 5: return Five;
+                case 6: return Six;
+                case 7: return Seven;
+                case Int32.MinValue: return MinValue;
+                case Int32.MaxValue: return MaxValue;
+                default: return new Days(days);
             }
         }
 
