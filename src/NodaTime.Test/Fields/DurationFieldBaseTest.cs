be79e9a75add27e59391082e46db4dcb707728d6
Jon Skeet <skeet@pobox.com>
2010-02-21 20:22:57 +0000

    Update copyright headers for all files, so I don't need to remember to do them on an individual basis.

--- src/NodaTime.Test/Fields/DurationFieldBaseTest.cs
+++ src/NodaTime.Test/Fields/DurationFieldBaseTest.cs
@@ -1,4 +1,4 @@
-﻿#region Copyright and license information
+#region Copyright and license information
 // Copyright 2001-2009 Stephen Colebourne
 // Copyright 2009 Jon Skeet
 // 
--- src/ZoneInfoCompiler/winmap/WindowsMapperCompilerOptions.cs
+++ src/ZoneInfoCompiler/winmap/WindowsMapperCompilerOptions.cs
@@ -1,6 +1,6 @@
-﻿#region Copyright and license information
+#region Copyright and license information
 // Copyright 2001-2009 Stephen Colebourne
-// Copyright 2009 Jon Skeet
+// Copyright 2009-2010 Jon Skeet
 // 
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -14,7 +14,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 #endregion
-
 using CommandLine;
 using CommandLine.Text;
 
