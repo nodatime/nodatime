d1bd62f4368025b1749830471ffb1cdf492d23e6
Jon Skeet <skeet@pobox.com>
2010-02-21 18:16:17 +0000

    Optimise IsoCalendarSystem to the hilt for years 1900-2100, and add more ICalendarSystem members to allow for optimisations based on the user not providing seconds, milliseconds or ticks.

--- src/NodaTime/Calendars/IsoCalendarSystem.cs
+++ src/NodaTime/Calendars/IsoCalendarSystem.cs
@@ -39,7 +39,28 @@ namespace NodaTime.Calendars
     {
         public static readonly IsoCalendarSystem Instance = new IsoCalendarSystem(GregorianCalendarSystem.Default);
 
-        private IsoCalendarSystem(ICalendarSystem baseSystem) : base(baseSystem)
+        // We precompute useful values for each month between these years, as we anticipate most
+        // dates will be in this range.
+        private const int FirstOptimizedYear = 1900;
+        private const int LastOptimizedYear = 2100;
+        private static readonly long[] MonthStartTicks = new long[(LastOptimizedYear + 1 - FirstOptimizedYear) * 12 + 1];
+        private static readonly int[] MonthLengths = new int[(LastOptimizedYear + 1 - FirstOptimizedYear) * 12 + 1];
+
+        static IsoCalendarSystem()
+        {
+            for (int year = FirstOptimizedYear; year <= LastOptimizedYear; year++)
+            {
+                for (int month = 1; month <= 12; month++)
+                {
+                    int yearMonthIndex = (year - FirstOptimizedYear) * 12 + month;
+                    MonthStartTicks[yearMonthIndex] = GregorianCalendarSystem.Default.GetYearMonthTicks(year, month);
+                    MonthLengths[yearMonthIndex] = GregorianCalendarSystem.Default.GetDaysInYearMonth(year, month);
+                }
+            }
+        }
+
+        private IsoCalendarSystem(ICalendarSystem baseSystem)
+            : base(baseSystem)
         {
         }
 
@@ -56,6 +77,63 @@ namespace NodaTime.Calendars
             fields.YearOfCentury = new RemainderDateTimeField(centuryOfEra, DateTimeFieldType.YearOfCentury);
             fields.WeekYearOfCentury = new RemainderDateTimeField(centuryOfEra, DateTimeFieldType.WeekYearOfCentury);
             fields.Centuries = centuryOfEra.DurationField;
+        }
+
+        public override LocalInstant GetLocalInstant(int year, int monthOfYear, int dayOfMonth, int hourOfDay, int minuteOfHour, int secondOfMinute, int millisecondOfSecond, int tickOfMillisecond)
+        {
+            int yearMonthIndex = (year - FirstOptimizedYear) * 12 + monthOfYear;
+            if (year < FirstOptimizedYear || year > LastOptimizedYear - 1 ||
+                monthOfYear < 1 || monthOfYear > 12 ||
+                dayOfMonth < 1 || dayOfMonth > MonthLengths[yearMonthIndex] ||
+                hourOfDay < 0 || hourOfDay > 23 ||
+                minuteOfHour < 0 || minuteOfHour > 59 ||
+                secondOfMinute < 0 || secondOfMinute > 59 ||
+                millisecondOfSecond < 0 || millisecondOfSecond > 999 ||
+                tickOfMillisecond < 0 || tickOfMillisecond > NodaConstants.TicksPerMillisecond - 1)
+            {
+                // It may still be okay - let's take the long way to find out
+                return base.GetLocalInstant(year, monthOfYear, dayOfMonth, hourOfDay, minuteOfHour, secondOfMinute, millisecondOfSecond, tickOfMillisecond);
+            }
+            return new LocalInstant(MonthStartTicks[yearMonthIndex] + 
+                (dayOfMonth - 1) * NodaConstants.TicksPerDay +
+                hourOfDay * NodaConstants.TicksPerHour + minuteOfHour * NodaConstants.TicksPerMinute +
+                secondOfMinute * NodaConstants.TicksPerSecond + millisecondOfSecond * NodaConstants.TicksPerMillisecond + tickOfMillisecond);
+        }
+
+        public override LocalInstant GetLocalInstant(int year, int monthOfYear, int dayOfMonth, int hourOfDay, int minuteOfHour, int secondOfMinute)
+        {
+            int yearMonthIndex = (year - FirstOptimizedYear) * 12 + monthOfYear;
+            if (year < FirstOptimizedYear || year > LastOptimizedYear - 1 ||
+                monthOfYear < 1 || monthOfYear > 12 ||
+                dayOfMonth < 1 || dayOfMonth > MonthLengths[yearMonthIndex] ||
+                hourOfDay < 0 || hourOfDay > 23 ||
+                minuteOfHour < 0 || minuteOfHour > 59 ||
+                secondOfMinute < 0 || secondOfMinute > 59)
+            {
+                // It may still be okay - let's take the long way to find out
+                return base.GetLocalInstant(year, monthOfYear, dayOfMonth, hourOfDay, minuteOfHour, secondOfMinute);
+            }
+            return new LocalInstant(MonthStartTicks[yearMonthIndex] +
+                (dayOfMonth - 1) * NodaConstants.TicksPerDay +
+                hourOfDay * NodaConstants.TicksPerHour + minuteOfHour * NodaConstants.TicksPerMinute +
+                secondOfMinute * NodaConstants.TicksPerSecond);
+        }
+
+        public override LocalInstant GetLocalInstant(int year, int monthOfYear, int dayOfMonth, int hourOfDay, int minuteOfHour)
+        {
+            int yearMonthIndex = (year - FirstOptimizedYear) * 12 + monthOfYear;
+            if (year < FirstOptimizedYear || year > LastOptimizedYear - 1 ||
+                monthOfYear < 1 || monthOfYear > 12 ||
+                dayOfMonth < 1 || dayOfMonth > MonthLengths[yearMonthIndex] ||
+                hourOfDay < 0 || hourOfDay > 23 ||
+                minuteOfHour < 0 || minuteOfHour > 59)
+            {
+                // It may still be okay - let's take the long way to find out
+                return base.GetLocalInstant(year, monthOfYear, dayOfMonth, hourOfDay, minuteOfHour);
+            }
+            return new LocalInstant(MonthStartTicks[yearMonthIndex] +
+                (dayOfMonth - 1) * NodaConstants.TicksPerDay +
+                hourOfDay * NodaConstants.TicksPerHour + minuteOfHour * NodaConstants.TicksPerMinute);
         }
 
         // TODO: Try overriding the GetLocalInstant methods to micro-optimise them (they will be called for almost every ZonedDateTime/LocalDateTime)
