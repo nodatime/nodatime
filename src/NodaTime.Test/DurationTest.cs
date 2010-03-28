41ca26bacd33e71ea66c7a00c211702eed13b6cf
Martinho Fernandes <martinho.fernandes@gmail.com>
2010-03-28 04:08:09 +0000

    Normalized Duration equality and comparison tests to use TestHelper.

--- src/NodaTime.Test/DurationTest.cs
+++ src/NodaTime.Test/DurationTest.cs
@@ -22,7 +22,6 @@ namespace NodaTime.Test
     [TestFixture]
     public partial class DurationTest
     {
-        readonly Duration onePrime = new Duration(1L);
         readonly Duration threeMillion = new Duration(3000000L);
         readonly Duration negativeFiftyMillion = new Duration(-50000000L);
     }
