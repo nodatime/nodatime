6d781daf39675a09d00f16741829513583250d2d
Dmitry Bulavin <dmitry.bulavin@gmail.com>
2010-04-02 15:16:07 +0400

    DateTimeFormats: new code + unit-tests

---
+++ src/NodaTime.Test/Format/DateTimeFormats.cs
@@ -0,0 +1,64 @@
+﻿#region Copyright and license information
+// Copyright 2001-2009 Stephen Colebourne
+// Copyright 2009-2010 Jon Skeet
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System;
+using NodaTime.Format;
+using NodaTime.TimeZones;
+using NUnit.Framework;
+using System.Globalization;
+
+namespace NodaTime.Test.Format
+{
+    [TestFixture]
+    public partial class DateTimeFormatsTest
+    {
+        #region Zones
+
+        private static IDateTimeZone UTC = DateTimeZones.Utc;
+        private static IDateTimeZone London = DateTimeZones.ForId("Europe/London");
+        private static IDateTimeZone Paris = DateTimeZones.ForId("Europe/Paris");
+
+        #endregion
+
+        #region Cultures
+
+        private static IFormatProvider EnUs = CultureInfo.GetCultureInfo("en-US");          //English (United States) 
+        private static IFormatProvider PortuBrazil = CultureInfo.GetCultureInfo("pt-BR");   //Portuguese (Brazil) 
+        private static IFormatProvider CroCro = CultureInfo.GetCultureInfo("hr-HR");        //Croatian (Croatia) 
+
+        #endregion
+
+        object[] StandardPatternTestData =
+        {
+            new TestCaseData( 'd', (IFormatProvider)null, new ZonedDateTime(2004, 6, 9, 10, 20, 30, 40, UTC), new DateTimeOffset(2004, 6, 9, 10, 20, 30, 40, TimeSpan.Zero)),
+            new TestCaseData( 'd', EnUs, new ZonedDateTime(2004, 6, 9, 10, 20, 30, 40, UTC), new DateTimeOffset(2004, 6, 9, 10, 20, 30, 40, TimeSpan.Zero)),
+            new TestCaseData( 'd', PortuBrazil, new ZonedDateTime(2004, 6, 9, 10, 20, 30, 40, UTC), new DateTimeOffset(2004, 6, 9, 10, 20, 30, 40, TimeSpan.Zero)),
+            new TestCaseData( 'd', CroCro, new ZonedDateTime(2004, 6, 9, 10, 20, 30, 40, UTC), new DateTimeOffset(2004, 6, 9, 10, 20, 30, 40, TimeSpan.Zero)),
+        };
+
+        [Test]
+        [TestCaseSource("StandardPatternTestData")]
+        public void StandardPattern_OutputsTheSameStringAsBCL(char standardPattern, IFormatProvider provider, ZonedDateTime nDateTime, DateTimeOffset sDateTime)
+        {
+            var nodaOutput = DateTimeFormats.ForStandardPattern(standardPattern).WithProvider(provider).Print(nDateTime);
+            var bclOutput = sDateTime.ToString(standardPattern.ToString(), provider);
+
+            Assert.That(nodaOutput, Is.EqualTo(bclOutput));
+        }
+
+    }
+}
