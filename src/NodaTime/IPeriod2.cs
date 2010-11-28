dddb1eecf378efa82d2c23b8e2158cf577d45332
Jon Skeet <skeet@pobox.com>
2010-11-28 21:49:32 +0000

    The start of the new period interface.

---
+++ src/NodaTime/IPeriod2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NodaTime.Periods;
+
+namespace NodaTime
+{
+    // TODO: Consider whether this should derive from IEnumerable[DurationFieldValue] instead
+    // of having a property for it.
+
+    /// <summary>
+    /// Represents a period of time expressed in human chronological terms: hours, days,
+    /// weeks, months and so on.
+    /// </summary>
+    /// <remarks>
+    /// Periods operate on calendar-related types such as
+    /// <see cref="LocalDateTime" /> whereas <see cref="Duration"/> operates on instants
+    /// on the time line. <see cref="ZonedDateTime" /> includes both concepts, so both
+    /// durations and periods can be added to zoned date-time instances - although the
+    /// results may not be the same. For example, adding a two hour period to a ZonedDateTime
+    /// will always give a result has a local date-time which is two hours later, even if that
+    /// means that three hours would have to actually pass in experienced time to arrive at
+    /// that local date-time, due to changes in the UTC offset (e.g. for daylight savings).
+    /// </remarks>
+    public interface IPeriod2
+    {
+        IEnumerable<DurationFieldValue> FieldValues { get; }
+    }
+}
---
+++ src/NodaTime/Periods/DurationFieldValue.cs
@@ -0,0 +1,25 @@
+﻿using NodaTime.Fields;
+
+namespace NodaTime.Periods
+{
+    // TODO: Implement IEquatable[T] etc? I'm not currently sure there's much point.
+
+    /// <summary>
+    /// A simple combination of a <see cref="DurationFieldType" /> and a 64-bit integer value.
+    /// These are used when representing periods.
+    /// </summary>
+    public struct DurationFieldValue
+    {
+        private readonly DurationFieldType fieldType;
+        public DurationFieldType FieldType { get { return fieldType; } }
+
+        private readonly long value;
+        public long Value { get { return value; } }
+
+        public DurationFieldValue(DurationFieldType fieldType, long value)
+        {
+            this.fieldType = fieldType;
+            this.value = value;
+        }
+    }
+}
