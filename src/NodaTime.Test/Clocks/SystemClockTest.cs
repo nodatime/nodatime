5a26eddb9bab10ad9e7d81a87f2094500d0888fa
Jon Skeet <skeet@pobox.com>
2011-07-24 15:17:09 +0100

    Clock refactoring.

---
+++ src/NodaTime.Test/Clocks/SystemClockTest.cs
@@ -0,0 +1,43 @@
+﻿#region Copyright and license information
+// Copyright 2001-2009 Stephen Colebourne
+// Copyright 2009-2010 Jon Skeet
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using NUnit.Framework;
+using System;
+using NodaTime.Clocks;
+
+namespace NodaTime.Test.Clocks
+{
+    [TestFixture]
+    public class SystemClockTest
+    {
+        [Test]
+        public void SystemNow()
+        {
+            long frameworkNowTicks = DateTime.UtcNow.Ticks;
+            long nodaTicks = SystemClock.SystemNow.Ticks;
+            Assert.Less(Math.Abs(nodaTicks - frameworkNowTicks), Duration.FromSeconds(1).Ticks);
+        }
+
+        [Test]
+        public void InstanceNow()
+        {
+            long frameworkNowTicks = DateTime.UtcNow.Ticks;
+            long nodaTicks = SystemClock.Instance.Now.Ticks;
+            Assert.Less(Math.Abs(nodaTicks - frameworkNowTicks), Duration.FromSeconds(1).Ticks);
+        }
+    }
+}
