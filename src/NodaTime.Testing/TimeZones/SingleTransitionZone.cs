90b14fb98486bec0a86f72b9d9b02ed04d8c9676
Jon Skeet <skeet@pobox.com>
2011-07-25 19:09:36 +0100

    Add a new Testing assembly for classes which help with unit testing - either for us or other users.

--- src/NodaTime.Testing/TimeZones/SingleTransitionZone.cs
+++ src/NodaTime.Testing/TimeZones/SingleTransitionZone.cs
@@ -1,10 +1,24 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using NodaTime.TimeZones;
+﻿#region Copyright and license information
+// Copyright 2001-2009 Stephen Colebourne
+// Copyright 2009 Jon Skeet
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
 
-namespace NodaTime.Test.TimeZones
+using NodaTime.TimeZones;
+using System;
+
+namespace NodaTime.Testing.TimeZones
 {
     /// <summary>
     /// Time zone with a single transition between two offsets. This is
@@ -12,20 +26,20 @@ namespace NodaTime.Test.TimeZones
     /// tail zone, but it's simpler to construct and can be used to test
     /// PrecalculatedDateTimeZone.
     /// </summary>
-    internal class SingleTransitionZone : DateTimeZone
+    public class SingleTransitionZone : DateTimeZone
     {
         private readonly ZoneInterval earlyInterval;
-        internal ZoneInterval EarlyInterval { get { return earlyInterval; } }
+        public ZoneInterval EarlyInterval { get { return earlyInterval; } }
 
         private readonly ZoneInterval lateInterval;
-        internal ZoneInterval LateInterval { get { return lateInterval; } }
+        public ZoneInterval LateInterval { get { return lateInterval; } }
 
-        internal SingleTransitionZone(Instant transitionPoint, int offsetBeforeHours, int offsetAfterHours)
+        public SingleTransitionZone(Instant transitionPoint, int offsetBeforeHours, int offsetAfterHours)
             : this(transitionPoint, Offset.ForHours(offsetBeforeHours), Offset.ForHours(offsetAfterHours))
         {
         }
 
-        internal SingleTransitionZone(Instant transitionPoint, Offset offsetBefore, Offset offsetAfter)
+        public SingleTransitionZone(Instant transitionPoint, Offset offsetBefore, Offset offsetAfter)
             : base("Single", false, Offset.Min(offsetBefore, offsetAfter), Offset.Max(offsetBefore, offsetAfter))
         {
             earlyInterval = new ZoneInterval("Single-Early", Instant.MinValue, transitionPoint,
