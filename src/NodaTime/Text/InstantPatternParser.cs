67498e8449ed6d62cf5299f887179a2f097f7794
Jon Skeet <skeet@pobox.com>
2011-08-24 14:20:20 +0100

    Introduce an AbstractParsedPattern, and use our own code to compute fields for formatting.

--- src/NodaTime/Text/InstantPatternParser.cs
+++ src/NodaTime/Text/InstantPatternParser.cs
@@ -17,6 +17,7 @@
 
 using System;
 using System.Globalization;
+using NodaTime.Fields;
 using NodaTime.Globalization;
 using NodaTime.Text.Patterns;
 
@@ -25,6 +26,14 @@ namespace NodaTime.Text
     /// <summary>
     /// Pattern parsing support for <see cref="Instant" />.
     /// </summary>
+    /// <remarks>
+    /// Supported patterns:
+    /// <list type="bullet">
+    ///   <item><description>g: general; the UTC ISO-8601 instant in the style yyyy-MM-ddTHH:mm:ssZ</description></item>
+    ///   <item><description>n: numeric; the number of ticks since the epoch using thousands separators</description></item>
+    ///   <item><description>d: numeric; the number of ticks since the epoch without thousands separators</description></item>
+    /// </list>
+    /// </remarks>
     internal sealed class InstantPatternParser : IPatternParser<Instant>
     {
 
@@ -47,47 +56,43 @@ namespace NodaTime.Text
             switch (patternChar)
             {
                 case 'g':
-                    return PatternParseResult<Instant>.ForValue(new GeneralParsePattern(formatInfo, parseStyles));
+                    return PatternParseResult<Instant>.ForValue(new GeneralParsedPattern(formatInfo, parseStyles));
                 case 'n':
-                    return PatternParseResult<Instant>.ForValue(new NumberParsePattern(pattern, formatInfo, parseStyles, "N0"));
+                    return PatternParseResult<Instant>.ForValue(new NumberParsedPattern(formatInfo, parseStyles, pattern, "N0"));
                 case 'd':
-                    return PatternParseResult<Instant>.ForValue(new NumberParsePattern(pattern, formatInfo, parseStyles, "D"));
+                    return PatternParseResult<Instant>.ForValue(new NumberParsedPattern(formatInfo, parseStyles, pattern, "D"));
                 default:
                     return PatternParseResult<Instant>.UnknownStandardFormat(patternChar);
             }
         }
 
-        // TODO: Create an AbstractParsePattern which contains the ParseStyles and NodaFormatInfo, and performs the appropriate trimming.
-        private sealed class GeneralParsePattern : IParsedPattern<Instant>
+        private sealed class GeneralParsedPattern : AbstractParsedPattern<Instant>
         {
-            private readonly NodaFormatInfo formatInfo;
-            private readonly ParseStyles parseStyles;
+            private static readonly DateTimeField YearField;
+            private static readonly DateTimeField MonthOfYearField;
+            private static readonly DateTimeField DayOfMonthField;
+            private static readonly DateTimeField HourOfDayField;
+            private static readonly DateTimeField MinuteOfHourField;
+            private static readonly DateTimeField SecondOfMinuteField;
 
-            internal GeneralParsePattern(NodaFormatInfo formatInfo, ParseStyles parseStyles)
+            static GeneralParsedPattern()
             {
-                this.formatInfo = formatInfo;
-                this.parseStyles = parseStyles;
+                var isoFields = CalendarSystem.Iso.Fields;
+                YearField = isoFields.Year;
+                MonthOfYearField = isoFields.MonthOfYear;
+                DayOfMonthField = isoFields.DayOfMonth;
+                HourOfDayField = isoFields.HourOfDay;
+                MinuteOfHourField = isoFields.MinuteOfHour;
+                SecondOfMinuteField = isoFields.SecondOfMinute;
             }
 
-            public ParseResult<Instant> Parse(string value)
+            internal GeneralParsedPattern(NodaFormatInfo formatInfo, ParseStyles parseStyles)
+                : base(formatInfo, parseStyles)
             {
-                if (value == null)
-                {
-                    return ParseResult<Instant>.ArgumentNull("value");
-                }
-                if (value.Length == 0)
-                {
-                    return ParseResult<Instant>.ValueStringEmpty;
-                }
+            }
 
-                if ((parseStyles & ParseStyles.AllowLeadingWhite) != 0)
-                {
-                    value = value.TrimStart();
-                }
-                if ((parseStyles & ParseStyles.AllowTrailingWhite) != 0)
-                {
-                    value = value.TrimEnd();
-                }
+            protected override ParseResult<Instant> ParseImpl(string value)
+            {
                 if (value.Equals(Instant.BeginningOfTimeLabel, StringComparison.OrdinalIgnoreCase))
                 {
                     return ParseResult<Instant>.ForValue(Instant.MinValue);
@@ -99,7 +104,7 @@ namespace NodaTime.Text
 
                 DateTime result;
                 // TODO: When we've got our own parsers fully working, parse this as a LocalDateTime.
-                if (!DateTime.TryParseExact(value, "yyyy'-'MM'-'dd'T'HH':'mm':'ss'Z'", formatInfo.DateTimeFormat,
+                if (!DateTime.TryParseExact(value, "yyyy'-'MM'-'dd'T'HH':'mm':'ss'Z'", FormatInfo.DateTimeFormat,
                                             DateTimeStyles.AssumeUniversal | DateTimeStyles.AdjustToUniversal, out result))
                 {
                     return ParseResult<Instant>.CannotParseValue(value, "g");
@@ -107,7 +112,7 @@ namespace NodaTime.Text
                 return ParseResult<Instant>.ForValue(Instant.FromDateTimeUtc(result));
             }
 
-            public string Format(Instant value)
+            public override string Format(Instant value)
             {
                 if (value == Instant.MinValue)
                 {
@@ -117,60 +122,44 @@ namespace NodaTime.Text
                 {
                     return Instant.EndOfTimeLabel;
                 }
-                // TODO: Use LocalDateTime instead, in the ISO calendar? Benchmark...
-                DateTime utc = value.ToDateTimeUtc();
+                var localInstant = new LocalInstant(value.Ticks); // Effectively UTC...
                 return string.Format(CultureInfo.InvariantCulture,
                         "{0}-{1:00}-{2:00}T{3:00}:{4:00}:{5:00}Z",
-                        utc.Year, utc.Month, utc.Day, utc.Hour, utc.Minute, utc.Second);
+                        YearField.GetValue(localInstant),
+                        MonthOfYearField.GetValue(localInstant),
+                        DayOfMonthField.GetValue(localInstant),
+                        HourOfDayField.GetValue(localInstant),
+                        MinuteOfHourField.GetValue(localInstant),
+                        SecondOfMinuteField.GetValue(localInstant));
             }
         }
 
-        private sealed class NumberParsePattern : IParsedPattern<Instant>
+        private sealed class NumberParsedPattern : AbstractParsedPattern<Instant>
         {
             private const NumberStyles ParsingNumberStyles = NumberStyles.AllowLeadingSign | NumberStyles.AllowThousands;
-            private readonly NodaFormatInfo formatInfo;
             private readonly string pattern;
-            private readonly ParseStyles parseStyles;
             private readonly string systemFormatString;
 
-            internal NumberParsePattern(string pattern, NodaFormatInfo formatInfo, ParseStyles parseStyles, string systemFormatString)
+            internal NumberParsedPattern(NodaFormatInfo formatInfo, ParseStyles parseStyles, string pattern, string systemFormatString)
+                : base(formatInfo, parseStyles)
             {
                 this.pattern = pattern;
-                this.formatInfo = formatInfo;
-                this.parseStyles = parseStyles;
                 this.systemFormatString = systemFormatString;
             }
 
-            public ParseResult<Instant> Parse(string value)
-            {
-                if (value == null)
-                {
-                    return ParseResult<Instant>.ArgumentNull("value");
-                }
-                if (value.Length == 0)
-                {
-                    return ParseResult<Instant>.ValueStringEmpty;
-                }
-                if ((parseStyles & ParseStyles.AllowLeadingWhite) != 0)
-                {
-                    value = value.TrimStart();
-                }
-                if ((parseStyles & ParseStyles.AllowTrailingWhite) != 0)
-                {
-                    value = value.TrimEnd();
-                }
-                
+            protected override ParseResult<Instant> ParseImpl(string value)
+            {                
                 long number;
-                if (Int64.TryParse(value, ParsingNumberStyles, formatInfo.NumberFormat, out number))
+                if (Int64.TryParse(value, ParsingNumberStyles, FormatInfo.NumberFormat, out number))
                 {
                     return ParseResult<Instant>.ForValue(new Instant(number));
                 }
                 return ParseResult<Instant>.CannotParseValue(value, pattern);
             }
 
-            public string Format(Instant value)
+            public override string Format(Instant value)
             {
-                return value.Ticks.ToString(systemFormatString, formatInfo);
+                return value.Ticks.ToString(systemFormatString, FormatInfo);
             }
         }
     }
