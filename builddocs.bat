de2791c38c5eea6706261ceaa8055118952409e9
Jon Skeet <skeet@pobox.com>
2011-09-04 21:52:38 +0100

    Quick and dirty batch file to build documentation.

---
+++ builddocs.bat
@@ -0,0 +1,6 @@
+REM TODO: Don't do this in a batch file!
+
+msbuild "src\NodaTime VS2010.sln" /property:Configuration=Release
+msbuild "tools\NodaTime.Tools.sln" /property:Configuration=Release
+tools\NodaTime.Tools.BuildMarkdownDocs\bin\Release\NodaTime.Tools.BuildMarkdownDocs tools\userguide-src docs\userguide
+msbuild NodaTime.shfbproj
