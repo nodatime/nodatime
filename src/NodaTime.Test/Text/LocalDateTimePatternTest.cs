93934a94213dcb5eb1fd9d79105dfab76d1f3874
Jon Skeet <skeet@pobox.com>
2011-10-19 19:54:35 +0100

    Workarounds for NCrunch's slightly-broken TestCaseSource support.

--- src/NodaTime.Test/Text/LocalDateTimePatternTest.cs
+++ src/NodaTime.Test/Text/LocalDateTimePatternTest.cs
@@ -35,10 +35,11 @@ namespace NodaTime.Test.Text
         private static readonly string[] AllStandardPatterns = { "f", "F", "g", "G", "o", "O", "s" };
         private static readonly IEnumerable<CultureInfo> AllCultures = CultureInfo.GetCultures(CultureTypes.SpecificCultures).ToList();
 
+        // TODO: Uncomment the code below when NCrunch supports names properly.
         // Used by tests via reflection - do not remove!
         private static readonly object[] AllCulturesStandardPatterns = (from culture in AllCultures
                                                                         from format in AllStandardPatterns
-                                                                        select new TestCaseData(culture, format).SetName(culture + ": " + format)).ToArray();
+                                                                        select new TestCaseData(culture, format)/*.SetName(culture + ": " + format)*/).ToArray();
 
 
         [Test]
