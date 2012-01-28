fba2900f3bac0920004a5f6d6fe322c922197b5d
Jon Skeet <skeet@pobox.com>
2012-01-28 11:05:33 +0000

    We have parity :) Noda Time and Joda Time issue the same set of transition instants and offsets (standard and savings) for all known time zones in the period 1900-2050.
    - Minor improvements to JodaDump
    - Readme for JodaDump
    - NodaTime equivalent of JodaDump

--- src/JodaDump/nodatime/jodadump/JodaDump.java
+++ src/JodaDump/nodatime/jodadump/JodaDump.java
@@ -1,8 +1,26 @@
+// Copyright 2009-2012 Jon Skeet
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package nodatime.jodadump;
 
-import java.io.*;
-//File;
-//import java.io.IOException;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 import org.joda.time.*;
@@ -40,14 +58,17 @@ public final class JodaDump {
                 dumpZone(id, zone);
             }
         } else {
-            for (String id : zones.keySet()) {
+            // Sort IDs in ordinal fashion
+            List<String> ids = new ArrayList<String>(zones.keySet());
+            Collections.sort(ids); // compareTo does ordinal comparisons
+            for (String id : ids) {
                 dumpZone(id, zones.get(id));
             }
         }
     }
     
     private static final DateTimeFormatter INSTANT_FORMAT = DateTimeFormat
-        .forPattern("yyyy-MM-dd HH:mm:ss.sss")
+        .forPattern("yyyy-MM-dd HH:mm:ss")
         .withZone(DateTimeZone.UTC);
     private static final Instant START =
         new DateTime(1900, 1, 1, 0, 0, DateTimeZone.UTC).toInstant();
@@ -55,22 +76,25 @@ public final class JodaDump {
         new DateTime(2050, 1, 1, 0, 0, DateTimeZone.UTC).toInstant();
     
     private static void dumpZone(String id, DateTimeZone zone) {
-        if (!id.equals(zone.getID())) {
-            System.out.println(id + " == " + zone.getID());
-            System.out.println();
-            return;
-        }
-        
+        // TODO: Reinstate this when Noda Time understands aliases better.
+        // See issue 32 for more details.
+        // if (!id.equals(zone.getID())) {
+        //    System.out.println(id + " == " + zone.getID());
+        //    System.out.println();
+        //    return;
+        // }
         System.out.println(id);
         long now = START.getMillis();
         while (now < END.getMillis()) {
             int standardOffset = zone.getStandardOffset(now);
             int wallOffset = zone.getOffset(now);
             
-            System.out.println(INSTANT_FORMAT.print(now) + "  "
-                               + printOffset(standardOffset) + "  "
-                               + printOffset(wallOffset - standardOffset) + "  "
-                               + zone.getName(now));
+            System.out.printf("%s  %s  %s%n",
+                              INSTANT_FORMAT.print(now),
+                              printOffset(standardOffset),
+                              printOffset(wallOffset - standardOffset));
+            // TODO: Output the name when we've got parity in handling
+            // of auto-addition for summer/winter in Noda Time.
             long next = zone.nextTransition(now);
             if (next <= now) {
                 break;
