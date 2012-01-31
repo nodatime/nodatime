b14e359fdedde0bda965fbfb07487ecf2fc5a6a1
Jon Skeet <skeet@pobox.com>
2012-01-31 19:35:38 +0000

    Add the files I forgot on the last commit...

---
+++ src/NodaTime.Test/Text/InstantPatternTest.Data.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NodaTime.Text;
+using NodaTime.Properties;
+
+namespace NodaTime.Test.Text
+{
+    public partial class InstantPatternTest
+    {
+         internal static readonly Data[] InvalidPatternData = {
+            new Data { Pattern = "!", Message = Messages.Parse_UnknownStandardFormat, Parameters = {'!', typeof(Instant).FullName}},
+            new Data { Pattern = "%", Message = Messages.Parse_UnknownStandardFormat, Parameters = { '%', typeof(Instant).FullName } },
+            new Data { Pattern = "\\", Message = Messages.Parse_UnknownStandardFormat, Parameters = { '\\', typeof(Instant).FullName } },
+            // Just a few - these are taken from other tests
+            new Data { Pattern = "%%", Message = Messages.Parse_PercentDoubled },
+            new Data { Pattern = "%\\", Message = Messages.Parse_EscapeAtEndOfString },
+            new Data { Pattern = "ffffffff", Message = Messages.Parse_RepeatCountExceeded, Parameters = { 'f', 7 } },
+            new Data { Pattern = "FFFFFFFF", Message = Messages.Parse_RepeatCountExceeded, Parameters = { 'F', 7 } },
+        };
+
+        internal static Data[] ParseFailureData = {
+            new Data { Text = "rubbish", Pattern = "yyyyMMddTHH:mm:ss", Message = Messages.Parse_MismatchedNumber, Parameters = { "yyyy" } },
+            new Data { Text = "17 6", Pattern = "HH h", Message = Messages.Parse_InconsistentValues2, Parameters = {'H', 'h', typeof(LocalTime).FullName}},
+            new Data { Text = "17 AM", Pattern = "HH tt", Message = Messages.Parse_InconsistentValues2, Parameters = {'H', 't', typeof(LocalTime).FullName}},
+        };
+
+        internal static Data[] ParseOnlyData = {
+        };
+        
+        internal static Data[] FormatOnlyData = {
+        };
+
+        /// <summary>
+        /// Common test data for both formatting and parsing. A test should be placed here unless is truly
+        /// cannot be run both ways. This ensures that as many round-trip type tests are performed as possible.
+        /// </summary>
+        internal static readonly Data[] FormatAndParseData = {
+            new Data(2012, 1, 31, 17, 36, 45) { Text = "2012-01-31T17:36:45", Pattern = "yyyy-MM-ddTHH:mm:ss" }
+        };
+
+        internal static IEnumerable<Data> ParseData = ParseOnlyData.Concat(FormatAndParseData);
+        internal static IEnumerable<Data> FormatData = FormatOnlyData.Concat(FormatAndParseData);
+
+        /// <summary>
+        /// A container for test data for formatting and parsing <see cref="LocalTime" /> objects.
+        /// </summary>
+        public sealed class Data : PatternTestData<Instant>
+        {
+            protected override Instant DefaultTemplate
+            {
+                get { return Instant.UnixEpoch; }
+            }
+
+            public Data(Instant value)
+                : base(value)
+            {
+            }
+
+            public Data(int year, int month, int day, int hour, int minute, int second)
+                : this(Instant.FromUtc(year, month, day, hour, minute, second))
+            {
+            }
+
+            public Data()
+                : this(Instant.UnixEpoch)
+            {
+            }
+
+            internal override IPattern<Instant> CreatePattern()
+            {
+                return InstantPattern.CreateWithInvariantInfo(Pattern)
+                    .WithCulture(Culture);
+            }
+        }        
+    }
+}
---
+++ src/NodaTime.Test/Text/InstantPatternTest.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace NodaTime.Test.Text
+{
+    [TestFixture]
+    public partial class InstantPatternTest
+    {
+        [Test]
+        [TestCaseSource("InvalidPatternData")]
+        public void InvalidPatterns(Data data)
+        {
+            data.TestInvalidPattern();
+        }
+
+        [Test]
+        [TestCaseSource("ParseFailureData")]
+        public void ParseFailures(Data data)
+        {
+            data.TestParseFailure();
+        }
+
+        [Test]
+        [TestCaseSource("ParseData")]
+        public void Parse(Data data)
+        {
+            data.TestParse();
+        }
+
+        [Test]
+        [TestCaseSource("FormatData")]
+        public void Format(Data data)
+        {
+            data.TestFormat();
+        }
+    }
+}
