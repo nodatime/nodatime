fac75d5ae4eccad060f8b1672e83ee3951979961
Jon Skeet <skeet@pobox.com>
2012-02-10 19:56:30 +0000

    Put builddocs back to what it should be...

--- builddocs.bat
+++ builddocs.bat
@@ -3,4 +3,4 @@ REM TODO: Don't do this in a batch file!
 msbuild "src\NodaTime VS2010.sln" /property:Configuration=Release
 msbuild "tools\NodaTime.Tools.sln" /property:Configuration=Release
 tools\NodaTime.Tools.BuildMarkdownDocs\bin\Release\NodaTime.Tools.BuildMarkdownDocs tools\userguide-src docs\userguide
-REM msbuild NodaTime.shfbproj
+msbuild NodaTime.shfbproj
