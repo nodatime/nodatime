327742db5fac971727a6a1d1ea7fad1a62f45188
Jon Skeet <skeet@pobox.com>
2012-02-13 19:51:50 +0000

    Another TODO done.

--- src/NodaTime/Text/FormatHelper.cs
+++ src/NodaTime/Text/FormatHelper.cs
@@ -27,7 +27,7 @@ namespace NodaTime.Text
     internal static class FormatHelper
     {
         /// <summary>
-        ///   The maximum number of characters allowed for padded values.
+        /// The maximum number of characters allowed for padded values.
         /// </summary>
         internal const int MaximumPaddingLength = 16;
 
@@ -43,7 +43,8 @@ namespace NodaTime.Text
         /// </summary>
         /// <remarks>
         /// Left pads with zeros the value into a field of <paramref name = "length" /> characters. If the value
-        /// is longer than <paramref name = "length" />, the entire value is formatted.
+        /// is longer than <paramref name = "length" />, the entire value is formatted. If the value is negative,
+        /// it is preceded by "-" but this does not count against the length.
         /// </remarks>
         /// <param name="value">The value to format.</param>
         /// <param name="length">The length to fill.</param>
@@ -51,21 +52,25 @@ namespace NodaTime.Text
         /// <exception cref="FormatException">if too many characters are requested. <see cref="MaximumPaddingLength" />.</exception>
         internal static void LeftPad(int value, int length, StringBuilder outputBuffer)
         {
-            // TODO(V1-Blocker): Do this properly... (or at least find out what's wrong with it!)
-            if (value < 0)
-            {
-                outputBuffer.Append('-');
-                if (value == int.MinValue)
-                {
-                    outputBuffer.Append('2');
-                    value = value / 10;
-                }
-                LeftPad(Math.Abs(value), length, outputBuffer);
-                return;
-            }
             if (length > MaximumPaddingLength)
             {
                 throw new FormatException("Too many digits");
+            }
+            if (value < 0)
+            {
+                outputBuffer.Append('-');
+                // Special case, as we can't use Math.Abs.
+                if (value == int.MinValue)
+                {
+                    if (length > 10)
+                    {
+                        outputBuffer.Append("000000".Substring(16 - length));
+                    }
+                    outputBuffer.Append("2147483648");
+                    return;
+                }
+                LeftPad(Math.Abs(value), length, outputBuffer);
+                return;
             }
             var digits = new char[MaximumPaddingLength];
             int pos = MaximumPaddingLength;
@@ -84,6 +89,7 @@ namespace NodaTime.Text
 
         /// <summary>
         /// Formats the given value right padded with zeros.
+        /// Note: current usage means this never has to cope with negative numbers.
         /// </summary>
         /// <param name="value">The value to format.</param>
         /// <param name="length">The length to fill.</param>
@@ -107,6 +113,7 @@ namespace NodaTime.Text
         /// <summary>
         /// Formats the given value right padded with zeros. The rightmost zeros are truncated.  If the entire value is truncated then
         /// the preceeding decimal separater is also removed.
+        /// Note: current usage means this never has to cope with negative numbers.
         /// </summary>
         /// <param name="value">The value to format.</param>
         /// <param name="length">The length to fill.</param>
