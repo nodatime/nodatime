655ee08a0186b011f91653f4f19b34d794e7b363
Jon Skeet <skeet@pobox.com>
2012-02-17 22:00:46 +0000

    Force a clean build so that "old" XML files don't get used, if they're all that's changed.

--- builddocs.bat
+++ builddocs.bat
@@ -1,5 +1,8 @@
 REM TODO: Don't do this in a batch file!
 
+REM Really force a clean build...
+rmdir /s /q src\NodaTime\bin\Release
+
 msbuild "src\NodaTime VS2010.sln" /property:Configuration=Release
 msbuild "tools\NodaTime.Tools.sln" /property:Configuration=Release
 tools\NodaTime.Tools.BuildMarkdownDocs\bin\Release\NodaTime.Tools.BuildMarkdownDocs tools\userguide-src docs\userguide
