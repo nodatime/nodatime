dc2ea24e351b3905d95766caf3e0bb1f8d1f9879
Jon Skeet <skeet@pobox.com>
2012-02-20 21:42:50 +0000

    More docs for delegates.

--- src/NodaTime/Delegates.cs
+++ src/NodaTime/Delegates.cs
@@ -17,6 +17,8 @@
 
 // This file contains all the delegates declared within the NodaTime namespace.
 // It's simpler than either nesting them or giving them a file per delegate.
+// Although Noda Time now only supports .NET 3.5+ (for TimeZoneInfo), it's simplest to keep
+// these delegates around for the moment, in case we need to go back.
 namespace NodaTime
 {
     /// <summary>
