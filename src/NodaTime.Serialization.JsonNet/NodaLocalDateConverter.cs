85bb861c8bac1869dc673e5b0ef01c1b83991fda
Jon Skeet <skeet@pobox.com>
2012-03-18 09:00:00 +0000

    New pattern-based converter, so most of the other classes are now extremely small, and can actually be removed...

--- src/NodaTime.Serialization.JsonNet/NodaLocalDateConverter.cs
+++ src/NodaTime.Serialization.JsonNet/NodaLocalDateConverter.cs
@@ -15,56 +15,19 @@
 // limitations under the License.
 #endregion
 
-using System;
-using System.Globalization;
-using System.IO;
-using Newtonsoft.Json;
+using NodaTime.Text;
 
 namespace NodaTime.Serialization.JsonNet
 {
     /// <summary>
-    /// Converts an <see cref="LocalDate"/> to and from the ISO 8601 date format (e.g. 2008-04-12).
+    /// Converts a <see cref="LocalDate"/> to and from the ISO 8601 date format (e.g. 2008-04-12).
     /// </summary>
-    public class NodaLocalDateConverter : NodaConverterBase<LocalDate>
+    public class NodaLocalDateConverter : NodaPatternConverter<LocalDate>
     {
-        private const string DefaultDateTimeFormat = "yyyy'-'MM'-'dd";
+        private static readonly LocalDatePattern LocalDatePattern = LocalDatePattern.CreateWithInvariantInfo("yyyy'-'MM'-'dd");
 
-        public NodaLocalDateConverter()
+        public NodaLocalDateConverter() : base(LocalDatePattern)
         {
-            // default values
-            DateFormat = DefaultDateTimeFormat;
-            Culture = CultureInfo.CurrentCulture;
-        }
-
-        /// <summary>
-        /// Gets or sets the date format used when converting to and from JSON.
-        /// </summary>
-        /// <value>The date format used when converting to and from JSON.</value>
-        public string DateFormat { get; set; }
-
-        /// <summary>
-        /// Gets or sets the culture used when converting to and from JSON.
-        /// </summary>
-        /// <value>The culture used when converting to and from JSON.</value>
-        public CultureInfo Culture { get; set; }
-
-        protected override LocalDate ReadJsonImpl(JsonReader reader, JsonSerializer serializer)
-        {
-            if (reader.TokenType != JsonToken.String)
-            {
-                throw new InvalidDataException(
-                    string.Format("Unexpected token parsing instant. Expected String, got {0}.",
-                    reader.TokenType));
-            }
-            string text = reader.Value.ToString();
-            return string.IsNullOrEmpty(DateFormat)
-                       ? LocalDate.Parse(text, Culture)
-                       : LocalDate.ParseExact(text, DateFormat, Culture);
-        }
-
-        protected override void WriteJsonImpl(JsonWriter writer, LocalDate value, JsonSerializer serializer)
-        {
-            writer.WriteValue(value.ToString(DateFormat, Culture));
         }
     }
 }
--- src/NodaTime.Serialization.JsonNet/NodaLocalDateTimeConverter.cs
+++ src/NodaTime.Serialization.JsonNet/NodaLocalDateTimeConverter.cs
@@ -15,56 +15,19 @@
 // limitations under the License.
 #endregion
 
-using System;
-using System.Globalization;
-using System.IO;
-using Newtonsoft.Json;
+using NodaTime.Text;
 
 namespace NodaTime.Serialization.JsonNet
 {
     /// <summary>
-    /// Converts an <see cref="LocalDateTime"/> to and from the ISO 8601 date format without a timezone specified (e.g. 2008-04-12T12:53).
+    /// Converts a <see cref="LocalDateTime"/> to and from the ISO 8601 date format without a timezone specified (e.g. 2008-04-12T12:53).
     /// </summary>
-    public class NodaLocalDateTimeConverter : NodaConverterBase<LocalDateTime>
+    public class NodaLocalDateTimeConverter : NodaPatternConverter<LocalDateTime>
     {
-        private const string DefaultDateTimeFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ss.FFFFFFF";
+        private static readonly LocalDateTimePattern LocalDateTimePattern = LocalDateTimePattern.CreateWithInvariantInfo("yyyy'-'MM'-'dd'T'HH':'mm':'ss.FFFFFFF");
 
-        public NodaLocalDateTimeConverter()
+        public NodaLocalDateTimeConverter() : base(LocalDateTimePattern)
         {
-            // default values
-            DateTimeFormat = DefaultDateTimeFormat;
-            Culture = CultureInfo.CurrentCulture;
-        }
-
-        /// <summary>
-        /// Gets or sets the date time format used when converting to and from JSON.
-        /// </summary>
-        /// <value>The date time format used when converting to and from JSON.</value>
-        public string DateTimeFormat { get; set; }
-
-        /// <summary>
-        /// Gets or sets the culture used when converting to and from JSON.
-        /// </summary>
-        /// <value>The culture used when converting to and from JSON.</value>
-        public CultureInfo Culture { get; set; }
-
-        protected override LocalDateTime ReadJsonImpl(JsonReader reader, JsonSerializer serializer)
-        {
-            if (reader.TokenType != JsonToken.String)
-            {
-                throw new InvalidDataException(
-                    string.Format("Unexpected token parsing instant. Expected String, got {0}.",
-                    reader.TokenType));
-            }
-            string text = reader.Value.ToString();
-            return string.IsNullOrEmpty(DateTimeFormat)
-                       ? LocalDateTime.Parse(text, Culture)
-                       : LocalDateTime.ParseExact(text, DateTimeFormat, Culture);
-        }
-
-        protected override void WriteJsonImpl(JsonWriter writer, LocalDateTime value, JsonSerializer serializer)
-        {
-            writer.WriteValue(value.ToString(DateTimeFormat, Culture));
         }
     }
 }
