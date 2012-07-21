aec586a29d644bd6620e7c198538b7bf8039295f
Jon Skeet <skeet@pobox.com>
2012-07-21 07:27:33 +0100

    Fix first Mono/RPi failure (skip the test).

--- src/NodaTime.Test/TimeZones/BclDateTimeZoneSourceTest.cs
+++ src/NodaTime.Test/TimeZones/BclDateTimeZoneSourceTest.cs
@@ -47,6 +47,11 @@ namespace NodaTime.Test.TimeZones
         {
             // Only a few fixed zones are advertised by Windows. We happen to know this one
             // is wherever we run tests :)
+            // Unfortunately, it doesn't always exist on Mono (at least not on the Rasperry Pi...)
+            if (TestHelper.IsRunningOnMono)
+            {
+                return;
+            }
             string id = "UTC-02";
             var source = new BclDateTimeZoneSource();
             Assert.Contains(id, source.GetIds().ToList());
