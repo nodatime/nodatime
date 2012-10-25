e7c8816e1d477a268f87f73181414ffcc152edd9
Jon Skeet <skeet@pobox.com>
2012-10-25 23:45:50 +0100

    Possible fix to issue 124 - document the NodaTime.Testing and NodaTime.Serialization.JsonNet namespaces. Malcolm to decide whether this is good enough :)

---
+++ src/NodaTime.Serialization.JsonNet/NamespaceDoc.cs
@@ -0,0 +1,36 @@
+﻿#region Copyright and license information
+// Copyright 2001-2009 Stephen Colebourne
+// Copyright 2009-2011 Jon Skeet
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System.Runtime.CompilerServices;
+
+namespace NodaTime.Serialization.JsonNet
+{
+    /// <summary>
+    /// <para>
+    /// The NodaTime.Serialization.JsonNet namespace contains support code to enable
+    /// JSON serialization for Noda Time types using <see href="http://json.net">Json.NET</see>.
+    /// </para>
+    /// <para>Code in this namespace is not currently included in Noda Time NuGet packages;
+    /// it is still deemed "experimental". To use these serializers, please download and build the Noda Time
+    /// source code from the <see cref="http://noda-time.googlecode.com">project home page</see>.</para>
+    /// </summary>
+    [CompilerGenerated]
+    internal class NamespaceDoc
+    {
+        // No actual code here - it's just for the sake of documenting the namespace.
+    }
+}
---
+++ src/NodaTime.Testing/NamespaceDoc.cs
@@ -0,0 +1,37 @@
+﻿#region Copyright and license information
+// Copyright 2001-2009 Stephen Colebourne
+// Copyright 2009-2011 Jon Skeet
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#endregion
+
+using System.Runtime.CompilerServices;
+
+namespace NodaTime.Testing
+{
+    /// <summary>
+    /// <para>
+    /// The NodaTime.Testing namespace contains types designed to help write tests against
+    /// code which uses Noda Time. "Child" namespaces are organized to reflect the same structure
+    /// as the main project.
+    /// </para>
+    /// <para>
+    /// This code is not included as part of the main NuGet package; it's in a separate NodaTime.Testing package.
+    /// </para>
+    /// </summary>
+    [CompilerGenerated]
+    internal class NamespaceDoc
+    {
+        // No actual code here - it's just for the sake of documenting the namespace.
+    }
+}
