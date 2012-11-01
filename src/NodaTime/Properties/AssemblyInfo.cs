b516ac99a1e29fe4caced4b9a9be1342da3b1375
Jon Skeet <skeet@pobox.com>
2012-11-01 10:21:06 +0000

    RC1 version changes

--- src/NodaTime/Properties/AssemblyInfo.cs
+++ src/NodaTime/Properties/AssemblyInfo.cs
@@ -57,7 +57,7 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("1.0")]
 [assembly: AssemblyFileVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-beta2")]
+[assembly: AssemblyInformationalVersion("1.0.0-rc1")]
 [assembly: CLSCompliant(true)]
 [assembly: NeutralResourcesLanguage("en")]
 
