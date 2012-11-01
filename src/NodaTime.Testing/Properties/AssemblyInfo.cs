8f785edd3df958a1dbee2ea6d8048ff513851378
Jon Skeet <skeet@pobox.com>
2012-11-01 10:21:06 +0000

    RC1 version changes

--- src/NodaTime.Testing/Properties/AssemblyInfo.cs
+++ src/NodaTime.Testing/Properties/AssemblyInfo.cs
@@ -50,4 +50,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0")]
 [assembly: AssemblyFileVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-beta2")]
+[assembly: AssemblyInformationalVersion("1.0.0-rc1")]
