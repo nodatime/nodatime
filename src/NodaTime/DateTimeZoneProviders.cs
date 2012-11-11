cb33de7cd091a3f1c115e9e7e71b5b3a77573383
Malcolm Rowe <malcolm.rowe@gmail.com>
2012-11-11 00:00:19 +0000

    Add TzdbDateTimeZoneSource.Default, which provides access to the TZDB source that was initialised from the embedded version of TZDB.
    Fixes issue 144.
    
    Also improve documentation slightly.

--- src/NodaTime/DateTimeZoneProviders.cs
+++ src/NodaTime/DateTimeZoneProviders.cs
@@ -7,7 +7,7 @@ namespace NodaTime
     /// </summary>
     public static class DateTimeZoneProviders
     {
-        private static readonly DateTimeZoneCache tzdbFactory = new DateTimeZoneCache(new TzdbDateTimeZoneSource("NodaTime.TimeZones.Tzdb"));
+        private static readonly DateTimeZoneCache tzdbFactory = new DateTimeZoneCache(TzdbDateTimeZoneSource.Default);
         private static readonly DateTimeZoneCache bclFactory = new DateTimeZoneCache(new BclDateTimeZoneSource());
 
         /// <summary>
@@ -19,12 +19,14 @@ namespace NodaTime
         public static IDateTimeZoneProvider Default { get { return Tzdb; } }
 
         /// <summary>
-        /// Gets a time zone provider which uses the <see cref="TzdbDateTimeZoneSource"/>.
+        /// Gets a time zone provider which uses a <see cref="TzdbDateTimeZoneSource"/>.
+        /// The underlying source is <see cref="TzdbDateTimeZoneSource.Default"/>, which is initialized from
+        /// resources within the NodaTime assembly.
         /// </summary>
         public static IDateTimeZoneProvider Tzdb { get { return tzdbFactory; } }
 
         /// <summary>
-        /// Gets a time zone provider which uses the <see cref="BclDateTimeZoneSource"/>.
+        /// Gets a time zone provider which uses a <see cref="BclDateTimeZoneSource"/>.
         /// </summary>
         public static IDateTimeZoneProvider Bcl { get { return bclFactory; } }
     }
