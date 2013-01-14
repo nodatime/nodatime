5c371af01bc32b8b6f40cfed5e16e5844fc7c649
Jon Skeet <skeet@pobox.com>
2013-01-14 19:58:45 +0000

    Remove regions for using directives. If we were doing this consistently everywhere, it would be one thing - but I don't think there's any benefit in doing it just in a few files.

--- src/NodaTime.Test/Globalization/FailingCultureInfo.cs
+++ src/NodaTime.Test/Globalization/FailingCultureInfo.cs
@@ -14,11 +14,9 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 #endregion
-#region usings
+
 using System;
 using System.Globalization;
-
-#endregion
 
 namespace NodaTime.Test.Globalization
 {
