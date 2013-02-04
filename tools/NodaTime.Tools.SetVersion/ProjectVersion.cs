2dc66b3dd788b9e2bcb342eb6f2ed9e11eadfe1a
Jon Skeet <skeet@pobox.com>
2013-02-04 15:26:24 +0000

    First implementation of SetVersion, and updated nuspec/AssemblyInfo files. I've kept the embarrassingly awful version number choice, but I hang my head in shame at it nonetheless.

---
+++ tools/NodaTime.Tools.SetVersion/ProjectVersion.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace NodaTime.Tools.SetVersion
+{
+    internal sealed class ProjectVersion
+    {
+        private static readonly Regex Pattern = new Regex(@"^(\d+\.\d+)\.(\d)+(-.*)?$");
+
+        /// <summary>
+        /// The original input, e.g. 1.1.2-dev
+        /// </summary>
+        public string FullText { get; private set; }
+
+        /// <summary>
+        /// The dependency version, which is Major.Minor.0 for stable releases,
+        /// and the full version text otherwise. (i.e. if there's a -foo part at the end,
+        /// use the full text.)
+        /// </summary>
+        public string Dependency { get; private set; }
+
+        /// <summary>
+        /// The major/minor/patch version.
+        /// So a full version of 1.1.2-dev would give 1.1.2.
+        /// </summary>
+        public string MajorMinorPatch { get; private set; }
+
+        /// <summary>
+        /// The major/minor version.
+        /// So a full version of 1.1.2-dev would give 1.1.
+        /// </summary>
+        public string MajorMinor { get; private set; }
+
+        internal ProjectVersion(string text)
+        {
+            Match match = Pattern.Match(text);
+            if (!match.Success)
+            {
+                throw new ArgumentException("Failed to match pattern");
+            }
+            FullText = text;
+            MajorMinor = match.Groups[1].Value;
+            MajorMinorPatch = MajorMinor + "." + match.Groups[2];
+            Dependency = FullText == MajorMinorPatch ? MajorMinor + ".0" : FullText;
+        }
+    }
+}
--- tools/NodaTime.Tools.SetVersion/Properties/AssemblyInfo.cs
+++ tools/NodaTime.Tools.SetVersion/Properties/AssemblyInfo.cs
@@ -1,31 +1,26 @@
-// Copyright 2011 The Noda Time Authors. All rights reserved.
-// Use of this source code is governed by the Apache License 2.0,
-// as found in the LICENSE.txt file.
-
-using System.Reflection;
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("NodaTime.Testing")]
+[assembly: AssemblyTitle("NodaTime.Tools.SetVersion")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NodaTime.Testing")]
-[assembly: AssemblyCopyright("Copyright 2013 The Noda Time Authors. All rights reserved.")]
+[assembly: AssemblyProduct("NodaTime.Tools.SetVersion")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-#if !PCL
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("961b5c16-d9c8-4cf4-9c5b-6f44523e2749")]
-#endif
+[assembly: Guid("eacca970-deea-47d4-a2e7-a1aceb7439b5")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -37,6 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1")]
-[assembly: AssemblyFileVersion("1.1.0")]
-[assembly: AssemblyInformationalVersion("1.1.0-dev-2013-31-01")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
