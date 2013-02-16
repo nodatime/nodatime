d3e3d08a4b516ecb882cee24a45253b804c72ce5
Jon Skeet <skeet@pobox.com>
2013-02-16 16:43:20 +0000

    Initial stab at a buildrelease batch file to automate some of the tedious copying. We won't be able to *really* know whether it's there until we do a beta release.

---
+++ buildrelease.bat
@@ -0,0 +1,47 @@
+@echo off
+
+if "%1" == "" (
+  echo Usage: buildrelease version-number
+  goto end
+)
+
+if NOT EXIST "NodaTime Release.snk" (
+  echo Copy NodaTime Release.snk into this directory first.
+  goto end
+)
+
+set VERSION=%1
+
+hg up -r %VERSION%
+IF ERRORLEVEL 1 EXIT /B 1
+
+hg archive -r %VERSION% NodaTime-%VERSION%-src.zip
+IF ERRORLEVEL 1 EXIT /B 1
+
+set STAGING=NodaTime-%VERSION%
+IF EXIST %STAGING% rmdir /s /q %STAGING%
+mkdir %STAGING%
+mkdir %STAGING%\docs
+
+xcopy /q /s /i docs\api %STAGING%\docs\api
+xcopy /q /s /i docs\developer %STAGING%\docs\developer
+xcopy /q /s /i docs\userguide %STAGING%\docs\userguide
+copy AUTHORS.txt %STAGING%
+copy LICENSE.txt %STAGING%
+copy "NodaTime Release Public Key.snk" %STAGING%
+copy readme.txt %STAGING%
+
+call buildnuget
+IF ERRORLEVEL 1 EXIT /B 1
+
+mkdir %STAGING%\Portable
+copy docs\PublicApi\NodaTime.xml %STAGING%
+copy docs\PublicApi\NodaTime.Testing.xml %STAGING%
+copy "src\NodaTime\bin\Signed Release\NodaTime.dll" %STAGING%
+copy "src\NodaTime.Testing\bin\Signed Release\NodaTime.Testing.dll" %STAGING%
+copy "src\NodaTime\bin\Signed Release Portable\NodaTime.dll" %STAGING%\Portable
+copy "src\NodaTime.Testing\bin\Signed Release Portable\NodaTime.Testing.dll" %STAGING%\Portable
+  
+zip -r -9 %STAGING%.zip %STAGING%
+  
+:end
