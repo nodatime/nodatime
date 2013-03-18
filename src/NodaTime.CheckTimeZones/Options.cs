06b7069391d32451a21d7571a88503911b196454
Jon Skeet <skeet@pobox.com>
2013-03-18 19:44:02 +0000

    Add command line options to CheckZones.

---
+++ src/NodaTime.CheckTimeZones/Options.cs
@@ -0,0 +1,42 @@
+﻿// Copyright 2013 The Noda Time Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+using CommandLine;
+using CommandLine.Text;
+
+namespace NodaTime.CheckTimeZones
+{
+    internal class Options
+    {
+        [Option("f", "file", Required = false, HelpText = "File to load zone database from, e.g. tzdb.nzd. If unspecifed, built-in zones are used.")]
+        public string File { get; set; }
+
+        [Option(null, "from", HelpText = "'From' year: the first year to show transitions from.", DefaultValue = 1950)]
+        public int FromYear { get; set; }
+
+        [Option(null, "to", HelpText = "'To' year: the last year to show transitions from", DefaultValue = 2049)]
+        public int ToYear { get; set; }
+
+        [Option("z", "zone", HelpText = "Zone ID of the single zone to display. If unspecified, all zones are shown.")]
+        public string Zone { get; set; }
+
+        [HelpOption("?", "help", HelpText = "Display this help screen.")]
+        public string GetUsage()
+        {
+            var help = new HelpText(new HeadingInfo("NodaTime.CheckZones"))
+            {
+                AdditionalNewLineAfterOption = true,
+            };
+            help.AddOptions(this);
+            return help;
+        }
+
+
+        internal Options()
+        {
+            FromYear = 1950;
+            ToYear = 2050;
+        }
+    }
+}
--- src/NodaTime.CheckTimeZones/Program.cs
+++ src/NodaTime.CheckTimeZones/Program.cs
@@ -5,7 +5,9 @@
 using System;
 using System.IO;
 using System.Linq;
+using CommandLine;
 using NodaTime.Text;
+using NodaTime.TimeZones;
 
 namespace NodaTime.CheckTimeZones
 {
@@ -17,42 +19,73 @@ namespace NodaTime.CheckTimeZones
     /// Currently, there's just code to dump both sets of transitions (for all zones) - you can then
     /// use a diffing tool to see the changes (or lack thereof).
     /// </remarks>
-    // TODO(Post-V1): Include the ability to dump a single zone
-    // TODO(Post-V1): Include the ability to parse the output of JodaDump and just validate it
-    // TODO(Post-V1): Include the ability to specify a resx file or possibly even a tzdb data directory
     public class Program
     {
-        private static readonly Instant Start = Instant.FromUtc(1900, 1, 1, 0,  0);
-        private static readonly Instant End = Instant.FromUtc(2050, 1, 1, 0, 0);
-
         private static readonly InstantPattern DateTimePattern
             = InstantPattern.CreateWithInvariantCulture("yyyy-MM-dd HH:mm:ss");
 
         private static readonly OffsetPattern OffsetPattern
             = OffsetPattern.CreateWithInvariantCulture("+HH:mm:ss");
 
-        private static void Main()
+        private static int Main(string[] args)
         {
-            // Ensure it really is a culture-insensitive ordinal ordering.
-            foreach (var id in DateTimeZoneProviders.Tzdb.Ids.OrderBy(x => x, StringComparer.Ordinal))
+            Options options = new Options();
+            ICommandLineParser parser = new CommandLineParser(new CommandLineParserSettings(Console.Error));
+            if (!parser.ParseArguments(args, options))
             {
-                DumpZone(DateTimeZoneProviders.Tzdb[id], Console.Out);
+                return 1;
             }
+            if (options.FromYear > options.ToYear)
+            {
+                Console.WriteLine("Error: 'from' year must be not be later than 'to' year");
+                return 1;
+            }
+            IDateTimeZoneSource source = TzdbDateTimeZoneSource.Default;
+
+            if (options.File != null)
+            {
+                using (var stream = File.OpenRead(options.File))
+                {
+                    source = TzdbDateTimeZoneSource.FromStream(stream);
+                }
+            }
+            Console.WriteLine("TZDB version: {0}", source.VersionId);
+
+            var provider = new DateTimeZoneCache(source);
+
+            if (options.Zone != null)
+            {
+                var zone = provider.GetZoneOrNull(options.Zone);
+                if (zone == null)
+                {
+                    Console.WriteLine("Unknown time zone: {0}", options.Zone);
+                    return 1;
+                }
+                DumpZone(zone, Console.Out, options.FromYear, options.ToYear);
+            }
+            else
+            {
+                foreach (var id in provider.Ids)
+                {
+                    DumpZone(provider[id], Console.Out, options.FromYear, options.ToYear);
+                }
+            }
+            return 0;
         }
 
-        private static void DumpZone(DateTimeZone zone, TextWriter output)
+        private static void DumpZone(DateTimeZone zone, TextWriter output, int fromYear, int toYear)
         {
             output.WriteLine(zone.Id);
-
-            Instant instant = Start;
-            while (instant < End)
+            var start = Instant.FromUtc(fromYear, 1, 1, 0, 0);
+            // Exclusive upper bound
+            var end = Instant.FromUtc(toYear + 1, 1, 1, 0, 0);
+            
+            foreach (var interval in zone.GetAllZoneIntervals(start, end))
             {
-                var interval = zone.GetZoneInterval(instant);
-                output.WriteLine("{0}  {1}  {2}",
-                    DateTimePattern.Format(instant),
+               output.WriteLine("{0}  {1}  {2}",
+                    DateTimePattern.Format(Instant.Max(start, interval.Start)),
                     OffsetPattern.Format(interval.StandardOffset),
                     OffsetPattern.Format(interval.Savings));
-                instant = interval.End;
             }
 
             output.WriteLine();
