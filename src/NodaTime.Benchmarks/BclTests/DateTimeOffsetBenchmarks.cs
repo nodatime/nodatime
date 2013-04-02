a36147869e05b96ef485007dd400b8d0afb9cc2a
Jon Skeet <skeet@pobox.com>
2013-04-02 20:38:04 +0100

    Initial benchmarks for comparisons between DateTimeOffset and OffsetDateTime.

---
+++ src/NodaTime.Benchmarks/BclTests/DateTimeOffsetBenchmarks.cs
@@ -0,0 +1,45 @@
+﻿// Copyright 2013 The Noda Time Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NodaTime.Benchmarks.Framework;
+
+namespace NodaTime.Benchmarks.BclTests
+{
+    public class DateTimeOffsetBenchmarks
+    {
+        private static readonly DateTimeOffset sample = new DateTimeOffset(2009, 12, 26, 10, 8, 30, 234, TimeSpan.Zero);
+        private static readonly DateTimeOffset earlier = new DateTimeOffset(2009, 12, 26, 10, 8, 30, 234, TimeSpan.FromHours(1));
+        private static readonly DateTimeOffset later = new DateTimeOffset(2009, 12, 26, 10, 8, 30, 234, TimeSpan.FromHours(-1));
+
+        private static readonly IComparer<DateTimeOffset> defaultComparer = Comparer<DateTimeOffset>.Default;
+            
+        [Benchmark]
+        public void CompareTo()
+        {
+            sample.CompareTo(earlier);
+            sample.CompareTo(sample);
+            sample.CompareTo(later);
+        }
+
+        [Benchmark]
+        public void Comparer_Compare()
+        {
+            defaultComparer.Compare(sample, earlier);
+            defaultComparer.Compare(sample, sample);
+            defaultComparer.Compare(sample, later);
+        }
+
+        [Benchmark]
+        public void Comparison_Operators()
+        {
+            (sample < earlier).Consume();
+            (sample < sample).Consume();
+            (sample < later).Consume();
+        }
+    }
+}
