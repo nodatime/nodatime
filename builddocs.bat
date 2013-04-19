bc19f46dd104994a58675eea7fa2d4be8c32b976
Jon Skeet <skeet@pobox.com>
2013-04-19 18:56:50 +0100

    Hopeful attempt at building docs and nuget packages. Fingers crossed.

--- builddocs.bat
+++ builddocs.bat
@@ -4,16 +4,12 @@ REM TODO: Don't do this in a batch file!
 REM Really force a clean build...
 rmdir /s /q src\NodaTime\bin\Release
 
-msbuild "src\NodaTime.sln" /property:Configuration=Release
+msbuild "src\NodaTime-All.sln" /property:Configuration=Release
 IF ERRORLEVEL 1 EXIT /B 1
 
-msbuild "tools\NodaTime.Tools.sln" /property:Configuration=Release
+src\NodaTime.Tools.BuildMarkdownDocs\bin\Release\NodaTime.Tools.BuildMarkdownDocs src\tools\userguide docs\userguide
 IF ERRORLEVEL 1 EXIT /B 1
-
-tools\NodaTime.Tools.BuildMarkdownDocs\bin\Release\NodaTime.Tools.BuildMarkdownDocs tools\userguide-src docs\userguide
-IF ERRORLEVEL 1 EXIT /B 1
-tools\NodaTime.Tools.BuildMarkdownDocs\bin\Release\NodaTime.Tools.BuildMarkdownDocs tools\developer-src docs\developer
-
+src\NodaTime.Tools.BuildMarkdownDocs\bin\Release\NodaTime.Tools.BuildMarkdownDocs src\docs\developer docs\developer
 IF ERRORLEVEL 1 EXIT /B 1
 
 msbuild NodaTime.shfbproj
