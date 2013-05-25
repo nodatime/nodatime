1bad1e86735b08a88f92236db3de4da577c6f7a1
Malcolm Rowe <malcolm.rowe@gmail.com>
2013-05-25 20:48:42 +0100

    Documentation improvements for AmbiguousTimeResolver, SkippedTimeResolver, and ZoneLocalMappingResolver.

--- src/NodaTime/TimeZones/Delegates.cs
+++ src/NodaTime/TimeZones/Delegates.cs
@@ -7,15 +7,17 @@ namespace NodaTime.TimeZones
     // Delegates used for mapping local date/time values to ZonedDateTime.
 
     /// <summary>
-    /// Resolves a <see cref="LocalDateTime"/> to a <see cref="ZonedDateTime" /> in the situation
-    /// where the requested local time is ambiguous, due to clocks moving backward in a time zone transition
-    /// (usually due to an autumnal daylight saving transition).
+    /// Chooses between two <see cref="ZonedDateTime"/> values that resolve to the same <see cref="LocalDateTime"/>.
     /// </summary>
     /// <remarks>
     /// <para>
+    /// This delegate is used by <see cref="Resolvers.CreateMappingResolver"/> when handling an ambiguous local time,
+    /// due to clocks moving backward in a time zone transition (usually due to an autumnal daylight saving transition).
+    /// </para>
+    /// <para>
     /// The returned value should be one of the two parameter values, based on the policy of the specific
-    /// implementation. Alternatively, it can throw a <see cref="AmbiguousTimeException" /> to implement a policy of "reject
-    /// ambiguous times."
+    /// implementation. Alternatively, it can throw an <see cref="AmbiguousTimeException" /> to implement a policy of
+    /// "reject ambiguous times."
     /// </para>
     /// <para>See the <see cref="Resolvers" /> class for predefined implementations.</para>
     /// <para>
@@ -27,15 +29,21 @@ namespace NodaTime.TimeZones
     /// <param name="earlier">The earlier of the ambiguous matches for the original local date and time</param>
     /// <param name="later">The later of the ambiguous matches for the original local date and time</param>
     /// <exception cref="AmbiguousTimeException">The implementation rejects requests to map ambiguous times.</exception>
-    /// <returns>A <see cref="ZonedDateTime"/> in the target time zone, </returns>
+    /// <returns>
+    /// A <see cref="ZonedDateTime"/> in the target time zone; typically, one of the two input parameters.
+    /// </returns>
     public delegate ZonedDateTime AmbiguousTimeResolver(ZonedDateTime earlier, ZonedDateTime later);
 
     /// <summary>
     /// Resolves a <see cref="LocalDateTime"/> to a <see cref="ZonedDateTime" /> in the situation
-    /// where the requested local time does not exist, due to clocks moving forward in a time zone transition
-    /// (usually due to a spring daylight saving transition).
+    /// where the requested local time does not exist in the target time zone.
     /// </summary>
     /// <remarks>
+    /// <para>
+    /// This delegate is used by <see cref="Resolvers.CreateMappingResolver"/> when handling the situation where the
+    /// requested local time does not exist, due to clocks moving forward in a time zone transition (usually due to a
+    /// spring daylight saving transition).
+    /// </para>
     /// <para>
     /// The returned value will necessarily represent a different local date and time to the target one, but
     /// the exact form of mapping is up to the delegate implementation. For example, it could return a value
@@ -55,18 +63,24 @@ namespace NodaTime.TimeZones
     /// <param name="intervalBefore">The zone interval directly before the target local date and time would have occurred</param>
     /// <param name="intervalAfter">The zone interval directly after the target local date and time would have occurred</param>
     /// <exception cref="SkippedTimeException">The implementation rejects requests to map skipped times.</exception>
-    /// <returns>A <see cref="ZonedDateTime"/> in the target time zone, </returns>
+    /// <returns>A <see cref="ZonedDateTime"/> in the target time zone.</returns>
     public delegate ZonedDateTime SkippedTimeResolver(LocalDateTime localDateTime, DateTimeZone zone, ZoneInterval intervalBefore, ZoneInterval intervalAfter);
 
     /// <summary>
-    /// Resolves a local date and time into a target time zone according to some policy.
+    /// Resolves the result of attempting to map a local date and time to a target time zone.
     /// </summary>
     /// <remarks>
+    /// <para>
+    /// This delegate is consumed by <see cref="LocalDateTime.InZone"/> and <see cref="DateTimeZone.ResolveLocal"/>,
+    /// among others. It provides the strategy for converting a <see cref="ZoneLocalMapping"/> (the result of attempting
+    /// to map a local date and time to a target time zone) to a <see cref="ZonedDateTime"/>.
+    /// </para>
     /// <para>See the <see cref="Resolvers" /> class for predefined implementations and a way of combining
     /// separate <see cref="SkippedTimeResolver" /> and <see cref="AmbiguousTimeResolver" /> values.</para>
     /// </remarks>
     /// <param name="mapping">The intermediate result of mapping a local time to a target time zone.</param>
+    /// <exception cref="AmbiguousTimeException">The implementation rejects requests to map ambiguous times.</exception>
     /// <exception cref="SkippedTimeException">The implementation rejects requests to map skipped times.</exception>
-    /// <returns>The resolved date and time.</returns>
+    /// <returns>A <see cref="ZonedDateTime"/> in the target time zone.</returns>
     public delegate ZonedDateTime ZoneLocalMappingResolver(ZoneLocalMapping mapping);
 }
