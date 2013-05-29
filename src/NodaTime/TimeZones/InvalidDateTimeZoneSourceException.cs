2bdb27b4d9f58ce0e9578ff495289bd019ca6bd9
Malcolm Rowe <malcolm.rowe@gmail.com>
2013-05-29 19:55:32 +0100

    Clarify (and in one case, fix) InvalidDateTimeZoneSourceException documentation.

--- src/NodaTime/TimeZones/InvalidDateTimeZoneSourceException.cs
+++ src/NodaTime/TimeZones/InvalidDateTimeZoneSourceException.cs
@@ -7,7 +7,7 @@ using System;
 namespace NodaTime.TimeZones
 {
     /// <summary>
-    /// Exception thrown to indicate that a time zone source has violated the conditions of <see cref="IDateTimeZoneSource"/>.
+    /// Exception thrown to indicate that a time zone source has violated the contract of <see cref="IDateTimeZoneSource"/>.
     /// This exception is primarily intended to be thrown from <see cref="DateTimeZoneCache"/>, and only in the face of a buggy
     /// source; user code should not usually need to be aware of this or catch it.
     /// </summary>
@@ -20,9 +20,9 @@ namespace NodaTime.TimeZones
     public sealed class InvalidDateTimeZoneSourceException : Exception
     {
         /// <summary>
-        /// Creates a new instance.
+        /// Creates a new instance with the given message.
         /// </summary>
-        /// <param name="message">The local date time which is skipped in the specified time zone.</param>
+        /// <param name="message">The message for the exception.</param>
         public InvalidDateTimeZoneSourceException(string message)
             : base(message)
         {
