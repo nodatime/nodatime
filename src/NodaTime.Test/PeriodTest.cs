591dbccca69a19d4323bc7679847bf49f14dee69
Jon Skeet <skeet@pobox.com>
2013-06-13 19:54:12 +0200

    Move the contents of PeriodAdditionTest into PeriodTest, which is where the rest of the related tests are.

--- src/NodaTime.Test/PeriodTest.cs
+++ src/NodaTime.Test/PeriodTest.cs
@@ -277,6 +277,47 @@ namespace NodaTime.Test
         }
 
         [Test]
+        public void Addition_DayCrossingMonthBoundary()
+        {
+            LocalDateTime start = new LocalDateTime(2010, 2, 20, 10, 0);
+            LocalDateTime result = start + Period.FromDays(10);
+            Assert.AreEqual(new LocalDateTime(2010, 3, 2, 10, 0), result);
+        }
+
+        [Test]
+        public void Addition_OneYearOnLeapDay()
+        {
+            LocalDateTime start = new LocalDateTime(2012, 2, 29, 10, 0);
+            LocalDateTime result = start + Period.FromYears(1);
+            // Feb 29th becomes Feb 28th
+            Assert.AreEqual(new LocalDateTime(2013, 2, 28, 10, 0), result);
+        }
+
+        [Test]
+        public void Addition_FourYearsOnLeapDay()
+        {
+            LocalDateTime start = new LocalDateTime(2012, 2, 29, 10, 0);
+            LocalDateTime result = start + Period.FromYears(4);
+            // Feb 29th is still valid in 2016
+            Assert.AreEqual(new LocalDateTime(2016, 2, 29, 10, 0), result);
+        }
+
+        [Test]
+        public void Addition_YearMonthDay()
+        {
+            // One year, one month, two days
+            Period period = Period.FromYears(1) + Period.FromMonths(1) + Period.FromDays(2);
+            LocalDateTime start = new LocalDateTime(2007, 1, 30, 0, 0);
+            // Periods are added in order, so this becomes...
+            // Add one year: Jan 30th 2008
+            // Add one month: Feb 29th 2008
+            // Add two days: March 2nd 2008
+            // If we added the days first, we'd end up with March 1st instead.
+            LocalDateTime result = start + period;
+            Assert.AreEqual(new LocalDateTime(2008, 3, 2, 0, 0), result);
+        }
+
+        [Test]
         public void Subtraction_WithDifferent_PeriodTypes()
         {
             Period p1 = Period.FromHours(3);
