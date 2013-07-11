5565435299becadfb94fdc9248d2e3b751672a63
Jon Skeet <skeet@pobox.com>
2013-07-10 21:40:02 -0700

    Added a simple cache class.

---
+++ src/NodaTime.Test/Utility/CacheTest.cs
@@ -0,0 +1,75 @@
+﻿// Copyright 2013 The Noda Time Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+using System;
+using NUnit.Framework;
+using NodaTime.Utility;
+
+namespace NodaTime.Test.Utility
+{
+    [TestFixture]
+    public class CacheTest
+    {
+        private int factoryCallCount;
+
+        /// <summary>
+        /// Creates a cache of size 3, from string to int, case-insensitive, where the value
+        /// associated with a key is its length. The factory call count is reset to 0, and incremented
+        /// each time the value factory is called.
+        /// </summary>
+        private Cache<string, int> CreateCache()
+        {
+            factoryCallCount = 0;
+            return new Cache<string, int>(3, text => { factoryCallCount++; return text.Length; }, 
+                                          StringComparer.InvariantCultureIgnoreCase);
+        }
+
+        [Test]
+        public void Clear()
+        {
+            var cache = CreateCache();
+            cache.GetOrAdd("A");
+            Assert.AreEqual(1, cache.Count);
+            cache.Clear();
+            Assert.AreEqual(0, cache.Count);
+            CollectionAssert.IsEmpty(cache.Keys);
+        }
+
+        [Test]
+        public void GetOrAdd_NewEntry()
+        {
+            var cache = CreateCache();
+            Assert.AreEqual(0, cache.Count);
+            Assert.AreEqual(1, cache.GetOrAdd("A"));
+            Assert.AreEqual(1, cache.Count);
+            Assert.AreEqual(1, factoryCallCount);
+            CollectionAssert.AreEqual(new[] { "A" }, cache.Keys);
+        }
+
+        [Test]
+        public void GetOrAdd_ExistingEntry()
+        {
+            var cache = CreateCache();
+            Assert.AreEqual(1, cache.GetOrAdd("A"));
+            Assert.AreEqual(1, cache.GetOrAdd("A"));
+            Assert.AreEqual(1, cache.GetOrAdd("A"));
+            Assert.AreEqual(1, factoryCallCount);
+            CollectionAssert.AreEqual(new[] { "A" }, cache.Keys);
+        }
+
+        [Test]
+        public void GetOrAdd_ReplacementEntry()
+        {
+            var cache = CreateCache();
+            Assert.AreEqual(1, cache.GetOrAdd("A"));
+            Assert.AreEqual(2, cache.GetOrAdd("BB"));
+            Assert.AreEqual(3, cache.GetOrAdd("CCC"));
+            CollectionAssert.AreEqual(new[] { "A", "BB", "CCC" }, cache.Keys);
+            Assert.AreEqual(4, cache.GetOrAdd("DDDD"));
+            CollectionAssert.AreEqual(new[] { "BB", "CCC", "DDDD" }, cache.Keys);
+            Assert.AreEqual(3, cache.Count);
+            Assert.AreEqual(4, factoryCallCount);
+        }
+    }
+}
---
+++ src/NodaTime/Utility/Cache.cs
@@ -0,0 +1,106 @@
+﻿// Copyright 2013 The Noda Time Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+using System.Collections.Generic;
+
+namespace NodaTime.Utility
+{
+    /// <summary>
+    /// Implements a thread-safe cache of a fixed size, with a single computation function.
+    /// (That happens to be all we need at the time of writing.)
+    /// </summary>
+    /// <remarks>
+    /// For simplicity's sake, eviction is currently on a least-recently-added basis (not LRU). This
+    /// may change in the future.
+    /// </remarks>
+    /// <typeparam name="TKey">Type of key</typeparam>
+    /// <typeparam name="TValue">Type of value</typeparam>
+    internal sealed class Cache<TKey, TValue>
+    {
+        private readonly int size;
+        private readonly object mutex = new object();
+        private readonly NodaFunc<TKey, TValue> valueFactory;
+        private LinkedList<TKey> keyList; 
+        private readonly Dictionary<TKey, TValue> dictionary; 
+
+        internal Cache(int size, NodaFunc<TKey, TValue> valueFactory, IEqualityComparer<TKey> keyComparer)
+        {
+            this.size = size;
+            this.valueFactory = valueFactory;
+            this.dictionary = new Dictionary<TKey,TValue>(keyComparer);
+            this.keyList = new LinkedList<TKey>();
+        }
+
+        /// <summary>
+        /// Fetches a value from the cache, populating it if necessary.
+        /// </summary>
+        /// <param name="key">Key to fetch</param>
+        /// <returns>The value associated with the key.</returns>
+        internal TValue GetOrAdd(TKey key)
+        {
+            lock (mutex)
+            {
+                // First check the cache...
+                TValue value;
+                if (dictionary.TryGetValue(key, out value))
+                {
+                    return value;
+                }
+
+                // Make space if necessary...
+                if (dictionary.Count == size)
+                {
+                    TKey firstKey = keyList.First.Value;
+                    keyList.RemoveFirst();
+                    dictionary.Remove(firstKey);
+                }
+
+                // Create and cache the new value
+                value = valueFactory(key);
+                keyList.AddLast(key);
+                dictionary[key] = value;
+                return value;
+            }
+        }
+
+        /// <summary>
+        /// Returns the number of entries currently in the cache, primarily for diagnostic purposes.
+        /// </summary>
+        internal int Count
+        { 
+            get 
+            {
+                lock (mutex)
+                {
+                    return dictionary.Count;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Returns a copy of the keys in the cache as a list, for diagnostic purposes.
+        /// </summary>
+        internal List<TKey> Keys
+        {
+            get
+            {
+                lock (mutex)
+                {
+                    return new List<TKey>(keyList);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Clears the cache.
+        /// </summary>
+        internal void Clear()
+        {
+            lock (mutex)
+            {
+                keyList.Clear();
+                dictionary.Clear();
+            }
+        }
+    }
+}
