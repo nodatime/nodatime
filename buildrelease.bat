4f42abf4d1dbc7d120e440bcaec8110537e3ba61
Jon Skeet <skeet@pobox.com>
2013-11-01 14:08:22 +0000

    Build nuget before copying to staging, so we pick up API docs

--- buildrelease.bat
+++ buildrelease.bat
@@ -25,6 +25,9 @@ mkdir %STAGING%\docs
 
 call buildofflineguide.bat %1
 
+call buildnuget
+IF ERRORLEVEL 1 EXIT /B 1
+
 xcopy /q /s /i docs\api %STAGING%\docs\api
 xcopy /q /s /i tmpdocs\_site %STAGING%\docs\userguide
 copy AUTHORS.txt %STAGING%
@@ -32,9 +35,6 @@ copy LICENSE.txt %STAGING%
 copy NOTICE.txt %STAGING%
 copy "NodaTime Release Public Key.snk" %STAGING%
 copy readme.txt %STAGING%
-
-call buildnuget
-IF ERRORLEVEL 1 EXIT /B 1
 
 mkdir %STAGING%\Portable
 copy docs\PublicApi\NodaTime.xml %STAGING%
