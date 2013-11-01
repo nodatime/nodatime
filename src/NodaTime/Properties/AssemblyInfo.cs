b025eb729a254c78a3c4a04d3bbbe27fa1cc23d6
Jon Skeet <skeet@pobox.com>
2013-11-01 13:45:59 +0000

    Updated default branch to be 1.3.0-dev

--- src/NodaTime/Properties/AssemblyInfo.cs
+++ src/NodaTime/Properties/AssemblyInfo.cs
@@ -44,9 +44,9 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.2")]
-[assembly: AssemblyFileVersion("1.2.0")]
-[assembly: AssemblyInformationalVersion("1.2.0-dev")]
+[assembly: AssemblyVersion("1.3")]
+[assembly: AssemblyFileVersion("1.3.0")]
+[assembly: AssemblyInformationalVersion("1.3.0-dev")]
 [assembly: CLSCompliant(true)]
 [assembly: NeutralResourcesLanguage("en")]
 
