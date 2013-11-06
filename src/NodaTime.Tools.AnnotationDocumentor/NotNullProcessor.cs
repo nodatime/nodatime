455ea8705053df8ffdf32aa5853ad7feaba1b3b1
Jon Skeet <skeet@pobox.com>
2013-11-06 14:48:23 +0000

    First pass of the annotation documentor - a post-build tool to edit the XML documentation based on annotations.

---
+++ src/NodaTime.Tools.AnnotationDocumentor/NotNullProcessor.cs
@@ -0,0 +1,48 @@
+﻿// Copyright 2013 The Noda Time Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Xml.Linq;
+
+namespace NodaTime.Tools.AnnotationDocumentor
+{
+    /// <summary>
+    /// Processor for NotNull annotations.
+    /// </summary>
+    internal sealed class NotNullProcessor : ProcessorBase
+    {
+        internal override void ProcessParameter(XElement parent, ParameterInfo parameter, Attribute annotation)
+        {
+            var existingElements = parent.Elements("exception")
+                                         .Where(x => (string)x.Attribute("cref") == "T:System.ArgumentNullException")
+                                         .ToList();
+            var parameterNames = existingElements.Descendants("paramref")
+                                                 .Select(x => (string) x.Attribute("name"))
+                                                 .Concat(new[] { parameter.Name })
+                                                 .Distinct()
+                                                 .ToList();
+            existingElements.Remove();
+            // Just to avoid the repetition later...
+            Func<string, XElement> exceptionElement = name => new XElement("paramref", new XAttribute("name", name));
+
+            XElement newElement = new XElement("exception", new XAttribute("cref", "T:System.ArgumentNullException"));
+            if (parameterNames.Count == 1)
+            {
+                newElement.Add(exceptionElement(parameterNames[0]), " is null.");
+            }
+            else
+            {
+                for (int i = 0; i < parameterNames.Count - 2; i++)
+                {
+                    newElement.Add(exceptionElement(parameterNames[i]), ", ");
+                }
+                newElement.Add(exceptionElement(parameterNames[parameterNames.Count - 2]), " or ",
+                    exceptionElement(parameterNames.Last()),
+                    " is null.");
+            }
+            parent.Add(newElement);
+        }
+    }
+}
---
+++ src/NodaTime.Tools.AnnotationDocumentor/Properties/AssemblyInfo.cs
@@ -0,0 +1,39 @@
+﻿// Copyright 2013 The Noda Time Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("NodaTime.Tools.AnnotationDocumentor")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("NodaTime.Tools.AnnotationDocumentor")]
+[assembly: AssemblyCopyright("Copyright 2013 The Noda Time Authors. All rights reserved.")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("32d6250c-39dc-4de2-a415-9c3ae5f6a079")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
