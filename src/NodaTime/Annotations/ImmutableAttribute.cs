ad9d0f5a79335d14299448b73cfaee21d426d202
Jon Skeet <skeet@pobox.com>
2013-11-09 12:05:35 +0000

    Improve documentation of immutability.

--- src/NodaTime/Annotations/ImmutableAttribute.cs
+++ src/NodaTime/Annotations/ImmutableAttribute.cs
@@ -6,13 +6,26 @@ using System;
 namespace NodaTime.Annotations
 {
     /// <summary>
-    /// Indicates that a type is mutable.
+    /// Indicates that a type is immutable. After construction, the publicly visible
+    /// state of the object will not change.
     /// </summary>
     /// <remarks>
+    /// <p>
+    /// This attribute only applies to types, not fields:
+    /// it's entirely feasible to have a readonly field of a mutable type, or a read/write
+    /// field of an immutable type. In such cases for reference types (classes and interfaces)
+    /// it's important to distinguish between the value of the variable (a reference) and the
+    /// object it refers to. Value types are more complicated as in some cases the compiler
+    /// will copy values before operating on them; however as all value types in Noda Time are
+    /// immutable (aside from explictily implemented serialization operations) this rarely causes
+    /// an issue.
+    /// </p>
+    /// <p>
     /// Some types may be publicly immutable, but contain privately mutable
     /// aspects, e.g. caches. If it proves to be useful to indicate the kind of
     /// immutability we're implementing, we can add an appropriate property to this
     /// attribute.
+    /// </p>
     /// </remarks>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
     internal sealed class ImmutableAttribute : Attribute
