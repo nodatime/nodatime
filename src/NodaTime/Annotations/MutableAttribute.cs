f0fb4220b5cfc0656466f2ed9f551d0365fd9105
Malcolm Rowe <malcolm.rowe@gmail.com>
2013-11-10 11:41:08 +0000

    Fix documentation confusion in a similar way to r3a00fecf9d42.

--- src/NodaTime/Annotations/MutableAttribute.cs
+++ src/NodaTime/Annotations/MutableAttribute.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2013 The Noda Time Authors. All rights reserved.
+ // Copyright 2013 The Noda Time Authors. All rights reserved.
 // Use of this source code is governed by the Apache License 2.0,
 // as found in the LICENSE.txt file.
 using System;
@@ -6,7 +6,7 @@ using System;
 namespace NodaTime.Annotations
 {
     /// <summary>
-    /// Indicates that a type is immutable. Some members of this type
+    /// Indicates that a type is mutable. Some members of this type
     /// allow state to be visibly changed.
     /// </summary>
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
