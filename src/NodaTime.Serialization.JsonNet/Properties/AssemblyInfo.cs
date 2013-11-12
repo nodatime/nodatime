c50f7dc9532ec17ffd725d9078c5a9ebc80b0714
Jon Skeet <skeet@pobox.com>
2013-11-12 19:22:57 +0000

    Updated user guide and version numbers

--- src/NodaTime.Serialization.JsonNet/Properties/AssemblyInfo.cs
+++ src/NodaTime.Serialization.JsonNet/Properties/AssemblyInfo.cs
@@ -44,8 +44,8 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyVersion("1.3")]
-[assembly: AssemblyFileVersion("1.3.0")]
-[assembly: AssemblyInformationalVersion("1.3.0-dev")]
+[assembly: AssemblyVersion("1.2")]
+[assembly: AssemblyFileVersion("1.2.0")]
+[assembly: AssemblyInformationalVersion("1.2.0-rc2")]
 [assembly: CLSCompliant(true)]
 [assembly: NeutralResourcesLanguage("en")]
