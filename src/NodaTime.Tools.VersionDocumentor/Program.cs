5b2593c14d2330494f766bd0f521601a7afa8e7c
Jon Skeet <skeet@pobox.com>
2014-01-02 14:34:17 +0000

    Remove extraneous (and broken) using directives.

--- src/NodaTime.Tools.VersionDocumentor/Program.cs
+++ src/NodaTime.Tools.VersionDocumentor/Program.cs
@@ -3,12 +3,8 @@
 // as found in the LICENSE.txt file.
 using System;
 using System.Collections.Generic;
-using System.Collections.Specialized;
 using System.IO;
 using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Xml.Linq;
 
 namespace NodaTime.Tools.VersionDocumentor
