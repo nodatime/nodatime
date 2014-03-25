dd0935649b4767d7022afbde0361b5104f7ffb64
Jon Skeet <skeet@pobox.com>
2014-03-25 12:44:39 +0000

    Baby steps towards dynamic content for the nodatime.org web site. Once I've got this building successfully on Bagpuss, buildweb.bat will be used to combine the various aspects of the site: - Static content (released documentation, downloads, benchmark data) - Generated static content (from Jekyll) - MVC app

---
+++ buildweb.bat
@@ -0,0 +1,53 @@
+@echo off
+
+if "%2" == "" (
+  echo Usage: buildweb stable-web-docs-directory output-directory
+  echo e.g. buildweb c:\users\jon\StableWebDocs c:\users\jon\nodatime.org
+  echo It is expected that the output directory already exists and is
+  echo set up for git...
+  goto end
+)
+
+set STABLE_DIR=%1
+set WEB_DIR=%2
+
+call :clean
+call buildapidocs
+call build_www
+call build_mvc
+call assemble
+goto :end
+
+
+:clean
+echo STABLE_DIR=%STABLE_DIR%
+goto :end
+
+IF EXIST tmpwebdir rmdir /s /q tmpwebdir
+
+REM Hacky way to start with a clean directory
+move %WEB_DIR% tmpwebdir
+mkdir %WEB_DIR%
+attrib -h tmpwebdir\.git
+move tmpwebdir\.git %WEB_DIR%
+attrib +h %WEB_DIR%\.git
+
+:build_www
+pushd www
+call jekyll build
+popd
+goto :end
+
+:build_mvc
+pushd src\NodaTime.Web
+msbuild /t:Rebuild /p:DeployOnBuild=true /p:Configuration=Release
+
+:assemble
+xcopy /Q /E /Y www\_site %WEB_DIR%
+mkdir %WEB_DIR%\unstable
+xcopy /Q /E /Y /I docs\api %WEB_DIR%\unstable\api
+xcopy /Q /E /Y %STABLE_DIR% %WEB_DIR%
+xcopy /Q /E /Y src\NodaTime.Web\obj\Release\Package\PackageTmp %WEB_DIR%
+goto :end
+
+:end
