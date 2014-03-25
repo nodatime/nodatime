5de08a9a0b8af9f69fd5633d444447a599fa6a9a
Jon Skeet <skeet@pobox.com>
2014-03-25 12:44:39 +0000

    Baby steps towards dynamic content for the nodatime.org web site. Once I've got this building successfully on Bagpuss, buildweb.bat will be used to combine the various aspects of the site: - Static content (released documentation, downloads, benchmark data) - Generated static content (from Jekyll) - MVC app

---
+++ src/NodaTime.Web/Controllers/BenchmarksController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+namespace NodaTime.Web.Controllers
+{
+    public class BenchmarksController : Controller
+    {
+        // GET: /Benchmarks/
+        // or
+        // GET: /Benchmarks/Machines
+        public ActionResult Machines()
+        {
+            object model = new string[] { "Machine1", "Machine2" };
+            return View(model);
+        }
+
+        public ActionResult Machine(string machine)
+        {
+            object model = machine;
+            return View(model);
+        }
+	}
+}
