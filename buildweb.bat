3f5522dfed2cab1e3ce52250b76f8e4d69760f0d
Jon Skeet <skeet@pobox.com>
2014-03-25 19:04:48 +0000

    Update buildweb.bat - I think it's now ready for prime time...

--- buildweb.bat
+++ buildweb.bat
@@ -12,17 +12,19 @@ set STABLE_DIR=%1
 set WEB_DIR=%2
 
 call :clean
+IF ERRORLEVEL 1 EXIT /B 1
 call buildapidocs
-call build_www
-call build_mvc
-call assemble
+IF ERRORLEVEL 1 EXIT /B 1
+call :build_www
+IF ERRORLEVEL 1 EXIT /B 1
+call :build_mvc
+IF ERRORLEVEL 1 EXIT /B 1
+call :assemble
+IF ERRORLEVEL 1 EXIT /B 1
 goto :end
 
 
 :clean
-echo STABLE_DIR=%STABLE_DIR%
-goto :end
-
 IF EXIST tmpwebdir rmdir /s /q tmpwebdir
 
 REM Hacky way to start with a clean directory
@@ -31,20 +33,29 @@ mkdir %WEB_DIR%
 attrib -h tmpwebdir\.git
 move tmpwebdir\.git %WEB_DIR%
 attrib +h %WEB_DIR%\.git
+goto :end
+
 
 :build_www
 pushd www
 call jekyll build
+IF ERRORLEVEL 1 EXIT /B 1
 popd
 goto :end
 
+
 :build_mvc
 pushd src\NodaTime.Web
+nuget restore -PackagesDirectory ..\packages
+IF ERRORLEVEL 1 EXIT /B 1
 msbuild /t:Rebuild /p:DeployOnBuild=true /p:Configuration=Release
+IF ERRORLEVEL 1 EXIT /B 1
+popd
+goto :end
+
 
 :assemble
 xcopy /Q /E /Y www\_site %WEB_DIR%
-mkdir %WEB_DIR%\unstable
 xcopy /Q /E /Y /I docs\api %WEB_DIR%\unstable\api
 xcopy /Q /E /Y %STABLE_DIR% %WEB_DIR%
 xcopy /Q /E /Y src\NodaTime.Web\obj\Release\Package\PackageTmp %WEB_DIR%
