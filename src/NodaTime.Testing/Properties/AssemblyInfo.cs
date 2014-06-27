adbf0252220f7d27d8824ce2776398f03717c526
Jon Skeet <skeet@pobox.com>
2014-06-27 09:16:37 +0100

    Set version to 1.3.0.

--- src/NodaTime.Testing/Properties/AssemblyInfo.cs
+++ src/NodaTime.Testing/Properties/AssemblyInfo.cs
@@ -39,4 +39,4 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.3")]
 [assembly: AssemblyFileVersion("1.3.0")]
-[assembly: AssemblyInformationalVersion("1.3.0-beta1")]
+[assembly: AssemblyInformationalVersion("1.3.0")]
