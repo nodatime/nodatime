b41d385b60aa12929d3cdf9d3c64f2a58a86c0f5
Jon Skeet <skeet@pobox.com>
2014-06-27 17:06:02 +0100

    Update version number to use "alpha" instead of "dev", so that any pre-beta releases will be viewed as "earlier" than the beta ones.

--- src/NodaTime.Serialization.JsonNet/Properties/AssemblyInfo.cs
+++ src/NodaTime.Serialization.JsonNet/Properties/AssemblyInfo.cs
@@ -45,6 +45,6 @@ using System.Runtime.InteropServices;
 
 [assembly: AssemblyVersion("2.0")]
 [assembly: AssemblyFileVersion("2.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0-dev")]
+[assembly: AssemblyInformationalVersion("2.0.0-alpha")]
 [assembly: CLSCompliant(true)]
 [assembly: NeutralResourcesLanguage("en")]
