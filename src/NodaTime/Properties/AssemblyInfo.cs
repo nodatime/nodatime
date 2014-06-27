d755a5649cd380de44fe3584300942e975d030c0
Jon Skeet <skeet@pobox.com>
2014-06-27 17:06:02 +0100

    Update version number to use "alpha" instead of "dev", so that any pre-beta releases will be viewed as "earlier" than the beta ones.

--- src/NodaTime/Properties/AssemblyInfo.cs
+++ src/NodaTime/Properties/AssemblyInfo.cs
@@ -47,7 +47,7 @@ using System.Runtime.CompilerServices;
 
 [assembly: AssemblyVersion("2.0")]
 [assembly: AssemblyFileVersion("2.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0-dev")]
+[assembly: AssemblyInformationalVersion("2.0.0-alpha")]
 [assembly: CLSCompliant(true)]
 [assembly: NeutralResourcesLanguage("en")]
 
