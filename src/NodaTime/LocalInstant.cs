0ee35382664de6a98935f2b279e4c1f5438d6f89
Jon Skeet <skeet@pobox.com>
2014-06-28 21:10:05 +0100

    We already have code to compute days from ticks efficiently, so let's use it.

--- src/NodaTime/LocalInstant.cs
+++ src/NodaTime/LocalInstant.cs
@@ -4,6 +4,7 @@
 
 using System;
 using JetBrains.Annotations;
+using NodaTime.Calendars;
 using NodaTime.Text;
 using NodaTime.Utility;
 
@@ -108,18 +109,7 @@ namespace NodaTime
         /// </summary>
         internal LocalDate ToIsoDate()
         {
-            int days;
-            if (ticks >= 0)
-            {
-                days = (int) (ticks / NodaConstants.TicksPerStandardDay);
-            }
-            else
-            {
-                // Division rounds towards zero; we want the floor instead. We can't just subtract
-                // one from the result of the division, as otherwise we'll be off by one if the instant
-                // is at the very start of a day.
-                days = (int) ((ticks - (NodaConstants.TicksPerStandardDay - 1)) / NodaConstants.TicksPerStandardDay);
-            }
+            int days = TickArithmetic.TicksToDays(ticks);
             return new LocalDate(days, CalendarSystem.Iso);
         }
 
