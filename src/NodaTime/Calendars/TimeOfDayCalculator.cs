b5306fa9ad58f8a3a326bd336cd295e8268d3613
Jon Skeet <skeet@pobox.com>
2014-06-29 08:34:37 +0100

    Make all operations in TimeOfDayCalculator unchecked.

--- src/NodaTime/Calendars/TimeOfDayCalculator.cs
+++ src/NodaTime/Calendars/TimeOfDayCalculator.cs
@@ -2,9 +2,6 @@
 // Use of this source code is governed by the Apache License 2.0,
 // as found in the LICENSE.txt file.
 
-using NodaTime.Fields;
-using NodaTime.Utility;
-
 namespace NodaTime.Calendars
 {
     /// <summary>
@@ -17,43 +14,58 @@ namespace NodaTime.Calendars
     {
         internal static int GetHourOfHalfDayFromTickOfDay(long tickOfDay)
         {
-            return GetHourOfDayFromTickOfDay(tickOfDay) % 12;
+            return unchecked(GetHourOfDayFromTickOfDay(tickOfDay) % 12);
         }
 
         internal static int GetClockHourOfHalfDayFromTickOfDay(long tickOfDay)
         {
-            int hourOfHalfDay = GetHourOfHalfDayFromTickOfDay(tickOfDay);
-            return hourOfHalfDay == 0 ? 12 : hourOfHalfDay;
+            unchecked
+            {
+                int hourOfHalfDay = GetHourOfHalfDayFromTickOfDay(tickOfDay);
+                return hourOfHalfDay == 0 ? 12 : hourOfHalfDay;
+            }
         }
 
         internal static int GetHourOfDayFromTickOfDay(long tickOfDay)
         {
-            // Effectively tickOfDay / NodaConstants.TicksPerHour.
-            // Note that NodaConstants.TicksPerStandardDay >> 11 is about 491 million; less than int.MaxValue.
-            return ((int) (tickOfDay >> 11)) / 17578125;
+            unchecked
+            {
+                // Effectively tickOfDay / NodaConstants.TicksPerHour.
+                // Note that NodaConstants.TicksPerStandardDay >> 11 is about 491 million; less than int.MaxValue.
+                return ((int) (tickOfDay >> 11)) / 17578125;
+            }
         }
 
         internal static int GetMinuteOfHourFromTickOfDay(long tickOfDay)
         {
-            int minuteOfDay = (int) (tickOfDay / (int) NodaConstants.TicksPerMinute);
-            return minuteOfDay % NodaConstants.MinutesPerHour;
+            unchecked
+            {
+                int minuteOfDay = (int) (tickOfDay / (int) NodaConstants.TicksPerMinute);
+                return minuteOfDay % NodaConstants.MinutesPerHour;
+            }
         }
 
         internal static int GetSecondOfMinuteFromTickOfDay(long tickOfDay)
         {
-            int secondOfDay = (int) (tickOfDay / (int) NodaConstants.TicksPerSecond);
-            return secondOfDay % NodaConstants.SecondsPerMinute;
+            unchecked
+            {
+                int secondOfDay = (int) (tickOfDay / (int) NodaConstants.TicksPerSecond);
+                return secondOfDay % NodaConstants.SecondsPerMinute;
+            }
         }
 
         internal static int GetMillisecondOfSecondFromTickOfDay(long tickOfDay)
         {
-            long milliSecondOfDay = (tickOfDay / (int) NodaConstants.TicksPerMillisecond);
-            return (int) (milliSecondOfDay % NodaConstants.MillisecondsPerSecond);
+            unchecked
+            {
+                long milliSecondOfDay = (tickOfDay / (int) NodaConstants.TicksPerMillisecond);
+                return (int) (milliSecondOfDay % NodaConstants.MillisecondsPerSecond);
+            }
         }
 
         internal static int GetTickOfSecondFromTickOfDay(long tickOfDay)
         {
-            return (int) (tickOfDay % (int) NodaConstants.TicksPerSecond);
+            return unchecked((int) (tickOfDay % (int) NodaConstants.TicksPerSecond));
         }
     }
 }
