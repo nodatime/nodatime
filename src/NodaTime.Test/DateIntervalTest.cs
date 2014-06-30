f54c1da257e06d243d78944a684e268aa3563ce8
Jon Skeet <skeet@pobox.com>
2014-06-30 17:51:58 +0100

    First attempt at a date interval type. Open questions:
    - Should this actually be a struct rather than a class?
    - Is it okay to reject dates in different calendars? (Hoping to make this a prevalent approach.)
    - Is Length a decent name, or should it be something like Days?
    - Should we allow serialization? I'm somewhat doubtful, at least for the first
      release - it's easier to add it later than change it.

---
+++ src/NodaTime.Test/DateIntervalTest.cs
@@ -0,0 +1,107 @@
+﻿// Copyright 2013 The Noda Time Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+using System;
+using System.Security.Cryptography;
+using NodaTime.Text;
+using NUnit.Framework;
+
+namespace NodaTime.Test
+{
+    [TestFixture]
+    public class DateIntervalTest
+    {
+        private static readonly CalendarSystem JulianCalendar = CalendarSystem.GetJulianCalendar(4);
+
+        [Test]
+        public void Construction_DifferentCalendars()
+        {
+            LocalDate start = new LocalDate(1600, 1, 1);
+            LocalDate end = new LocalDate(1800, 1, 1, JulianCalendar);
+            Assert.Throws<ArgumentException>(() => new DateInterval(start, end));
+            Assert.Throws<ArgumentException>(() => new DateInterval(start, end, true));
+        }
+
+        [Test]
+        public void Construction_EndBeforeStart()
+        {
+            LocalDate start = new LocalDate(1600, 1, 1);
+            LocalDate end = new LocalDate(1500, 1, 1);
+            Assert.Throws<ArgumentException>(() => new DateInterval(start, end));
+            Assert.Throws<ArgumentException>(() => new DateInterval(start, end, true));
+        }
+
+        [Test]
+        public void Construction_EqualStartAndEnd()
+        {
+            LocalDate start = new LocalDate(2000, 1, 1);
+            Assert.DoesNotThrow(() => new DateInterval(start, start));
+            Assert.DoesNotThrow(() => new DateInterval(start, start, true));
+        }
+
+        [Test]
+        public void Construction_DefaultToInclusive()
+        {
+            LocalDate start = new LocalDate(2000, 1, 1);
+            LocalDate end = new LocalDate(2001, 6, 19);
+            var interval = new DateInterval(start, end);
+            Assert.IsTrue(interval.Inclusive);
+        }
+
+        [Test]
+        public void Construction_Properties()
+        {
+            LocalDate start = new LocalDate(2000, 1, 1);
+            LocalDate end = new LocalDate(2001, 6, 19);
+            var interval = new DateInterval(start, end, false);
+            Assert.AreEqual(start, interval.Start);
+            Assert.AreEqual(end, interval.End);
+            Assert.IsFalse(interval.Inclusive);
+        }
+
+        [Test]
+        public void Length_Inclusive()
+        {
+            LocalDate start = new LocalDate(2000, 1, 1);
+            LocalDate end = new LocalDate(2000, 2, 10);
+            var interval = new DateInterval(start, end, true);
+            Assert.AreEqual(41, interval.Length);
+        }
+
+        [Test]
+        public void Length_Exclusive()
+        {
+            LocalDate start = new LocalDate(2000, 1, 1);
+            LocalDate end = new LocalDate(2000, 2, 10);
+            var interval = new DateInterval(start, end, false);
+            Assert.AreEqual(40, interval.Length);
+        }
+
+        [Test]
+        [TestCase("1999-12-31", false, false, TestName = "Before start")]
+        [TestCase("2000-01-01", true, true, TestName = "On start")]
+        [TestCase("2005-06-06", true, true, TestName = "In middle")]
+        [TestCase("2014-06-30", true, false, TestName = "On end")]
+        [TestCase("2014-07-01", false, false, TestName = "After end")]
+        public void Contains(string candidateText, bool expectedInclusive, bool expectedExclusive)
+        {
+            var start = new LocalDate(2000, 1, 1);
+            var end = new LocalDate(2014, 06, 30);
+            var candidate = LocalDatePattern.IsoPattern.Parse(candidateText).Value;
+            var interval = new DateInterval(start, end, true);
+            Assert.AreEqual(expectedInclusive, interval.Contains(candidate));
+            interval = new DateInterval(start, end, false);
+            Assert.AreEqual(expectedExclusive, interval.Contains(candidate));
+        }
+
+        [Test]
+        public void Contains_DifferentCalendar()
+        {
+            var start = new LocalDate(2000, 1, 1);
+            var end = new LocalDate(2014, 06, 30);
+            var interval = new DateInterval(start, end);
+            var candidate = new LocalDate(2000, 1, 1, JulianCalendar);
+            Assert.Throws<ArgumentException>(() => interval.Contains(candidate));
+        }
+    }
+}
---
+++ src/NodaTime/DateInterval.cs
@@ -0,0 +1,133 @@
+﻿// Copyright 2014 The Noda Time Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NodaTime.Annotations;
+using NodaTime.Utility;
+
+namespace NodaTime
+{
+    /// <summary>
+    /// An interval between two dates.
+    /// </summary>
+    /// <remarks>
+    /// <para>
+    /// The two dates must be in the same calendar, and the end date must not be earlier than the start date.
+    /// </para>
+    /// <para>
+    /// By default, the end date is deemed to be part of the range, as this matches many real life uses of
+    /// date ranges. For example, if someone says "I'm going to be on holiday from Monday to Friday," they
+    /// usually mean that Friday is part of their holiday. This can be configured via a constructor parameter,
+    /// as occasionally an exclusive end date can be useful. For example, to create an interval covering a
+    /// whole month, you can simply provide the first day of the month as the start and the first day of the
+    /// next month as the exclusive end.
+    /// </para>
+    /// </remarks>
+    /// <threadsafety>This type is immutable reference type. See the thread safety section of the user guide for more information.</threadsafety>
+    [Immutable]
+    public sealed class DateInterval
+    {
+        private readonly LocalDate start;
+        private readonly LocalDate end;
+        private readonly bool inclusive;
+
+        /// <summary>
+        /// Gets the start date of the interval, which is always included in the interval.
+        /// </summary>
+        /// <returns>The start date of the interval.</returns>
+        public LocalDate Start { get { return start; } }
+
+        /// <summary>
+        /// Gets the end date of the interval.
+        /// </summary>
+        /// <remarks>
+        /// Use the <see cref="Inclusive"/> property to determine whether or not the end
+        /// date is considered part of the interval.
+        /// </remarks>
+        /// <returns>The end date of the interval.</returns>
+        public LocalDate End { get { return end; } }
+
+        /// <summary>
+        /// Indicates whether or not this interval includes its end date.
+        /// </summary>
+        /// <returns>Whether or not this </returns>
+        public bool Inclusive { get { return inclusive; } }
+
+        /// <summary>
+        /// Constructs a date interval from a start date and an end date, and an indication
+        /// of whether the end date should be included in the interval.
+        /// </summary>
+        /// <param name="start">Start date of the interval</param>
+        /// <param name="end">End date of the interval</param>
+        /// <param name="inclusive"><c>true</c> to include the end date in the interval;
+        /// <c>false</c> to exclude it.
+        /// </param>
+        /// <exception cref="ArgumentException"><paramref name="end"/> is earlier than <paramref name="start"/>
+        /// or the two dates are in different calendars.
+        /// </exception>
+        /// <returns>A date interval between the specified dates, with the specified inclusivity.</returns>
+        public DateInterval(LocalDate start, LocalDate end, bool inclusive)
+        {
+            Preconditions.CheckArgument(start.Calendar.Equals(end.Calendar), "end",
+                "Calendars of start and end dates must be the same.");
+            Preconditions.CheckArgument(!(end < start), "end", "End date must not be earlier than the start date");
+            this.start = start;
+            this.end = end;
+            this.inclusive = inclusive;
+        }
+
+        /// <summary>
+        /// Constructs a date interval from a start date and an inclusive end date.
+        /// </summary>
+        /// <param name="start">Start date of the interval</param>
+        /// <param name="end">End date of the interval, inclusive</param>
+        /// <exception cref="ArgumentException"><paramref name="end"/> is earlier than <paramref name="start"/>
+        /// or the two dates are in different calendars.
+        /// </exception>
+        /// <returns>An inclusive date interval between the specified dates.</returns>
+        public DateInterval(LocalDate start, LocalDate end)
+            : this(start, end, true)
+        {
+        }
+
+        /// <summary>
+        /// Checks whether the given date is within this date interval. This requires
+        /// that the date is not earlier than the start date, and not later than the end
+        /// date. If the given date is exactly equal to the end date, it is considered
+        /// to be within the interval if and only if the interval is <see cref="Inclusive"/>.
+        /// </summary>
+        /// <param name="date">The date to check for containment within this interval.</param>
+        /// <exception cref="ArgumentException"><paramref name="date"/> is not in the same
+        /// calendar as the start and end date of this interval.</exception>
+        /// <returns><c>true</c> if <paramref name="date"/> is within this interval; <c>false</c> otherwise.</returns>
+        public bool Contains(LocalDate date)
+        {
+            Preconditions.CheckArgument(date.Calendar.Equals(start.Calendar), "date",
+                "The date to check must be in the same calendar as the start and end dates");
+            return start <= date && (inclusive ? date <= end : date < end);
+        }
+
+        /// <summary>
+        /// Gets the length of this date interval in days.
+        /// </summary>
+        /// <remarks>
+        /// The end date is included or excluded according to the <see cref="Inclusive"/>
+        /// property. For example, an inclusive interval where the start and end date are the
+        /// same has a length of 1, whereas an exclusive interval for the same dates has a
+        /// length of 0.
+        /// </remarks>
+        /// <returns>The length of this date interval in days.</returns>
+        public int Length
+        {
+            get
+            {
+                // Period.Between will give us the exclusive result, so we need to add 1
+                // if this period is inclusive.
+                return Period.Between(start, end, PeriodUnits.Days).Days + (inclusive ? 1 : 0);
+            }
+        }
+    }
+}
