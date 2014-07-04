74efa3484adde4605c0e1cf4d1c68188a2035507
Jon Skeet <skeet@pobox.com>
2014-07-04 06:59:17 +0100

    Extra tests for Nanoseconds.
    I suspect we're ready to go all-in now...

--- src/NodaTime.Test/NanosecondsTest.cs
+++ src/NodaTime.Test/NanosecondsTest.cs
@@ -1,10 +1,6 @@
 ﻿// Copyright 2014 The Noda Time Authors. All rights reserved.
 // Use of this source code is governed by the Apache License 2.0,
 // as found in the LICENSE.txt file.
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using NUnit.Framework;
 
 namespace NodaTime.Test
@@ -104,9 +100,62 @@ namespace NodaTime.Test
             Assert.AreEqual(500, nanos.NanosecondOfDay);
         }
 
-        // TODO:
-        // - Comparisons and comparison operators
-        // - Equality
-        // - + and -
+        [Test]
+        [TestCase(1, 100L, 2, 200L, 3, 300L)]
+        [TestCase(1, NodaConstants.NanosecondsPerStandardDay - 5,
+                  3, 100L,
+                  5, 95L, TestName = "Overflow")]
+        [TestCase(1, 10L,
+                  -1, NodaConstants.NanosecondsPerStandardDay - 100L,
+                  0, NodaConstants.NanosecondsPerStandardDay - 90L,
+                  TestName = "Underflow")]
+        public void Addition_Subtraction(int leftDays, long leftNanos,
+                                         int rightDays, long rightNanos,
+                                         int resultDays, long resultNanos)
+        {
+            var left = new Nanoseconds(leftDays, leftNanos);
+            var right = new Nanoseconds(rightDays, rightNanos);
+            var result = new Nanoseconds(resultDays, resultNanos);
+
+            Assert.AreEqual(result, left + right);
+            Assert.AreEqual(result, left.Plus(right));
+            Assert.AreEqual(result, Nanoseconds.Add(left, right));
+
+            Assert.AreEqual(left, result - right);
+            Assert.AreEqual(left, result.Minus(right));
+            Assert.AreEqual(left, Nanoseconds.Subtract(result, right));
+        }
+
+        [Test]
+        public void Equality()
+        {
+            var equal1 = new Nanoseconds(1, NodaConstants.NanosecondsPerHour);
+            var equal2 = Nanoseconds.FromTicks(NodaConstants.TicksPerHour * 25);
+            var different1 = new Nanoseconds(1, 200L);
+            var different2 = new Nanoseconds(2, NodaConstants.TicksPerHour);
+
+            TestHelper.TestEqualsStruct(equal1, equal2, different1);
+            TestHelper.TestOperatorEquality(equal1, equal2, different1);
+
+            TestHelper.TestEqualsStruct(equal1, equal2, different2);
+            TestHelper.TestOperatorEquality(equal1, equal2, different2);
+        }
+
+        [Test]
+        public void Comparison()
+        {
+            var equal1 = new Nanoseconds(1, NodaConstants.NanosecondsPerHour);
+            var equal2 = Nanoseconds.FromTicks(NodaConstants.TicksPerHour * 25);
+            var greater1 = new Nanoseconds(1, NodaConstants.NanosecondsPerHour + 1);
+            var greater2 = new Nanoseconds(2, 0L);
+
+            TestHelper.TestCompareToStruct(equal1, equal2, greater1);
+            TestHelper.TestNonGenericCompareTo(equal1, equal2, greater1);
+            TestHelper.TestOperatorComparisonEquality(equal1, equal2, greater1);
+
+            TestHelper.TestCompareToStruct(equal1, equal2, greater2);
+            TestHelper.TestNonGenericCompareTo(equal1, equal2, greater2);
+            TestHelper.TestOperatorComparisonEquality(equal1, equal2, greater2);
+        }
     }
 }
