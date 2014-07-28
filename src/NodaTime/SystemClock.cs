bc4e773763fdfa3d1e7b60913c6767d7c23e8ccd
Jon Skeet <skeet@pobox.com>
2014-07-28 22:18:48 +0100

    Move the code to use UTC or the system time zone for a clock into extension methods. Also, introduce ZonedClock.GetCurrentDate and ZonedClock.GetCurrentTimeOfDay, and make ZonedClock implement IClock.
    Fixes issue 302.

--- src/NodaTime/SystemClock.cs
+++ src/NodaTime/SystemClock.cs
@@ -34,48 +34,6 @@ namespace NodaTime
         }
 
         /// <summary>
-        /// Returns a <see cref="ZonedClock"/> backed by the system clock, in the UTC
-        /// time zone and the ISO calendar system.
-        /// </summary>
-        /// <returns>A <c>ZonedClock</c> in the UTC time zone and ISO calendar system,
-        /// using the system clock.</returns>
-        public static ZonedClock GetUtcInstance()
-        {
-            return new ZonedClock(instance, DateTimeZone.Utc, CalendarSystem.Iso);
-        }
-
-        /// <summary>
-        /// Returns a <see cref="ZonedClock"/> backed by the system clock, in the TZDB mapping for the
-        /// system default time zone time zone and the ISO calendar system.
-        /// </summary>
-        /// <returns>A <c>ZonedClock</c> in the system default time zone (using TZDB) and the ISO calendar system,
-        /// using the system clock.</returns>
-        /// <exception cref="DateTimeZoneNotFoundException">The system default time zone is not mapped by
-        /// TZDB.</exception>
-        /// <seealso cref="DateTimeZoneProviders.Tzdb"/>
-        public static ZonedClock GetSystemDefaultTzdbInstance()
-        {
-            var zone = DateTimeZoneProviders.Tzdb.GetSystemDefault();
-            return new ZonedClock(instance, zone, CalendarSystem.Iso);
-        }
-
-#if !PCL
-        /// <summary>
-        /// Returns a <see cref="ZonedClock"/> backed by the system clock, in the wrapper for the
-        /// BCL system default time zone time zone and the ISO calendar system.
-        /// </summary>
-        /// <remarks>The <c>DateTimeZone</c> used is a wrapper for <see cref="TimeZoneInfo.Local"/>.</remarks>
-        /// <returns>A <c>ZonedClock</c> in the system default time zone and the ISO calendar system,
-        /// using the system clock.</returns>
-        /// <seealso cref="DateTimeZoneProviders.Bcl"/>
-        public static ZonedClock GetSystemDefaultBclInstance()
-        {
-            var zone = DateTimeZoneProviders.Bcl.GetSystemDefault();
-            return new ZonedClock(instance, zone, CalendarSystem.Iso);
-        }
-#endif
-
-        /// <summary>
         /// Gets the current time as an <see cref="Instant"/>.
         /// </summary>
         /// <returns>The current time in ticks as an <see cref="Instant"/>.</returns>
