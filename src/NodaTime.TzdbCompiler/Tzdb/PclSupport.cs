3062ba0368c254cc07e61e47fa4e833725730fcc
Jon Skeet <skeet@pobox.com>
2014-10-23 07:17:21 +0100

    Fix some missing time zone translations for the PCL. There are four zones which apparently don't have a mapping in CLDR at the moment; hopefully those will be updated over time. (grafted from f9f5089ebfff339b1b791d096c0b5b32e0778d49)

--- src/NodaTime.TzdbCompiler/Tzdb/PclSupport.cs
+++ src/NodaTime.TzdbCompiler/Tzdb/PclSupport.cs
@@ -26,6 +26,20 @@ namespace NodaTime.TzdbCompiler.Tzdb
                 { "Co-ordinated Universal Time", "UTC" },
                 { "Jerusalem Standard Time", "Israel Standard Time" },
                 { "Malay Peninsula Standard Time" , "Singapore Standard Time" },
-            });
+                { "Russia TZ 1 Standard Time", "Kaliningrad Standard Time" },
+                { "Russia TZ 2 Standard Time", "Russian Standard Time" },
+                { "Russia TZ 4 Standard Time", "Ekaterinburg Standard Time" },
+                { "Russia TZ 6 Standard Time", "North Asia Standard Time" },
+                { "Russia TZ 7 Standard Time", "North Asia East Standard Time" },
+                { "Russia TZ 8 Standard Time", "Yakutsk Standard Time" },
+                { "Russia TZ 9 Standard Time", "Vladivostok Standard Time" },
+                // The following name/ID mappings give an ID which then isn't present in CLDR
+                // (at least in v26 data); these zones will have to be mapped by transitions
+                // in the PCL.
+                // { "Russia TZ 3 Standard Time", "Russia Time Zone 3" },
+                // { "Russia TZ 5 Standard Time", "N.Central Asia Standard Time" },
+                // { "Russia TZ 10 Standard Time", "Russia Time Zone 10" },
+                // { "Russia TZ 11 Standard Time", "Russia Time Zone 11" },
+                });
     }
 }
