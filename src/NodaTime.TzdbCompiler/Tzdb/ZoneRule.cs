4c30aae0bcb7ab69f5794fddcd761ddc0c9a599d
Jon Skeet <skeet@pobox.com>
2014-11-18 17:44:34 +0000

    One more expression-bodied member...

--- src/NodaTime.TzdbCompiler/Tzdb/ZoneRule.cs
+++ src/NodaTime.TzdbCompiler/Tzdb/ZoneRule.cs
@@ -120,11 +120,7 @@ namespace NodaTime.TzdbCompiler.Tzdb
         ///   <c>true</c> if the specified <see cref="System.Object" /> is equal to this instance;
         ///   otherwise, <c>false</c>.
         /// </returns>
-        public override bool Equals(object obj)
-        {
-            var rule = obj as ZoneRule;
-            return rule != null && Equals(rule);
-        }
+        public override bool Equals(object obj) => Equals(obj as ZoneRule);
 
         /// <summary>
         ///   Returns a hash code for this instance.
