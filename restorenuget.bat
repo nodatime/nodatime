02fa3b9817d9d3276c42493603111a263d5c3211
Jon Skeet <skeet@pobox.com>
2015-01-26 20:13:29 +0000

    Fix restorepackages.bat to restore all packages in all solutions.

--- restorenuget.bat
+++ restorenuget.bat
@@ -1,12 +1,10 @@
 @echo off
 
-REM TODO: detect these automatically. Probably a job for PowerShell...
+pushd src
 
-set PROJECTS=Benchmarks,CodeDiagnostics,CodeDiagnostics.Test,Demo
-set PROJECTS=%PROJECTS%,Serialization.JsonNet,Serialization.Test,Test
-set PROJECTS=%PROJECTS%,TzdbCompiler.Test,Web
-
-for %%p in (%PROJECTS%) DO (
-  echo Restoring NodaTime.%%p packages
-  nuget restore -NonInteractive -PackagesDirectory src\packages src\NodaTime.%%p\packages.config
+for %%s in (*.sln) DO (
+  echo Restoring %%s packages
+  nuget restore %%s
 )
+
+popd src
