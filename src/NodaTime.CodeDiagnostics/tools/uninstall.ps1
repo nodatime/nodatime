fb89be4559461d0a6fb5efeaaf9b152262deed28
Jon Skeet <skeet@pobox.com>
2016-01-25 19:48:43 +0000

    Remove code diagnostics - for now. May be revisited later.

--- src/NodaTime.CodeDiagnostics/tools/uninstall.ps1
+++
@@ -1,6 +0,0 @@
-﻿param($installPath, $toolsPath, $package, $project)
-
-$analyzerPath = join-path $toolsPath "analyzers"
-$analyzerFilePath = join-path $analyzerPath "NodaTime.CodeDiagnostics.dll"
-
-$project.Object.AnalyzerReferences.Remove("$analyzerFilePath")
