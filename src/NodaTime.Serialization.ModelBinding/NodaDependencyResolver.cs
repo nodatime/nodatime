15840a024227108ab3dd5499ba36f5a7218d1665
Zac Charles <zac.charles@gmail.com>
2016-02-01 12:29:27 +0200

    Add copyright headers to new files

--- src/NodaTime.Serialization.ModelBinding/NodaDependencyResolver.cs
+++ src/NodaTime.Serialization.ModelBinding/NodaDependencyResolver.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿// Copyright 2012 The Noda Time Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+using System;
 using System.Collections.Generic;
 using System.Web.Http.Dependencies;
 
--- src/NodaTime.Serialization.ModelBinding/Properties/AssemblyInfo.cs
+++ src/NodaTime.Serialization.ModelBinding/Properties/AssemblyInfo.cs
@@ -1,4 +1,8 @@
-﻿using System.Resources;
+﻿// Copyright 2012 The Noda Time Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+using System.Resources;
 using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
