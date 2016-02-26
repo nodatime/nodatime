3cdec5a97e16980e2541ffe46bfefb8b84403732
Jon Skeet <skeet@pobox.com>
2016-02-26 07:43:02 +0000

    Remove version and metadata redundancy
    Most projects now don't need AssemblyInfo, and those that have it need less of it.
    project.json keeps metadata, basically.
    
    Microsoft projects appear not to have the equivalent of -alpha in their project.json files,
    presumably specifying that only in environment variables. I propose we keep -alpha/-beta
    in the master branch, to avoid finger failures accidentally creating a full release. We could
    fairly easily create a myget feed to publish a build after each commit; we probably don't
    want to do that on nuget.org.

--- src/NodaTime/Properties/AssemblyInfo.cs
+++ src/NodaTime/Properties/AssemblyInfo.cs
@@ -3,51 +3,15 @@
 // as found in the LICENSE.txt file.
 
 using System;
-using System.Reflection;
 using System.Resources;
 using System.Runtime.CompilerServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-
-[assembly: AssemblyTitle("NodaTime")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("NodaTime")]
-[assembly: AssemblyCopyright("Copyright 2015 The Noda Time Authors. All rights reserved.")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
 #if !PCL
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: System.Runtime.InteropServices.ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: System.Runtime.InteropServices.Guid("c91ba14f-e2a9-4ed0-a501-a31b97c035fb")]
-
 // Although AllowPartiallyTrustedCallers is available in some flavours of the PCL,
 // it's not available in all of the ones we're currently targeting. See issue 268 for details.
 [assembly: System.Security.AllowPartiallyTrustedCallers]
 #endif
 
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyVersion("2.0")]
-[assembly: AssemblyFileVersion("2.0.0")]
-[assembly: AssemblyInformationalVersion("2.0.0-alpha20150718")]
 [assembly: CLSCompliant(true)]
 [assembly: NeutralResourcesLanguage("en")]
 
