4513df2a559ccf7d0183b60ea2eb7731d05d4845
Jon Skeet <skeet@pobox.com>
2016-04-15 22:10:28 +0100

    If an alias won't work, maybe an environment variable will

--- build/jenkins.sh
+++ build/jenkins.sh
@@ -6,15 +6,15 @@ cd `dirname $0`/..
 # we need to explicitly make the .cmd file available.
 # ... and for some reason, it isn't found on the path. That's just weird,
 # but let's get it building to start with...
-alias dnu=/c/Users/skeet/.dnx/runtimes/dnx-clr-win-x86.1.0.0-rc1-update2/bin/dnu.cmd
+DNU=/c/Users/skeet/.dnx/runtimes/dnx-clr-win-x86.1.0.0-rc1-update2/bin/dnu.cmd
 
-dnu restore
-dnu build src/NodaTime
-dnu build src/NodaTime.Testing
-dnu build src/NodaTime.Test
-dnu build src/NodaTime.Serialization.JsonNet
-dnu build src/NodaTime.Serialization.Test
-dnu build src/NodaTime.TzdbCompiler.Test
+$DNU restore
+$DNU build src/NodaTime
+$DNU build src/NodaTime.Testing
+$DNU build src/NodaTime.Test
+$DNU build src/NodaTime.Serialization.JsonNet
+$DNU build src/NodaTime.Serialization.Test
+$DNU build src/NodaTime.TzdbCompiler.Test
 dnx -p src/NodaTime.Serialization.Test test
 dnx -p src/NodaTime.Test test "--where=cat != Slow && cat != Overflow"
 dnx -p src/NodaTime.TzdbCompiler.Test test
