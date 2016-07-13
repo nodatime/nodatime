c68c55a84ffbf974ca46098c2e1e206b49ae674f
Jon Skeet <skeet@pobox.com>
2016-07-13 11:45:26 +0100

    Serve .nzd extensions as application/octet-stream
    This will fix issue #548 when the web site is rebuilt.

--- src/NodaTime.Web/Controllers/TzValidateController.cs
+++ src/NodaTime.Web/Controllers/TzValidateController.cs
@@ -2,6 +2,9 @@
 // Use of this source code is governed by the Apache License 2.0,
 // as found in the LICENSE.txt file.
 using Microsoft.AspNetCore.Mvc;
+using NodaTime.TimeZones;
+using NodaTime.TzValidate.NodaDump;
+using System.IO;
 using System.Net;
 
 namespace NodaTime.Web.Controllers
@@ -9,11 +12,17 @@ namespace NodaTime.Web.Controllers
     public class TzValidateController : Controller
     {
         // This will do something soon :)
-        public IActionResult Index(int startYear = 1, int endYear = 2035)
+        public IActionResult Generate(int startYear = 1, int endYear = 2035)
         {
+            var source = TzdbDateTimeZoneSource.Default;
+            var writer = new StringWriter();
+            var options = new Options { FromYear = startYear, ToYear = endYear };
+            var dumper = new ZoneDumper(source, options);
+            dumper.Dump(writer);
+
             return new ContentResult
             {
-                Content = $"Foo; start={startYear} end = {endYear}",
+                Content = writer.ToString(),
                 ContentType = "text/plain",
                 StatusCode = (int) HttpStatusCode.OK
             };
