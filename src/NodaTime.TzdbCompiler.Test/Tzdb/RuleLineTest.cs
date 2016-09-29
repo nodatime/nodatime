267544c4213ba2962a06528a6b762ce91cec2afd
Jon Skeet <skeet@pobox.com>
2016-09-29 08:01:18 +0100

    Fix name formatting for Asia/Dushanbe in 1991 March-September
    We were only applying "slash" formatting (where the format specifies
    daylight and standard time separately) for zone lines which referred to a named rule.
    Asia/Dushanbe has a zone line like this:
    
    5:00 1:00 +05/+06 1991 Sep  9  2:00s
    
    Now admittedly specifying a format of +05/+06 for a line which is fixed as daylight is a bit odd, but it should still work.

---
+++ src/NodaTime.TzdbCompiler.Test/Tzdb/RuleLineTest.cs
@@ -0,0 +1,24 @@
+// Copyright 2009 The Noda Time Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+using NodaTime.TimeZones;
+using NodaTime.TzdbCompiler.Tzdb;
+using NUnit.Framework;
+using System.Linq;
+
+namespace NodaTime.TzdbCompiler.Test.Tzdb
+{
+    public class RuleLineTest
+    {
+        [Test]
+        public void WriteRead()
+        {
+            var yearOffset = new ZoneYearOffset(TransitionMode.Utc, 10, 31, (int)IsoDayOfWeek.Wednesday, true, LocalTime.Midnight);
+            var recurrence = new ZoneRecurrence("bob", Offset.Zero, yearOffset, 1971, 2009);
+            var actual = new RuleLine(recurrence, "D", null);
+            var expected = new RuleLine(recurrence, "D", null);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
