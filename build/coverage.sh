a3f1966644d67eb611a12020850f8640ad993189
Jon Skeet <skeet@pobox.com>
2016-11-27 20:58:59 +0000

    Alternative fix for running coverage

--- build/coverage.sh
+++ build/coverage.sh
@@ -12,15 +12,13 @@ nuget install -Verbosity quiet -OutputDirectory packages -Version 2.4.5.0 Report
 dotnet restore src
 dotnet build -c Release src/NodaTime.Test
 
-# Note: need to run netcoreapp1.0 build to avoid InvalidProgramException,
-# due to RyuJIT (I think). See 
-# http://stackoverflow.com/questions/36755337
-# This does mean we don't get coverage for types not in .NET Core though.
+# Use the legacy JIT to avoid getting InvalidProgramException.
+export COMPLUS_useLegacyJit=1
 packages/OpenCover.4.6.519/tools/OpenCover.Console.exe \
   -register:user \
   -oldStyle \
   -target:"c:\Program Files\dotnet\dotnet.exe" \
-  -targetargs:"test -f netcoreapp1.0 -c Release src/NodaTime.Test -where=cat!=Slow" \
+  -targetargs:"test -f net451 -c Release src/NodaTime.Test -where=cat!=Slow" \
   -output:coverage.xml \
   -filter:"+[NodaTime]NodaTime.*" \
   -searchdirs:NodaTime/bin/Release/net451/win7-x64
