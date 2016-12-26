c6375d77aafd752deb4e6c11fbca58d4c9a25c74
Jon Skeet <skeet@pobox.com>
2016-12-26 20:48:26 +0000

    Autogenerate downloads page from GCS
    Some of this may be unnecessary; or we may well want
    to host all the markdown in ASP.NET Core at some point.

---
+++ src/NodaTime.Web/Models/IReleaseRepository.cs
@@ -0,0 +1,12 @@
+﻿// Copyright 2016 The Noda Time Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+using System.Collections.Generic;
+
+namespace NodaTime.Web.Models
+{
+    public interface IReleaseRepository
+    {
+        IList<ReleaseDownload> GetReleases();
+    }
+}
