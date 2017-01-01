8cbc882da886db83ece254d8ab85bb8f54112aeb
Jon Skeet <skeet@pobox.com>
2017-01-01 11:34:48 +0000

    Extension methods to construct Duration values.
    The overloads to accept different types (int, double, long) are
    required as (oddly?) the implicit numeric conversions appear not to
    be valid for extension methods.

---
+++ src/NodaTime.Test/Testing/Extensions/DurationConstructionTest.cs
@@ -0,0 +1,66 @@
+﻿// Copyright 2016 The Noda Time Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+using NUnit.Framework;
+using static NodaTime.Testing.Extensions.DurationConstruction;
+
+namespace NodaTime.Test.Testing.Extensions
+{
+    public class DurationConstructionTest
+    {
+        [Test]
+        public void Days()
+        {
+            Assert.AreEqual(Duration.FromDays(2), 2.Days());
+            Assert.AreEqual(Duration.FromDays(2.5), 2.5.Days());
+        }
+
+        [Test]
+        public void Hours()
+        {
+            Assert.AreEqual(Duration.FromHours(2), 2.Hours());
+            Assert.AreEqual(Duration.FromHours(2.5), 2.5.Hours());
+        }
+
+        [Test]
+        public void Minutes()
+        {
+            Assert.AreEqual(Duration.FromMinutes(2), 2.Minutes());
+            Assert.AreEqual(Duration.FromMinutes(2), 2L.Minutes());
+            Assert.AreEqual(Duration.FromMinutes(2.5), 2.5.Minutes());
+        }
+
+        [Test]
+        public void Seconds()
+        {
+            Assert.AreEqual(Duration.FromSeconds(2), 2.Seconds());
+            Assert.AreEqual(Duration.FromSeconds(2), 2L.Seconds());
+            Assert.AreEqual(Duration.FromSeconds(2.5), 2.5.Seconds());
+        }
+
+        [Test]
+        public void Milliseconds()
+        {
+            Assert.AreEqual(Duration.FromMilliseconds(2), 2.Milliseconds());
+            Assert.AreEqual(Duration.FromMilliseconds(2), 2L.Milliseconds());
+            Assert.AreEqual(Duration.FromMilliseconds(2.5), 2.5.Milliseconds());
+        }
+
+        [Test]
+        public void Ticks()
+        {
+            Assert.AreEqual(Duration.FromTicks(2), 2.Ticks());
+            Assert.AreEqual(Duration.FromTicks(2), 2L.Ticks());
+            Assert.AreEqual(Duration.FromTicks(2.5), 2.5.Ticks());
+        }
+
+        [Test]
+        public void Nanoseconds()
+        {
+            Assert.AreEqual(Duration.FromNanoseconds(2), 2.Nanoseconds());
+            Assert.AreEqual(Duration.FromNanoseconds(2), 2L.Nanoseconds());
+            Assert.AreEqual(Duration.FromNanoseconds(2.5), 2.5.Nanoseconds());
+        }
+    }
+}
---
+++ src/NodaTime.Testing/Extensions/DurationConstruction.cs
@@ -0,0 +1,145 @@
+﻿// Copyright 2016 The Noda Time Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+
+namespace NodaTime.Testing.Extensions
+{
+    /// <summary>
+    /// Extension methods for constructing <see cref="Duration"/> values.
+    /// </summary>
+    public static class DurationConstruction
+    {
+        /// <summary>
+        /// Constructs a duration with the specified number of days.
+        /// </summary>
+        /// <param name="days">The desired number of days.</param>
+        /// <returns>A duration with the specified number of days.</returns>
+        public static Duration Days(this double days) => Duration.FromDays(days);
+
+        /// <summary>
+        /// Constructs a duration with the specified number of days.
+        /// </summary>
+        /// <param name="days">The desired number of days.</param>
+        /// <returns>A duration with the specified number of days.</returns>
+        public static Duration Days(this int days) => Duration.FromDays(days);
+
+        /// <summary>
+        /// Constructs a duration with the specified number of hours.
+        /// </summary>
+        /// <param name="hours">The desired number of hours.</param>
+        /// <returns>A duration with the specified number of hours.</returns>
+        public static Duration Hours(this double hours) => Duration.FromHours(hours);
+        /// <summary>
+        /// Constructs a duration with the specified number of hours.
+        /// </summary>
+        /// <param name="hours">The desired number of hours.</param>
+        /// <returns>A duration with the specified number of hours.</returns>
+        public static Duration Hours(this int hours) => Duration.FromHours(hours);
+
+        /// <summary>
+        /// Constructs a duration with the specified number of minutes.
+        /// </summary>
+        /// <param name="minutes">The desired number of minutes.</param>
+        /// <returns>A duration with the specified number of minutes.</returns>
+        public static Duration Minutes(this double minutes) => Duration.FromMinutes(minutes);
+
+        /// <summary>
+        /// Constructs a duration with the specified number of minutes.
+        /// </summary>
+        /// <param name="minutes">The desired number of minutes.</param>
+        /// <returns>A duration with the specified number of minutes.</returns>
+        public static Duration Minutes(this int minutes) => Duration.FromMinutes(minutes);
+
+        /// <summary>
+        /// Constructs a duration with the specified number of minutes.
+        /// </summary>
+        /// <param name="minutes">The desired number of minutes.</param>
+        /// <returns>A duration with the specified number of minutes.</returns>
+        public static Duration Minutes(this long minutes) => Duration.FromMinutes(minutes);
+
+        /// <summary>
+        /// Constructs a duration with the specified number of seconds.
+        /// </summary>
+        /// <param name="seconds">The desired number of seconds.</param>
+        /// <returns>A duration with the specified number of seconds.</returns>
+        public static Duration Seconds(this double seconds) => Duration.FromSeconds(seconds);
+
+        /// <summary>
+        /// Constructs a duration with the specified number of seconds.
+        /// </summary>
+        /// <param name="seconds">The desired number of seconds.</param>
+        /// <returns>A duration with the specified number of seconds.</returns>
+        public static Duration Seconds(this int seconds) => Duration.FromSeconds(seconds);
+
+        /// <summary>
+        /// Constructs a duration with the specified number of seconds.
+        /// </summary>
+        /// <param name="seconds">The desired number of seconds.</param>
+        /// <returns>A duration with the specified number of seconds.</returns>
+        public static Duration Seconds(this long seconds) => Duration.FromSeconds(seconds);
+
+        /// <summary>
+        /// Constructs a duration with the specified number of milliseconds.
+        /// </summary>
+        /// <param name="milliseconds">The desired number of milliseconds.</param>
+        /// <returns>A duration with the specified number of milliseconds.</returns>
+        public static Duration Milliseconds(this double milliseconds) => Duration.FromMilliseconds(milliseconds);
+
+        /// <summary>
+        /// Constructs a duration with the specified number of milliseconds.
+        /// </summary>
+        /// <param name="milliseconds">The desired number of milliseconds.</param>
+        /// <returns>A duration with the specified number of milliseconds.</returns>
+        public static Duration Milliseconds(this int milliseconds) => Duration.FromMilliseconds(milliseconds);
+
+        /// <summary>
+        /// Constructs a duration with the specified number of milliseconds.
+        /// </summary>
+        /// <param name="milliseconds">The desired number of milliseconds.</param>
+        /// <returns>A duration with the specified number of milliseconds.</returns>
+        public static Duration Milliseconds(this long milliseconds) => Duration.FromMilliseconds(milliseconds);
+
+        /// <summary>
+        /// Constructs a duration with the specified number of ticks.
+        /// </summary>
+        /// <param name="ticks">The desired number of ticks.</param>
+        /// <returns>A duration with the specified number of ticks.</returns>
+        public static Duration Ticks(this double ticks) => Duration.FromTicks(ticks);
+
+        /// <summary>
+        /// Constructs a duration with the specified number of ticks.
+        /// </summary>
+        /// <param name="ticks">The desired number of ticks.</param>
+        /// <returns>A duration with the specified number of ticks.</returns>
+        public static Duration Ticks(this int ticks) => Duration.FromTicks(ticks);
+
+        /// <summary>
+        /// Constructs a duration with the specified number of ticks.
+        /// </summary>
+        /// <param name="ticks">The desired number of ticks.</param>
+        /// <returns>A duration with the specified number of ticks.</returns>
+        public static Duration Ticks(this long ticks) => Duration.FromTicks(ticks);
+
+        /// <summary>
+        /// Constructs a duration with the specified number of nanoseconds.
+        /// </summary>
+        /// <param name="nanoseconds">The desired number of nanoseconds.</param>
+        /// <returns>A duration with the specified number of nanoseconds.</returns>
+        public static Duration Nanoseconds(this double nanoseconds) => Duration.FromNanoseconds(nanoseconds);
+
+        /// <summary>
+        /// Constructs a duration with the specified number of nanoseconds.
+        /// </summary>
+        /// <param name="nanoseconds">The desired number of nanoseconds.</param>
+        /// <returns>A duration with the specified number of nanoseconds.</returns>
+        public static Duration Nanoseconds(this int nanoseconds) => Duration.FromNanoseconds(nanoseconds);
+
+        /// <summary>
+        /// Constructs a duration with the specified number of nanoseconds.
+        /// </summary>
+        /// <param name="nanoseconds">The desired number of nanoseconds.</param>
+        /// <returns>A duration with the specified number of nanoseconds.</returns>
+        public static Duration Nanoseconds(this long nanoseconds) => Duration.FromNanoseconds(nanoseconds);
+    }
+}
