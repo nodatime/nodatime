0f3be860425e12e83d180762a381ba6a861bf1a3
Jon Skeet <skeet@pobox.com>
2017-01-19 14:09:15 +0000

    First pass at a public composite pattern
    This is designed to provide a way of fixing #607.
    
    It's currently internal so we can merge it; to fix #607 we'll
    need to make it public.

---
+++ src/NodaTime.Test/Text/CompositePatternBuilderPatternTest.cs
@@ -0,0 +1,65 @@
+﻿// Copyright 2017 The Noda Time Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+using NodaTime.Text;
+using NUnit.Framework;
+using System;
+
+namespace NodaTime.Test.Text
+{
+    public class CompositePatternBuilderPatternTest
+    {
+        // See https://github.com/nodatime/nodatime/issues/607
+        [Test]
+        [TestCase("2017-02-23T16:40:50.123456789")]
+        [TestCase("2017-02-23T16:40:50.123")]
+        [TestCase("2017-02-23T16:40:50")]
+        [TestCase("2017-02-23T16:40")]
+        public void IsoPattern(string text)
+        {
+            // We assert that the text round-trips. If it does, it's
+            // reasonable to assume it parsed correctly...
+            var shortPattern = LocalDateTimePattern.CreateWithInvariantCulture("uuuu'-'MM'-'dd'T'HH':'mm");
+            var pattern = new CompositePatternBuilder<LocalDateTime>
+            {
+                { LocalDateTimePattern.ExtendedIso, _ => true },
+                { shortPattern, ldt => ldt.Second == 0 && ldt.NanosecondOfSecond == 0 }
+            }.Build();
+            var value = pattern.Parse(text).Value;
+            string formatted = pattern.Format(value);
+            Assert.AreEqual(text, formatted);
+        }
+
+        [Test]
+        public void Format_NoValidPattern()
+        {
+            var pattern = new CompositePatternBuilder<LocalDate>
+            {
+                { LocalDatePattern.Iso, _ => false },
+                { LocalDatePattern.CreateWithInvariantCulture("yyyy"), _ => false },
+            }.Build();
+            Assert.Throws<FormatException>(() => pattern.Format(new LocalDate(2017, 1, 1)));
+        }
+
+        [Test]
+        public void Parse()
+        {
+            var pattern = new CompositePatternBuilder<LocalDate>
+            {
+                { LocalDatePattern.Iso, _ => true },
+                { LocalDatePattern.CreateWithInvariantCulture("yyyy"), _ => false },
+            }.Build();
+            Assert.IsTrue(pattern.Parse("2017-03-20").Success);
+            Assert.IsFalse(pattern.Parse("2017-03").Success);
+            Assert.IsTrue(pattern.Parse("2017").Success);
+        }
+
+        [Test]
+        public void Build_Empty()
+        {
+            var pattern = new CompositePatternBuilder<LocalDate>();
+            Assert.Throws<InvalidOperationException>(() => pattern.Build());
+        }
+    }
+}
---
+++ src/NodaTime/Text/CompositePatternBuilder.cs
@@ -0,0 +1,126 @@
+﻿// Copyright 2017 The Noda Time Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+using JetBrains.Annotations;
+using NodaTime.Utility;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NodaTime.Text
+{
+    /// <summary>
+    /// A builder for composite patterns.
+    /// </summary>
+    /// <remarks>
+    /// A composite pattern is a combination of multiple patterns. When parsing, these are checked
+    /// in the order in which they are added to the builder with the <see cref="Add(IPattern{T}, Func{T, bool})"/>
+    /// method, by trying to parse and seeing if the result is a successful one. When formatting,
+    /// the patterns are checked in the reverse order, using the predicate provided along with the pattern
+    /// when calling <c>Add</c>. The intention is that patterns are added in "most precise first" order,
+    /// and the predicate should indicate whether it can fully represent the given value - so the "less precise"
+    /// (and therefore usually shorter) pattern can be used first.
+    /// </remarks>
+    /// <typeparam name="T">The type of value to be parsed or formatted by the resulting pattern.</typeparam>
+    internal sealed class CompositePatternBuilder<T> : IEnumerable<IPattern<T>>
+    {
+        private readonly List<IPattern<T>> patterns = new List<IPattern<T>>();
+        private readonly List<Func<T, bool>> formatPredicates = new List<Func<T, bool>>();
+
+        /// <summary>
+        /// Adds a component pattern to this builder.
+        /// </summary>
+        /// <param name="pattern">The component pattern to use as part of the eventual composite pattern.</param>
+        /// <param name="formatPredicate">A predicate to determine whether or not this pattern is suitable for
+        /// formatting the given value.</param>
+        public void Add(IPattern<T> pattern, Func<T, bool> formatPredicate)
+        {
+            patterns.Add(Preconditions.CheckNotNull(pattern, nameof(pattern)));
+            formatPredicates.Add(Preconditions.CheckNotNull(formatPredicate, nameof(formatPredicate)));
+        }
+
+        /// <summary>
+        /// Builds a composite pattern from this builder. Further changes to this builder
+        /// will have no impact on the returned pattern.
+        /// </summary>
+        /// <exception cref="InvalidOperationException">No component patterns have been added.</exception>
+        /// <returns>A pattern using the patterns added to this builder.</returns>
+        public IPattern<T> Build()
+        {
+            Preconditions.CheckState(patterns.Count != 0, "A composite pattern must have at least one component pattern.");
+            return new CompositePattern(patterns, formatPredicates);
+        }
+
+        internal IPartialPattern<T> BuildAsPartial()
+        {
+            Preconditions.DebugCheckState(patterns.All(p => p is IPartialPattern<T>), "All patterns should be partial");
+            return (IPartialPattern<T>) Build();
+        }
+
+        /// <inheritdoc />
+        IEnumerator<IPattern<T>> IEnumerable<IPattern<T>>.GetEnumerator() => patterns.GetEnumerator();
+        /// <inheritdoc />
+        IEnumerator IEnumerable.GetEnumerator() => patterns.GetEnumerator();
+
+        private sealed class CompositePattern : IPartialPattern<T>
+        {
+            private readonly IPattern<T>[] patterns;
+            private readonly Func<T, bool>[] formatPredicates;
+
+            internal CompositePattern(List<IPattern<T>> patterns, List<Func<T, bool>> formatPredicates)
+            {
+                this.patterns = patterns.ToArray();
+                this.formatPredicates = formatPredicates.ToArray();
+            }
+
+            public ParseResult<T> Parse(string text)
+            {
+                foreach (IPattern<T> pattern in patterns)
+                {
+                    ParseResult<T> result = pattern.Parse(text);
+                    if (result.Success || !result.ContinueAfterErrorWithMultipleFormats)
+                    {
+                        return result;
+                    }
+                }
+                return ParseResult<T>.NoMatchingFormat(new ValueCursor(text));
+            }
+
+            public ParseResult<T> ParsePartial(ValueCursor cursor)
+            {
+                int index = cursor.Index;
+                foreach (IPartialPattern<T> pattern in patterns)
+                {
+                    cursor.Move(index);
+                    ParseResult<T> result = pattern.ParsePartial(cursor);
+                    if (result.Success || !result.ContinueAfterErrorWithMultipleFormats)
+                    {
+                        return result;
+                    }
+                }
+                cursor.Move(index);
+                return ParseResult<T>.NoMatchingFormat(cursor);
+            }
+
+            public string Format(T value) => FindFormatPattern(value).Format(value);
+
+            public StringBuilder AppendFormat(T value, [NotNull] StringBuilder builder) =>
+                FindFormatPattern(value).AppendFormat(value, builder);
+
+            private IPattern<T> FindFormatPattern(T value)
+            {
+                for (int i = formatPredicates.Length - 1; i >= 0; i--)
+                {
+                    if (formatPredicates[i](value))
+                    {
+                        return patterns[i];
+                    }
+                }
+                throw new FormatException("Composite pattern was unable to format value using any of the provided patterns.");
+            }
+        }
+    }
+}
