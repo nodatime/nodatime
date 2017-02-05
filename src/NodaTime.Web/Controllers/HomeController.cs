06fa3559b2ef1961004804e031d5aa3c1847da39
Jon Skeet <skeet@pobox.com>
2017-02-05 09:00:19 +0000

    Majority of rewrite of NodaTime.Web
    - Include all the user guides and developer guides as Markdown
    - Parse the Markdown on first request
    - Serve API documentation from a "docfx" directory under NodaTime.Web
      (to be built fresh before deployment)
    
    Work originally used MarkdownViewEngine, but it's now simpler to
    have our own controller which loads the Markdown.
    
    The "no live updates, parse upfront" aspect is suboptimal, but for
    Noda Time it's fine.

---
+++ src/NodaTime.Web/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NodaTime.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
