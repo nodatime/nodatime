006aaaaff2f7474cf19c524523f9f8e0eaf4f78a
Jon Skeet <skeet@pobox.com>
2017-03-21 20:35:03 +0000

    Modify TZDB script post-csproj changes

--- build/updatetzdb.sh
+++ build/updatetzdb.sh
@@ -9,34 +9,34 @@ then
   exit 1
 fi
 
-SRCDIR=../src
-OUTPUT=$SRCDIR/NodaTime/TimeZones/Tzdb.nzd
-DATADIR=../data
-WWWDIR=../src/NodaTime.Web/wwwroot
+declare -r SRCDIR=../src
+declare -r OUTPUT=$SRCDIR/NodaTime/TimeZones/Tzdb.nzd
+declare -r DATADIR=../data
+declare -r WWWDIR=../src/NodaTime.Web/wwwroot
+declare -r PROJECTS="NodaTime NodaTime.TzdbCompiler NodaTime.TzValidate.NodaDump NodaTime.TzValidate.NzdCompatibility"
 
-dotnet restore -v Error $SRCDIR
-dotnet build $SRCDIR/{NodaTime,NodaTime.TzdbCompiler,NodaTime.TzValidate.NodaDump,NodaTime.TzValidate.NzdCompatibility} \
-  > /dev/null
+for proj in $PROJECTS
+do
+  dotnet restore -v quiet $SRCDIR/$proj
+  dotnet build $SRCDIR/$proj
+done
 
-dotnet run -p $SRCDIR/NodaTime.TzdbCompiler -- \
+dotnet run -p $SRCDIR/NodaTime.TzdbCompiler/*.csproj -- \
   -o $OUTPUT \
   -s http://www.iana.org/time-zones/repository/releases/tzdata$1.tar.gz \
   -w $DATADIR/cldr \
   | grep -v "Skipping"
-  
-dotnet build $SRCDIR/{NodaTime,NodaTime.TzdbCompiler,NodaTime.TzValidate.NodaDump,NodaTime.TzValidate.NzdCompatibility} \
-  > /dev/null
   
 echo ""
 
 echo Hash on github pages:
 wget -q -O - http://nodatime.github.io/tzvalidate/tzdata$1-sha256.txt 2> /dev/null
 echo Hash from new file:
-dotnet run -p $SRCDIR/NodaTime.TzValidate.NodaDump -- -s $OUTPUT --hash | grep -v "Skipping"
+dotnet run -p $SRCDIR/NodaTime.TzValidate.NodaDump/*.csproj -- -s $OUTPUT --hash | grep -v "Skipping"
 echo Hash from new file without abbreviations:
-dotnet run -p $SRCDIR/NodaTime.TzValidate.NodaDump -- -s $OUTPUT --hash --noabbr | grep -v "Skipping"
+dotnet run -p $SRCDIR/NodaTime.TzValidate.NodaDump/*.csproj -- -s $OUTPUT --hash --noabbr | grep -v "Skipping"
 echo Hash from new file without abbreviations, using Noda Time 1.1:
-dotnet run -p $SRCDIR/NodaTime.TzValidate.NzdCompatibility -- -s $OUTPUT --hash --noabbr | grep -v "Skipping"
+dotnet run -p $SRCDIR/NodaTime.TzValidate.NzdCompatibility/*.csproj -- -s $OUTPUT --hash --noabbr | grep -v "Skipping"
 
 echo ""
 echo "When you're ready, update Google Cloud Storage:"
