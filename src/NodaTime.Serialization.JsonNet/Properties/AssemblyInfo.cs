83990cde5d4d1ee3cecfb613962939589af4a375
Jon Skeet <skeet@pobox.com>
2017-03-24 14:11:35 +0000

    Remove serialization code
    This is now in https://github.com/nodatime/nodatime.serialization
    
    Fixes #436.

--- src/NodaTime.Serialization.JsonNet/Properties/AssemblyInfo.cs
+++
@@ -1,9 +0,0 @@
-// Copyright 2012 The Noda Time Authors. All rights reserved.
-// Use of this source code is governed by the Apache License 2.0,
-// as found in the LICENSE.txt file.
-
-using System;
-using System.Resources;
-
-[assembly: CLSCompliant(true)]
-[assembly: NeutralResourcesLanguage("en")]
