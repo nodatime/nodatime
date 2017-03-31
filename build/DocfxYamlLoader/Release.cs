77d8d037faaea25356126ea9cdf6c70170ac0a93
Jon Skeet <skeet@pobox.com>
2017-03-31 20:48:35 +0100

    First pass at generating API diffs
    Part of #732.

---
+++ build/DocfxYamlLoader/Release.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace DocfxYamlLoader
+{
+    public class Release
+    {
+        public string Version { get; }
+        public List<DocfxMember> Members { get; }
+        public Dictionary<string, DocfxMember> MembersByUid { get; }
+
+        private Release(string version, List<DocfxMember> members)
+        {
+            Version = version;
+            Members = members;
+            MembersByUid = Members.ToDictionary(m => m.Uid);
+            foreach (var member in members)
+            {
+                if (member.Parent != null)
+                {
+                    member.ParentMember = MembersByUid[member.Parent];
+                }
+            }
+        }
+
+        public static Release Load(string directory, string version)
+        {
+            var serializer = new DeserializerBuilder()
+                .WithNamingConvention(new CamelCaseNamingConvention())
+                .IgnoreUnmatchedProperties()
+                .Build();
+            var members = Directory.EnumerateFiles(directory, "*.yml", SearchOption.AllDirectories)
+                .Where(f => Path.GetFileName(f) != "toc.yml")
+                .Select(f => File.ReadAllText(f))
+                .Select(yaml => serializer.Deserialize<DocfxYamlFile>(yaml))
+                .SelectMany(f => f.Items)
+                .ToList();
+            return new Release(version, members);
+        }
+
+        public class DocfxYamlFile
+        {
+            public List<DocfxMember> Items { get; set; }
+        }
+    }
+}
---
+++ build/ReleaseDiffGenerator/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.IO;
+using DocfxYamlLoader;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ReleaseDiffGenerator
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Usage: ReleaseDiffGenerator <old release directory> <new release directory>");
+                return 1;
+            }
+
+            var oldRelease = Release.Load(args[0], Path.GetFileName(args[0]));
+            var newRelease = Release.Load(args[1], Path.GetFileName(args[1]));
+
+
+            var oldMemberUids = new HashSet<string>(oldRelease.MembersByUid.Keys);
+            var newMemberUids = new HashSet<string>(newRelease.MembersByUid.Keys);
+
+            var addedMembers = newMemberUids
+                .Except(oldMemberUids)
+                .OrderBy(uid => uid)
+                .Select(uid => newRelease.MembersByUid[uid])
+                // Don't include members where the parent is also new
+                .Where(m => m.Parent == null || oldMemberUids.Contains(m.Parent))
+                .ToList();
+            var removedMembers = oldMemberUids
+                .Except(newMemberUids)
+                .OrderBy(uid => uid)
+                .Select(uid => oldRelease.MembersByUid[uid])
+                // Don't include members where the parent was also removed
+                .Where(m => m.Parent == null || newMemberUids.Contains(m.Parent))
+                .ToList();
+
+            // TODO:
+            // - Linking of removed items (can't be a normal link, as it has to be to previous version)
+            // - Display text of link
+            // - Grouping by type (only when there's more than one item for the type? Unsure)
+            // - Investigate warnings given by docfx of invalid UIDs.
+            using (var writer = File.CreateText(Path.Combine(args[1], "api", "changes.md")))
+            {
+                writer.WriteLine($"# API changes from {oldRelease.Version} to {newRelease.Version}");
+                writer.WriteLine();
+
+                writer.WriteLine($"## Added in {newRelease.Version}");
+                writer.WriteLine();
+                foreach (var member in addedMembers)
+                {
+                    writer.WriteLine($"- [{member.Uid}](xref:{member.Uid}) ({member.Type})");
+                }
+                writer.WriteLine();
+
+                writer.WriteLine($"## Removed in {newRelease.Version} (present in {oldRelease.Version})");
+                writer.WriteLine();
+                foreach (var member in removedMembers)
+                {
+                    writer.WriteLine($"- {member.Uid} ({member.Type})");
+                }
+            }
+
+            var tocFile = Path.Combine(args[1], "api", "toc.yml");
+            var toc = File.ReadAllLines(tocFile).ToList();
+            if (!toc[1].StartsWith("- name: Changes"))
+            {
+                toc.Insert(1, $"- name: Changes from {oldRelease.Version}");
+                toc.Insert(2, "  href: changes.md");
+            }
+            File.WriteAllLines(tocFile, toc);
+
+            return 0;
+        }
+    }
+}
