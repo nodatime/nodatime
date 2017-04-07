a1b6b0cbc8ed3c6544a13921ebe774b1a0372b26
Jon Skeet <skeet@pobox.com>
2017-04-07 17:50:53 +0100

    Start generating an "availability" annotation for platforms
    It might make sense to only do this at a type level - but let's
    leave it for now and revisit.

--- build/DocfxAnnotationGenerator/Program.cs
+++ build/DocfxAnnotationGenerator/Program.cs
@@ -8,32 +8,43 @@ namespace DocfxAnnotationGenerator
 {
     class Program
     {
+        private static readonly string[] packages = { "NodaTime", "NodaTime.Testing", "NodaTime.Serialization.JsonNet" };
+        private static readonly string[] unstableFrameworks = { "net45", "netstandard1.3" };
+
         private readonly IEnumerable<Release> releases;
+        private readonly Dictionary<string, List<BuildAssembly>> reflectionDataByVersion;
         private readonly string docfxRoot;
+        private readonly string historyRoot;
+        private readonly string srcRoot;
 
         private static int Main(string[] args)
         {
-            if (args.Length < 2)
+            if (args.Length < 4)
             {
-                Console.WriteLine("Arguments: <docfx root> <version1> <version2>");
+                Console.WriteLine("Arguments: <docfx root> <history root> <src root> <version1> <version2> ...");
                 Console.WriteLine("The docfx root dir should contain the obj directory");
+                Console.WriteLine("The history root dir should contain the nuget packages (as NodaTime-1.0.x.nupkg etc)");
+                Console.WriteLine("The src root dir should contain the 'unstable' code");
                 return 1;
             }
 
-            var releases = args.Skip(1)
-                .Select(arg => Release.Load(Path.Combine(args[0], "obj", arg), arg))
-                .ToList();
 
-            var instance = new Program(releases, args[0]);
+            var instance = new Program(args.Skip(3), args[0], args[1], args[2]);
             instance.CreateDirectories();
             instance.WriteSinceAnnotations();
+            instance.WriteAvailabilityAnnotations();
             return 0;
         }
 
-        private Program(IEnumerable<Release> releases, string docfxRoot)
+        private Program(IEnumerable<string> versions, string docfxRoot, string historyRoot, string srcRoot)
         {
-            this.releases = releases;
             this.docfxRoot = docfxRoot;
+            this.historyRoot = historyRoot;
+            this.srcRoot = srcRoot;
+            Console.WriteLine("Loading docfx metadata");
+            releases = versions.Select(v => Release.Load(Path.Combine(docfxRoot, "obj", v), v)).ToList();
+            Console.WriteLine("Loading assemblies");
+            reflectionDataByVersion = versions.ToDictionary(v => v, v => LoadAssemblies(v, historyRoot, srcRoot).ToList());
         }
 
         private void CreateDirectories()
@@ -44,8 +55,40 @@ namespace DocfxAnnotationGenerator
             }
         }
 
+        private void WriteAvailabilityAnnotations()
+        {
+            Console.WriteLine("Generating 'availability' annotations");
+            foreach (var release in releases)
+            {
+                var assemblies = reflectionDataByVersion[release.Version];
+                // I'm sure there's a cleaner way of doing this, but it should work...
+                var frameworksByUid = 
+                    assemblies.SelectMany(asm => asm.Members.Select(mem => new { Uid = mem.DocfxUid, Framework = asm.TargetFramework }))
+                              .ToLookup(pair => pair.Uid, pair => pair.Framework);
+                var file = Path.Combine(GetOverwriteDirectory(release), "availability.md");
+                using (var writer = File.CreateText(file))
+                {
+                    foreach (var uid in release.Members.Where(m => m.Type != DocfxMember.TypeKind.Namespace).Select(m => m.Uid))
+                    {
+                        string availability = string.Join(", ", frameworksByUid[uid].OrderBy(f => f));
+                        if (availability == "")
+                        {
+                            // We can refine this later...
+                            throw new Exception($"No reflection metadata for {uid}");
+                        }
+                        writer.WriteLine("---");
+                        writer.WriteLine($"uid: {uid}");
+                        writer.WriteLine($"availability: '{availability}'");
+                        writer.WriteLine("---");
+                        writer.WriteLine();
+                    }
+                }
+            }
+        }
+
         private void WriteSinceAnnotations()
         {
+            Console.WriteLine("Generating 'since' annotations");
             var uidsToVersions = new Dictionary<string, string>();
 
             foreach (Release release in releases)
@@ -79,5 +122,26 @@ namespace DocfxAnnotationGenerator
 
         private string GetOverwriteDirectory(Release release)
             => Path.Combine(docfxRoot, "obj", release.Version, "overwrite");
+
+        private static IEnumerable<BuildAssembly> LoadAssemblies(string version, string historyRoot, string srcRoot)
+        {
+            if (version == "unstable")
+            {
+                return from package in packages
+                       from framework in unstableFrameworks
+                       let file = $"{package}.dll"
+                       select BuildAssembly.Load(framework, file, Path.Combine(srcRoot, package, "bin", "Debug", framework, file));
+            }
+            else
+            {
+                return packages
+                    .Select(p => Path.Combine(historyRoot, $"{p}-{version}.nupkg"))
+                    .Where(file => File.Exists(file))
+                    .Select(file => NuGetPackage.Load(file))
+                    .SelectMany(pkg => pkg.Assemblies);
+            }
+        }
+
+
     }
 }
---
+++ build/Sandbox/Program.cs
@@ -0,0 +1,42 @@
+﻿using DocfxAnnotationGenerator;
+using DocfxYamlLoader;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace Sandbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var stream = File.OpenRead(@"c:\users\jon\test\Projects\nodatime\build\tmp\docfx\build\src\NodaTime.Serialization.JsonNet\bin\Debug\net45\NodaTime.Serialization.JsonNet.dll"))
+            {
+                var cecilUids = ReflectionMember.Load(stream).Select(m => m.DocfxUid).ToList();
+                var release = Release.Load(@"c:\users\jon\test\projects\nodatime\build\tmp\docfx\obj\2.0.x", "2.0.x");
+                var realUids = release.Members
+                    .Where(m => m.Type != DocfxMember.TypeKind.Namespace)
+                    .Where(m => m.Uid.StartsWith("NodaTime.Serialization"))
+                    .Where(m => !m.Uid.StartsWith("NodaTime.Testing"))
+                    .Select(m => m.Uid).ToList();
+
+                Console.WriteLine($"UIDs from Cecil: {cecilUids.Count}");
+                Console.WriteLine($"UIDs from Docfx: {realUids.Count}");
+                Console.WriteLine();
+                PrintUids("UIDs only in Cecil", cecilUids.Except(realUids));
+                Console.WriteLine();
+                PrintUids("UIDs only in Docfx", realUids.Except(cecilUids));
+            }
+        }
+
+        static void PrintUids(string title, IEnumerable<string> uids)
+        {
+            Console.WriteLine($"{title} ({uids.Count()})");
+            foreach (var uid in uids)
+            {
+                Console.WriteLine(uid);
+            }
+        }
+    }
+}
