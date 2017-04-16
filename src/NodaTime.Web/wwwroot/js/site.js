29b765faf8a3351f06ee918b5aedb1c16058aa5f
Malcolm Rowe <malcolm.rowe@gmail.com>
2017-04-16 15:09:25 +0100

    Fold the remaining scripts and stylesheets into the minified bundle.

--- src/NodaTime.Web/wwwroot/js/site.js
+++ src/NodaTime.Web/wwwroot/js/site.js
@@ -1,1 +1,16 @@
-﻿// Write your Javascript code.
+// Copyright 2017 The Noda Time Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+// Initialise Foundation.
+$(document).foundation();
+
+// Load the NuGet button.
+(function () {
+  var nb = document.createElement('script'); nb.type = 'text/javascript'; nb.async = true;
+  nb.src = 'http://s.prabir.me/nuget-button/0.2.1/nuget-button.min.js';
+  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(nb, s);
+})();
+
+// Initialise highlight.js.
+hljs.initHighlightingOnLoad();
