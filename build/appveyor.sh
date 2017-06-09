68937ff244dcf92cbaadfab5ea6f0970ca56a0d6
Jon Skeet <skeet@pobox.com>
2017-06-09 16:44:10 +0100

    Build more projects in CI
    In AppVeyor we can build all of NodaTime-All.sln
    
    In Travis it's harder, as building the solution will fail due to the
    desktop frameworks not being available.

--- build/appveyor.sh
+++ build/appveyor.sh
@@ -5,15 +5,14 @@
 
 cd $(dirname $0)/..
 
+dotnet restore src/NodaTime-All.sln
+dotnet build -c Release src/NodaTime-All.sln
+
 if [ -n "$COVERALLS_REPO_TOKEN" ]
 then
   build/coverage.sh
   packages/coveralls.net.0.7.0/tools/csmacnz.Coveralls.exe --opencover -i coverage.xml --useRelativePaths
 else
-  # Just do the build and test instead...
-  dotnet restore src/NodaTime
-  dotnet restore src/NodaTime.Test
-  dotnet build -c Release src/NodaTime
-  dotnet build -c Release src/NodaTime.Test
+  # Just run tests instead...
   dotnet run -c Release -f net451 -p src/NodaTime.Test/*.csproj -- --where=cat!=Slow
 fi
--- build/coverage.sh
+++ build/coverage.sh
@@ -2,16 +2,14 @@
 
 set -e
 
+# Note: this script assumes everything has already been built
+
 # cd to repository root
 cd $(dirname $0)/..
 
 nuget install -Verbosity quiet -OutputDirectory packages -Version 4.6.519 OpenCover
 nuget install -Verbosity quiet -OutputDirectory packages -Version 0.7.0 coveralls.net
 nuget install -Verbosity quiet -OutputDirectory packages -Version 2.4.5.0 ReportGenerator
-
-dotnet restore src/NodaTime
-dotnet restore src/NodaTime.Test
-dotnet build -c Release src/NodaTime.Test
 
 # Use the legacy JIT to avoid getting InvalidProgramException.
 export COMPLUS_useLegacyJit=1
