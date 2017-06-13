03cab7a07d2c08c1d81df8704b014f73aedbbfb5
Jon Skeet <skeet@pobox.com>
2017-06-13 14:37:43 +0100

    Migration to 2.0 for IClock
    - Introduce IClock.GetCurrentInstant() extension method
    - Make IClock.Now obsolete
    - Introduce ZonedClock
    - Introduce ClockExtensions to obtain ZonedClock instances
    
    Although ZonedClock is effectively a new feature, introducing it now will allow users to make a single set of changes ready for 2.0

---
+++ src/NodaTime/ClockExtensions.cs
@@ -0,0 +1,25 @@
+﻿// Copyright 2017 The Noda Time Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+using NodaTime.Utility;
+
+namespace NodaTime
+{
+    /// <summary>
+    /// Extension methods for <see cref="IClock"/> to enable migration to Noda Time 2.0.
+    /// </summary>
+    public static class ClockExtensions
+    {
+        /// <summary>
+        /// Gets the current <see cref="Instant"/> on the time line according to <paramref name="clock"/>.
+        /// </summary>
+        /// <remarks>This extension method in 1.4 is an instance method in 2.0, replacing the
+        /// <see cref="IClock.Now"/> property.</remarks>
+        /// <returns>The current instant on the time line according to this clock.</returns>
+        public static Instant GetCurrentInstant(this IClock clock) =>
+#pragma warning disable CS0618 // Type or member is obsolete
+            Preconditions.CheckNotNull(clock, nameof(clock)).Now;
+#pragma warning restore CS0618 // Type or member is obsolete
+    }
+}
---
+++ src/NodaTime/ZonedClock.cs
@@ -0,0 +1,92 @@
+﻿// Copyright 2014 The Noda Time Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+using JetBrains.Annotations;
+using NodaTime.Annotations;
+using NodaTime.Utility;
+
+namespace NodaTime
+{
+    /// <summary>
+    /// A clock with an associated time zone and calendar. This is effectively a convenience
+    /// class decorating an <see cref="IClock"/>.
+    /// </summary>
+    /// <threadsafety>This type is immutable reference type. See the thread safety section of the user guide for more information.</threadsafety>
+    [Immutable]
+    public sealed class ZonedClock : IClock
+    {
+        private readonly IClock clock;
+        private readonly DateTimeZone zone;
+        private readonly CalendarSystem calendar;
+
+        /// <summary>
+        /// Creates a new <see cref="ZonedClock"/> with the given clock, time zone and calendar system.
+        /// </summary>
+        /// <param name="clock">Clock to use to obtain instants.</param>
+        /// <param name="zone">Time zone to adjust instants into.</param>
+        /// <param name="calendar">Calendar system to use.</param>
+        public ZonedClock([NotNull] IClock clock, [NotNull] DateTimeZone zone, [NotNull] CalendarSystem calendar)
+        {
+            this.clock = Preconditions.CheckNotNull(clock, nameof(clock));
+            this.zone = Preconditions.CheckNotNull(zone, nameof(zone));
+            this.calendar = Preconditions.CheckNotNull(calendar, nameof(calendar));
+        }
+
+        /// <summary>
+        /// Explicit interface implementation of <see cref="IClock.Now"/>; use
+        /// <see cref="GetCurrentInstant()"/> in preference.
+        /// </summary>
+        Instant IClock.Now => GetCurrentInstant();
+
+        /// <summary>
+        /// Returns the current instant provided by the underlying clock.
+        /// </summary>
+        /// <returns>The current instant provided by the underlying clock.</returns>
+        public Instant GetCurrentInstant() => clock.GetCurrentInstant();
+
+        /// <summary>
+        /// Returns the current instant provided by the underlying clock, adjusted
+        /// to the time zone of this object.
+        /// </summary>
+        /// <returns>The current instant provided by the underlying clock, adjusted to the
+        /// time zone of this object.</returns>
+        [Pure]
+        public ZonedDateTime GetCurrentZonedDateTime() => GetCurrentInstant().InZone(zone, calendar);
+
+        /// <summary>
+        /// Returns the local date/time of the current instant provided by the underlying clock, adjusted
+        /// to the time zone of this object.
+        /// </summary>
+        /// <returns>The local date/time of the current instant provided by the underlying clock, adjusted to the
+        /// time zone of this object.</returns>
+        [Pure]
+        public LocalDateTime GetCurrentLocalDateTime() => GetCurrentZonedDateTime().LocalDateTime;
+
+        /// <summary>
+        /// Returns the offset date/time of the current instant provided by the underlying clock, adjusted
+        /// to the time zone of this object.
+        /// </summary>
+        /// <returns>The offset date/time of the current instant provided by the underlying clock, adjusted to the
+        /// time zone of this object.</returns>
+        [Pure]
+        public OffsetDateTime GetCurrentOffsetDateTime() => GetCurrentZonedDateTime().ToOffsetDateTime();
+
+        /// <summary>
+        /// Returns the local date of the current instant provided by the underlying clock, adjusted
+        /// to the time zone of this object.
+        /// </summary>
+        /// <returns>The local date of the current instant provided by the underlying clock, adjusted to the
+        /// time zone of this object.</returns>
+        [Pure]
+        public LocalDate GetCurrentDate() => GetCurrentZonedDateTime().Date;
+
+        /// <summary>
+        /// Returns the local time of the current instant provided by the underlying clock, adjusted
+        /// to the time zone of this object.
+        /// </summary>
+        /// <returns>The local time of the current instant provided by the underlying clock, adjusted to the
+        /// time zone of this object.</returns>
+        [Pure]
+        public LocalTime GetCurrentTimeOfDay() => GetCurrentZonedDateTime().TimeOfDay;
+    }
+}
