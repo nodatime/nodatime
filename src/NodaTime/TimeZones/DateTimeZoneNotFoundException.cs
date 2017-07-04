5702ea69a0460969dde2d1616cf26626378e70b1
Jonas Nyrup <jnyrup@users.noreply.github.com>
2017-07-04 13:42:01 +0200

    Remove reference to satan

--- src/NodaTime/TimeZones/DateTimeZoneNotFoundException.cs
+++ src/NodaTime/TimeZones/DateTimeZoneNotFoundException.cs
@@ -12,7 +12,7 @@ namespace NodaTime.TimeZones
     /// but the specified ID is invalid for that provider.
     /// </summary>
     /// <remarks>
-    /// This type only exists as <c>TimeZoneNotFoundException</c> doesn't exist in netsatandard1.x.
+    /// This type only exists as <c>TimeZoneNotFoundException</c> doesn't exist in netstandard1.x.
     /// By creating an exception which derives from <c>TimeZoneNotFoundException</c> on the desktop version
     /// and <c>Exception</c> on the .NET Standard 1.3 version, we achieve reasonable consistency while remaining
     /// backwardly compatible with Noda Time v1 (which was desktop-only, and threw <c>TimeZoneNotFoundException</c>).
