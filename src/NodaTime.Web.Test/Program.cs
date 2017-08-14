0a9bfe9cfb0636fba76c5816e640da2ca4a79132
Jon Skeet <skeet@pobox.com>
2017-08-14 10:44:30 +0100

    Add unit test project for NodaTime.Web

---
+++ src/NodaTime.Web.Test/Program.cs
@@ -0,0 +1,23 @@
+﻿using NUnit.Framework;
+using NUnitLite;
+using System.Reflection;
+
+namespace NodaTime.Web.Test
+{
+    class Program
+    {
+        public static int Main(string[] args)
+        {
+            return new AutoRun(typeof(Program).GetTypeInfo().Assembly).Execute(args);
+        }
+    }
+
+    // Fake test just so that there's something to test in the first commit.
+    public class DummyTest
+    {
+        [Test]
+        public void Dummy()
+        {
+        }
+    }
+}
