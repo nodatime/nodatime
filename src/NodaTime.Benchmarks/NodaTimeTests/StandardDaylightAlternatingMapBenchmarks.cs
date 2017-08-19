8a49a1e079957ad379047728253073605e3d8728
Jon Skeet <skeet@pobox.com>
2017-08-19 08:50:18 +0100

    Actually fix it this time.

--- src/NodaTime.Benchmarks/NodaTimeTests/StandardDaylightAlternatingMapBenchmarks.cs
+++ src/NodaTime.Benchmarks/NodaTimeTests/StandardDaylightAlternatingMapBenchmarks.cs
@@ -33,16 +33,10 @@ namespace NodaTime.Benchmarks.NodaTimeTests
         }
 
         [Benchmark]
-        public ZoneInterval GetZoneInterval_Winter()
-        {
-            SampleZone.GetZoneInterval(February1st);
-        }
+        public ZoneInterval GetZoneInterval_Winter() => SampleZone.GetZoneInterval(February1st);
 
         [Benchmark]
-        public ZoneInterval GetZoneInterval_Summer()
-        {
-            SampleZone.GetZoneInterval(July1st);
-        }
+        public ZoneInterval GetZoneInterval_Summer() => SampleZone.GetZoneInterval(July1st);
     }
 }
 #endif
