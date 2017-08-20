2cd6ecfa719cec96a8f50b31054af48f34af1883
Jon Skeet <skeet@pobox.com>
2017-08-20 15:48:49 +0100

    Reinstate code coverage using dotCover and codecov
    (Will modify github readme badge when we *have* some coverage.)

--- build/appveyor.sh
+++ build/appveyor.sh
@@ -3,17 +3,20 @@
 # Build script run by Appveyor. Might eventually be less
 # single-purpose, but let's get coverage going ASAP...
 
-cd $(dirname $0)/..
+set -e
+
+declare -r ROOT=$(realpath $(dirname $0)/..)
 
 dotnet --info
 
 dotnet build -c Release src/NodaTime-All.sln
 
-# TODO: Reinstate coverage
+# Run just the net451 tests under dotCover
+$ROOT/build/coverage.sh
 
-dotnet run -c Release -f net451 -p src/NodaTime.Test -- --where=cat!=Slow
-dotnet run -c Release -f netcoreapp1.0 -p src/NodaTime.Test -- --where=cat!=Slow
-dotnet run -c Release -f netcoreapp2.0 -p src/NodaTime.Test -- --where=cat!=Slow
+dotnet run --no-build -c Release -f netcoreapp1.0 -p src/NodaTime.Test -- --where=cat!=Slow
+dotnet run --no-build -c Release -f netcoreapp2.0 -p src/NodaTime.Test -- --where=cat!=Slow
 
 dotnet build src/NodaTime-Web.sln
 dotnet run -f netcoreapp1.0 -p src/NodaTime.Web.Test
+
--- build/travis.sh
+++ build/travis.sh
@@ -8,6 +8,6 @@ dotnet build -c Release src/NodaTime.Benchmarks -f netcoreapp1.1
 dotnet build -c Release src/NodaTime.Demo -f netcoreapp1.0
 dotnet run -c Release -p src/NodaTime.Test -f netcoreapp1.0 -- --where=cat!=Slow
 dotnet run -c Release -p src/NodaTime.Test -f netcoreapp2.0 -- --where=cat!=Slow
+
 dotnet build src/NodaTime.Web -f netcoreapp1.0
-dotnet build src/NodaTime.Web.Test -f netcoreapp1.0
 dotnet run -p src/NodaTime.Web.Test -f netcoreapp1.0
