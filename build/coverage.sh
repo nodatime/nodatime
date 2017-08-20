48a8e2f2639cc59387f3a0760655f580c97ea466
Jon Skeet <skeet@pobox.com>
2017-08-20 15:48:49 +0100

    Reinstate code coverage using dotCover and codecov
    (Will modify github readme badge when we *have* some coverage.)

--- build/coverage.sh
+++ build/coverage.sh
@@ -2,27 +2,32 @@
 
 set -e
 
-# Note: this script assumes everything has already been built
+# Run coverage tests and optionally create a report.
 
-# cd to repository root
-cd $(dirname $0)/..
+declare -r ROOT=$(realpath $(dirname $0)/..)
+declare -r DOTCOVER_VERSION=2017.1.20170613.162720 
+declare -r REPORTGENERATOR_VERSION=2.5.10
 
-nuget install -Verbosity quiet -OutputDirectory packages -Version 4.6.519 OpenCover
-nuget install -Verbosity quiet -OutputDirectory packages -Version 0.7.0 coveralls.net
-nuget install -Verbosity quiet -OutputDirectory packages -Version 2.4.5.0 ReportGenerator
+nuget.exe install -Verbosity quiet -OutputDirectory $ROOT/packages -Version $DOTCOVER_VERSION JetBrains.dotCover.CommandLineTools
 
-# Use the legacy JIT to avoid getting InvalidProgramException.
-export COMPLUS_useLegacyJit=1
-packages/OpenCover.4.6.519/tools/OpenCover.Console.exe \
-  -register:user \
-  -oldStyle \
-  -target:"c:\Program Files\dotnet\dotnet.exe" \
-  -targetargs:"run -f net451 -c Release -p src/NodaTime.Test/NodaTime.Test.csproj -- -where=cat!=Slow" \
-  -output:coverage.xml \
-  -filter:"+[NodaTime]NodaTime.*" \
-  -searchdirs:NodaTime/bin/Release/net451/win7-x64
+declare -r DOTCOVER=$ROOT/packages/JetBrains.dotCover.CommandLineTools.$DOTCOVER_VERSION/tools/dotCover.exe
 
-packages/ReportGenerator.2.4.5.0/tools/ReportGenerator.exe \
-  -reports:coverage.xml \
-  -targetdir:coverage \
-  -verbosity:Error
+rm -rf $ROOT/coverage
+mkdir $ROOT/coverage
+
+# Run just the net451 tests under dotCover
+(cd $ROOT/src/NodaTime.Test; 
+ $DOTCOVER cover coverageparams.xml /ReturnTargetExitCode)
+
+$DOTCOVER report -Source=$ROOT/coverage/NodaTime.dvcr -Output=$ROOT/coverage/coverage.xml -ReportType=DetailedXML ""
+
+if [[ $1 == "--report" ]]
+then
+  nuget.exe install -Verbosity quiet -OutputDirectory $ROOT/packages -Version $REPORTGENERATOR_VERSION ReportGenerator
+  declare -r REPORTGENERATOR=$ROOT/packages/ReportGenerator.$REPORTGENERATOR_VERSION/tools/ReportGenerator.exe
+  
+  $REPORTGENERATOR \
+   -reports:$ROOT/coverage/coverage.xml \
+   -targetdir:$ROOT/coverage/report \
+   -verbosity:Error
+fi
