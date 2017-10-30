fd1a1f52321cd0223fecb5b4f046920060134d30
Carlos.Schults <carlos@smartbiotecnologia.com.br>
2017-10-30 12:33:23 -0200

    Add first snippet to PeriodDemo class

--- src/NodaTime.Demo/PeriodDemo.cs
+++ src/NodaTime.Demo/PeriodDemo.cs
@@ -1,12 +1,18 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// Copyright 2010 The Noda Time Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+using NUnit.Framework;
 
 namespace NodaTime.Demo
 {
-    class PeriodDemo
+    public class PeriodDemo
     {
+        [Test]
+        public void ConstructionFromDays()
+        {
+            Period period = Snippet.For(Period.FromDays(3));
+            Assert.AreEqual(3, period.Days);
+        }
     }
 }
