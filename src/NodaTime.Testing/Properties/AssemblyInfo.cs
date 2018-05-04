a0767182df71a6b0c673dee2baf70e0c85e96ec7
Jon Skeet <skeet@pobox.com>
2018-05-04 10:57:44 +0100

    Update to TZDB 2018e for release 1.4.5

--- src/NodaTime.Testing/Properties/AssemblyInfo.cs
+++ src/NodaTime.Testing/Properties/AssemblyInfo.cs
@@ -38,5 +38,5 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.4")]
-[assembly: AssemblyFileVersion("1.4.4")]
-[assembly: AssemblyInformationalVersion("1.4.4")]
+[assembly: AssemblyFileVersion("1.4.5")]
+[assembly: AssemblyInformationalVersion("1.4.5")]
