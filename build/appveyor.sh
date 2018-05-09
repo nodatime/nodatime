55058fd048472e50d74b95e5b4747ae02dfa8c39
Jon Skeet <skeet@pobox.com>
2018-05-09 15:54:32 +0200

    Update CI scripts for new target frameworks

--- build/appveyor.sh
+++ build/appveyor.sh
@@ -12,12 +12,11 @@ dotnet --info
 
 dotnet build -c Release src/NodaTime-All.sln
 
-# Run just the net451 tests under dotCover
+# Run the tests under dotCover
 build/coverage.sh
 
-dotnet run --no-build -c Release -f netcoreapp1.0 -p src/NodaTime.Test -- --where=cat!=Slow
-dotnet run --no-build -c Release -f netcoreapp2.0 -p src/NodaTime.Test -- --where=cat!=Slow
+dotnet run --no-build -c Release -p src/NodaTime.Test -- --where=cat!=Slow
 
 dotnet build src/NodaTime-Web.sln
-dotnet run -f netcoreapp2.0 -p src/NodaTime.Web.Test
+dotnet run -p src/NodaTime.Web.Test
 
--- build/travis.sh
+++ build/travis.sh
@@ -2,11 +2,10 @@
 
 set -e
 
-dotnet build -c Release src/NodaTime -f netstandard1.3
-dotnet build -c Release src/NodaTime.Testing -f netstandard1.3
-dotnet build -c Release src/NodaTime.Benchmarks -f netcoreapp1.1
-dotnet run -c Release -p src/NodaTime.Test -f netcoreapp1.0 -- --where=cat!=Slow
-dotnet run -c Release -p src/NodaTime.Test -f netcoreapp2.0 -- --where=cat!=Slow
+dotnet build -c Release src/NodaTime
+dotnet build -c Release src/NodaTime.Testing
+dotnet build -c Release src/NodaTime.Benchmarks
+dotnet run -c Release -p src/NodaTime.Test -- --where=cat!=Slow
 
-dotnet build src/NodaTime.Web -f netcoreapp2.0
-dotnet run -p src/NodaTime.Web.Test -f netcoreapp2.0
+dotnet build src/NodaTime.Web
+dotnet run -p src/NodaTime.Web.Test
