71e93d43927559fcb27098a483748f1c3504799a
Jon Skeet <skeet@pobox.com>
2018-05-22 11:44:23 +0100

    Fix travis build (but not tests yet)

--- build/travis.sh
+++ build/travis.sh
@@ -4,7 +4,7 @@ set -e
 
 dotnet build -c Release src/NodaTime
 dotnet build -c Release src/NodaTime.Testing
-dotnet build -c Release src/NodaTime.Benchmarks
+dotnet build -c Release src/NodaTime.Benchmarks -f netcoreapp2.0
 dotnet run -c Release -p src/NodaTime.Test -- --where=cat!=Slow
 
 dotnet build src/NodaTime.Web
