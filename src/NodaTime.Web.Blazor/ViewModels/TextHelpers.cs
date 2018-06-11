a76ca84fa64b8108aa01442d411a728a1f887e00
Jon Skeet <skeet@pobox.com>
2018-06-11 17:08:37 +0200

    Highly experimental!
    Pushing this live so that I can demo it to the lovely crowd in
    Copenhagen.

---
+++ src/NodaTime.Web.Blazor/ViewModels/TextHelpers.cs
@@ -0,0 +1,43 @@
+﻿// Copyright 2018 The Noda Time Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+using NodaTime.Text;
+
+namespace NodaTime.Web.Blazor.ViewModels
+{
+    internal static class TextHelpers
+    {
+        internal static LocalDate? ParseDate(string text, out string error)
+        {
+            var parseResult = LocalDatePattern.Iso.Parse(text ?? "");
+            if (parseResult.Success)
+            {
+                error = null;
+                return parseResult.Value;
+            }
+            else
+            {
+                var exception = parseResult.Exception;
+                error = $"{exception.GetType().Name}: {exception.Message}";
+                return null;
+            }
+        }
+
+        internal static Period ParsePeriod(string text, out string error)
+        {
+            var parseResult = PeriodPattern.Roundtrip.Parse(text ?? "");
+            if (parseResult.Success)
+            {
+                error = null;
+                return parseResult.Value;
+            }
+            else
+            {
+                var exception = parseResult.Exception;
+                error = $"{exception.GetType().Name}: {exception.Message}";
+                return null;
+            }
+        }
+    }
+}
