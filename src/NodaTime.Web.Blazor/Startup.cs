30804ca75a7c901f1a7773267763f116039daddc
Jon Skeet <skeet@pobox.com>
2018-08-07 06:41:16 -0700

    Update Blazor to 0.5.1
    Also change the way it's configured from NodaTime.Web to make it easier to include/exclude.

---
+++ src/NodaTime.Web.Blazor/Startup.cs
@@ -0,0 +1,19 @@
+﻿// Copyright 2018 The Noda Time Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+using Microsoft.AspNetCore.Blazor.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace NodaTime.Web.Blazor
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        public void Configure(IBlazorApplicationBuilder app) =>
+            app.AddComponent<App>("app");
+    }
+}
