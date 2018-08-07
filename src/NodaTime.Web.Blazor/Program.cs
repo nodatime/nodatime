4ad2b40d2f096b2400efa2b87f068cf756a2b2a6
Jon Skeet <skeet@pobox.com>
2018-08-07 06:41:16 -0700

    Update Blazor to 0.5.1
    Also change the way it's configured from NodaTime.Web to make it easier to include/exclude.

--- src/NodaTime.Web.Blazor/Program.cs
+++ src/NodaTime.Web.Blazor/Program.cs
@@ -1,20 +1,19 @@
-﻿using Microsoft.AspNetCore.Blazor.Browser.Rendering;
-using Microsoft.AspNetCore.Blazor.Browser.Services;
-using Microsoft.Extensions.DependencyInjection;
-using System;
+﻿// Copyright 2018 The Noda Time Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+using Microsoft.AspNetCore.Blazor.Hosting;
 
 namespace NodaTime.Web.Blazor
 {
     public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            var serviceProvider = new BrowserServiceProvider(services =>
-            {
-                // Add any custom services here
-            });
-
-            new BrowserRenderer(serviceProvider).AddComponent<App>("app");
+            CreateHostBuilder(args).Build().Run();
         }
+
+        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
+            BlazorWebAssemblyHost.CreateDefaultBuilder().UseBlazorStartup<Startup>();
     }
 }
