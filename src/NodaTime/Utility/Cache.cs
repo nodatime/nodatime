b0fb782ae255b794962b98360e6baa784cc87b06
Jon Skeet <skeet@pobox.com>
2018-08-18 12:52:35 +0100

    Use ConcurrentDictionary for caching, instead of locking
    Fixes #1193.

--- src/NodaTime/Utility/Cache.cs
+++ src/NodaTime/Utility/Cache.cs
@@ -3,7 +3,9 @@
 // as found in the LICENSE.txt file.
 
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace NodaTime.Utility
 {
@@ -20,17 +22,18 @@ namespace NodaTime.Utility
     internal sealed class Cache<TKey, TValue>
     {
         private readonly int size;
-        private readonly object mutex = new object();
         private readonly Func<TKey, TValue> valueFactory;
-        private readonly LinkedList<TKey> keyList; 
-        private readonly Dictionary<TKey, TValue> dictionary; 
+        // List of keys in (rough) insertion order. Used for eviction ordering.
+        // The same key may appear in the list multiple times.
+        private readonly ConcurrentQueue<TKey> keyList; 
+        private readonly ConcurrentDictionary<TKey, TValue> dictionary; 
 
         internal Cache(int size, Func<TKey, TValue> valueFactory, IEqualityComparer<TKey> keyComparer)
         {
             this.size = size;
             this.valueFactory = valueFactory;
-            this.dictionary = new Dictionary<TKey,TValue>(keyComparer);
-            this.keyList = new LinkedList<TKey>();
+            this.dictionary = new ConcurrentDictionary<TKey,TValue>(keyComparer);
+            this.keyList = new ConcurrentQueue<TKey>();
         }
 
         /// <summary>
@@ -40,68 +43,46 @@ namespace NodaTime.Utility
         /// <returns>The value associated with the key.</returns>
         internal TValue GetOrAdd(TKey key)
         {
-            lock (mutex)
+            if (dictionary.TryGetValue(key, out var value))
             {
-                // First check the cache...
-                if (dictionary.TryGetValue(key, out TValue value))
-                {
-                    return value;
-                }
-
-                // Make space if necessary...
-                if (dictionary.Count == size)
-                {
-                    TKey firstKey = keyList.First.Value;
-                    keyList.RemoveFirst();
-                    dictionary.Remove(firstKey);
-                }
-
-                // Create and cache the new value
-                value = valueFactory(key);
-                keyList.AddLast(key);
-                dictionary[key] = value;
                 return value;
             }
+            // Add the key to the eviction queue is we're *probably* going to be adding it now.
+            // There's no easy way of telling whether a particular call to GetOrAdd has added the key.
+            keyList.Enqueue(key);
+            value = dictionary.GetOrAdd(key, valueFactory);
+
+            // Trim to size if necessary, bearing in mind that other threads may be doing
+            // the same thing at the same time, and that there may be multiple entries for a particular key.
+            while (dictionary.Count > size && keyList.TryDequeue(out var keyToRemove))
+            {
+                dictionary.TryRemove(keyToRemove, out _);
+            }
+            return value;
         }
 
         /// <summary>
         /// Returns the number of entries currently in the cache, primarily for diagnostic purposes.
         /// </summary>
-        internal int Count
-        { 
-            get 
-            {
-                lock (mutex)
-                {
-                    return dictionary.Count;
-                }
-            }
-        }
+        internal int Count => dictionary.Count;
 
         /// <summary>
         /// Returns a copy of the keys in the cache as a list, for diagnostic purposes.
         /// </summary>
-        internal List<TKey> Keys
-        {
-            get
-            {
-                lock (mutex)
-                {
-                    return new List<TKey>(keyList);
-                }
-            }
-        }
+        internal List<TKey> Keys => dictionary.ToArray().Select(pair => pair.Key).ToList();
 
         /// <summary>
-        /// Clears the cache.
+        /// Clears the cache. This is never surfaced publicly (directly or indirectly) - it's just
+        /// for testing.
         /// </summary>
         internal void Clear()
         {
-            lock (mutex)
+            // There's no Clear method on ConcurrentQueue, so we need to iterate over
+            // it. 
+            while (keyList.TryDequeue(out _))
             {
-                keyList.Clear();
-                dictionary.Clear();
             }
+            dictionary.Clear();
         }
     }
 }
