1e24f0c70dd5762101b92bd05cee89532eef5936
Jon Skeet <skeet@pobox.com>
2018-10-18 12:12:16 +0100

    Update to TZDB 2018f for release 1.4.6

--- src/NodaTime/Properties/AssemblyInfo.cs
+++ src/NodaTime/Properties/AssemblyInfo.cs
@@ -46,8 +46,8 @@ using System.Runtime.CompilerServices;
 // [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyVersion("1.4")]
-[assembly: AssemblyFileVersion("1.4.5")]
-[assembly: AssemblyInformationalVersion("1.4.5")]
+[assembly: AssemblyFileVersion("1.4.6")]
+[assembly: AssemblyInformationalVersion("1.4.6")]
 [assembly: CLSCompliant(true)]
 [assembly: NeutralResourcesLanguage("en")]
 
