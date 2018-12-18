d60e815f8bff6013a02d2268583191075e07a13d
Jon Skeet <skeet@pobox.com>
2018-12-18 07:59:49 +0000

    Fetch and install the .NET Core 3.0 SDK preview in Travis
    This should be a temporary measure until there's an apt package.
    It's a bit clunky, but appears to work.
    
    Additionally, move to Xenial so that the 2.2 SDK can be installed correctly.

--- build/travis.sh
+++ build/travis.sh
@@ -2,6 +2,18 @@
 
 set -e
 
+curl -o preview-sdk.tgz https://download.visualstudio.microsoft.com/download/pr/9f071c35-36b4-48c9-bcc2-b381ecb6cada/5be4784f19c28cb58f8c79219347201a/dotnet-sdk-3.0.100-preview-009812-linux-x64.tar.gz
+mkdir -p $HOME/dotnet && tar zxf preview-sdk.tgz -C $HOME/dotnet
+export DOTNET_ROOT=$HOME/dotnet 
+export PATH=$HOME/dotnet:$PATH
+# Make older SDKs and runtimes available
+for i in /usr/share/dotnet/sdk/*; do ln -s $i $DOTNET_ROOT/sdk; done
+for i in /usr/share/dotnet/shared/Microsoft.AspNetCore.All/*; do ln -s $i $DOTNET_ROOT/shared/Microsoft.AspNetCore.All; done
+for i in /usr/share/dotnet/shared/Microsoft.AspNetCore.App/*; do ln -s $i $DOTNET_ROOT/shared/Microsoft.AspNetCore.App; done
+for i in /usr/share/dotnet/shared/Microsoft.NETCore.App/*; do ln -s $i $DOTNET_ROOT/shared/Microsoft.NETCore.App; done
+
+dotnet --info
+
 dotnet build -c Release src/NodaTime
 dotnet build -c Release src/NodaTime.Testing
 dotnet build -c Release src/NodaTime.Benchmarks -f netcoreapp2.0
