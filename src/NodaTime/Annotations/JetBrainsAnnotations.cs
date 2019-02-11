cbc07ddf8ff0a14e0925aaad155fd7afe875258b
Jon Skeet <skeet@pobox.com>
2019-02-11 22:40:04 +0000

    Remove annotation attributes that are no longer used
    (And one rogue CanBeNull that was left over...)

--- src/NodaTime/Annotations/JetBrainsAnnotations.cs
+++ src/NodaTime/Annotations/JetBrainsAnnotations.cs
@@ -27,37 +27,6 @@ using System;
 namespace JetBrains.Annotations
 {
     /// <summary>
-    /// Indicates that the value of the marked element could be <c>null</c> sometimes,
-    /// so the check for <c>null</c> is necessary before its usage
-    /// </summary>
-    /// <example><code>
-    /// [CanBeNull] public object Test() { return null; }
-    /// public void UseTest() {
-    ///   var p = Test();
-    ///   var s = p.ToString(); // Warning: Possible 'System.NullReferenceException'
-    /// }
-    /// </code></example>
-    [AttributeUsage(
-      AttributeTargets.Method | AttributeTargets.Parameter |
-      AttributeTargets.Property | AttributeTargets.Delegate |
-      AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
-    internal sealed class CanBeNullAttribute : Attribute { }
-
-    /// <summary>
-    /// Indicates that the value of the marked element could never be <c>null</c>
-    /// </summary>
-    /// <example><code>
-    /// [NotNull] public object Foo() {
-    ///   return null; // Warning: Possible 'null' assignment
-    /// }
-    /// </code></example>
-    [AttributeUsage(
-      AttributeTargets.Method | AttributeTargets.Parameter |
-      AttributeTargets.Property | AttributeTargets.Delegate |
-      AttributeTargets.Field, AllowMultiple = false, Inherited = true)]
-    internal sealed class NotNullAttribute : Attribute { }
-
-    /// <summary>
     /// Indicates that the marked method builds string by format pattern and (optional) arguments.
     /// Parameter, which contains format string, should be given in constructor. The format string
     /// should be in <see cref="string.Format(IFormatProvider,string,object[])"/>-like form
@@ -144,10 +113,10 @@ namespace JetBrains.Annotations
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
     internal sealed class ContractAnnotationAttribute : Attribute
     {
-        public ContractAnnotationAttribute([NotNull] string contract)
+        public ContractAnnotationAttribute(string contract)
             : this(contract, false) { }
 
-        public ContractAnnotationAttribute([NotNull] string contract, bool forceFullStates)
+        public ContractAnnotationAttribute(string contract, bool forceFullStates)
         {
             Preconditions.CheckNotNull(contract, nameof(contract));
             Contract = contract;
@@ -157,130 +126,7 @@ namespace JetBrains.Annotations
         public string Contract { get; private set; }
         public bool ForceFullStates { get; private set; }
     }
-
-    /// <summary>
-    /// Indicates that marked element should be localized or not
-    /// </summary>
-    /// <example><code>
-    /// [LocalizationRequiredAttribute(true)]
-    /// public class Foo {
-    ///   private string str = "my string"; // Warning: Localizable string
-    /// }
-    /// </code></example>
-    [AttributeUsage(AttributeTargets.All, AllowMultiple = false, Inherited = true)]
-    internal sealed class LocalizationRequiredAttribute : Attribute
-    {
-        public LocalizationRequiredAttribute() : this(true) { }
-        public LocalizationRequiredAttribute(bool required)
-        {
-            Required = required;
-        }
-
-        public bool Required { get; private set; }
-    }
-
-    /// <summary>
-    /// Indicates that the marked symbol is used implicitly
-    /// (e.g. via reflection, in external library), so this symbol
-    /// will not be marked as unused (as well as by other usage inspections)
-    /// </summary>
-    [AttributeUsage(AttributeTargets.All, AllowMultiple = false, Inherited = true)]
-    internal sealed class UsedImplicitlyAttribute : Attribute
-    {
-        public UsedImplicitlyAttribute()
-            : this(ImplicitUseKindFlags.Default, ImplicitUseTargetFlags.Default) { }
-
-        public UsedImplicitlyAttribute(ImplicitUseKindFlags useKindFlags)
-            : this(useKindFlags, ImplicitUseTargetFlags.Default) { }
-
-        public UsedImplicitlyAttribute(ImplicitUseTargetFlags targetFlags)
-            : this(ImplicitUseKindFlags.Default, targetFlags) { }
-
-        public UsedImplicitlyAttribute(
-          ImplicitUseKindFlags useKindFlags, ImplicitUseTargetFlags targetFlags)
-        {
-            UseKindFlags = useKindFlags;
-            TargetFlags = targetFlags;
-        }
-
-        public ImplicitUseKindFlags UseKindFlags { get; private set; }
-        public ImplicitUseTargetFlags TargetFlags { get; private set; }
-    }
-
-    /// <summary>
-    /// Should be used on attributes and causes ReSharper
-    /// to not mark symbols marked with such attributes as unused
-    /// (as well as by other usage inspections)
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
-    internal sealed class MeansImplicitUseAttribute : Attribute
-    {
-        public MeansImplicitUseAttribute()
-            : this(ImplicitUseKindFlags.Default, ImplicitUseTargetFlags.Default) { }
-
-        public MeansImplicitUseAttribute(ImplicitUseKindFlags useKindFlags)
-            : this(useKindFlags, ImplicitUseTargetFlags.Default) { }
-
-        public MeansImplicitUseAttribute(ImplicitUseTargetFlags targetFlags)
-            : this(ImplicitUseKindFlags.Default, targetFlags) { }
-
-        public MeansImplicitUseAttribute(
-          ImplicitUseKindFlags useKindFlags, ImplicitUseTargetFlags targetFlags)
-        {
-            UseKindFlags = useKindFlags;
-            TargetFlags = targetFlags;
-        }
-
-        [UsedImplicitly]
-        public ImplicitUseKindFlags UseKindFlags { get; private set; }
-        [UsedImplicitly]
-        public ImplicitUseTargetFlags TargetFlags { get; private set; }
-    }
-
-    [Flags]
-    internal enum ImplicitUseKindFlags
-    {
-        Default = Access | Assign | InstantiatedWithFixedConstructorSignature,
-        /// <summary>Only entity marked with attribute considered used</summary>
-        Access = 1,
-        /// <summary>Indicates implicit assignment to a member</summary>
-        Assign = 2,
-        /// <summary>
-        /// Indicates implicit instantiation of a type with fixed constructor signature.
-        /// That means any unused constructor parameters won't be reported as such.
-        /// </summary>
-        InstantiatedWithFixedConstructorSignature = 4,
-        /// <summary>Indicates implicit instantiation of a type</summary>
-        InstantiatedNoFixedConstructorSignature = 8,
-    }
-
-    /// <summary>
-    /// Specify what is considered used implicitly
-    /// when marked with <see cref="MeansImplicitUseAttribute"/>
-    /// or <see cref="UsedImplicitlyAttribute"/>
-    /// </summary>
-    [Flags]
-    internal enum ImplicitUseTargetFlags
-    {
-        Default = Itself,
-        Itself = 1,
-        /// <summary>Members of entity marked with attribute are considered used</summary>
-        Members = 2,
-        /// <summary>Entity marked with attribute and all its members considered used</summary>
-        WithMembers = Itself | Members
-    }
-
-
-    /// <summary>
-    /// Tells code analysis engine if the parameter is completely handled
-    /// when the invoked method is on stack. If the parameter is a delegate,
-    /// indicates that delegate is executed while the method is executed.
-    /// If the parameter is an enumerable, indicates that it is enumerated
-    /// while the method is executed
-    /// </summary>
-    [AttributeUsage(AttributeTargets.Parameter, Inherited = true)]
-    internal sealed class InstantHandleAttribute : Attribute { }
-
+    
     /// <summary>
     /// Indicates that a method does not make any observable state changes.
     /// The same as <c>System.Diagnostics.Contracts.PureAttribute</c>
