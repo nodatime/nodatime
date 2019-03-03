1f412d0337e2212440bd9cfa15db9425219c7dd0
Jon Skeet <skeet@pobox.com>
2019-03-03 12:56:39 +0000

    Allow a zone and version to be specified in the tzvalidate controller

--- src/NodaTime.Web/Controllers/TzValidateController.cs
+++ src/NodaTime.Web/Controllers/TzValidateController.cs
@@ -4,20 +4,46 @@
 using Microsoft.AspNetCore.Mvc;
 using NodaTime.TimeZones;
 using NodaTime.TzValidate.NodaDump;
+using NodaTime.Web.Models;
 using System.IO;
+using System.Linq;
 using System.Net;
 
 namespace NodaTime.Web.Controllers
 {
     public class TzValidateController : Controller
     {
-        // This will do something soon :)
+        private readonly ITzdbRepository repository;
+
+        public TzValidateController(ITzdbRepository repository) =>
+            this.repository = repository;
+
         [Route("/tzvalidate/generate")]
-        public IActionResult Generate(int startYear = 1, int endYear = 2035)
+        public IActionResult Generate(int startYear = 1, int endYear = 2035, string zone = null, string version = null)
         {
+            if (startYear < 1 || endYear > 3000 || startYear > endYear)
+            {
+                return BadRequest("Invalid start/end year combination");
+            }
+
             var source = TzdbDateTimeZoneSource.Default;
+            if (version != null)
+            {
+                var release = repository.GetRelease($"tzdb{version}.nzd");
+                if (release == null)
+                {
+                    return BadRequest("Unknown version");
+                }
+                source = TzdbDateTimeZoneSource.FromStream(release.GetContent());
+            }
+
+            if (zone != null && !source.GetIds().Contains(zone))
+            {
+                return BadRequest("Unknown zone");
+            }
+
             var writer = new StringWriter();
-            var options = new Options { FromYear = startYear, ToYear = endYear };
+            var options = new Options { FromYear = startYear, ToYear = endYear, ZoneId = zone };
             var dumper = new ZoneDumper(source, options);
             dumper.Dump(writer);
 
