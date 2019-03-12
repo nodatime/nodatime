d798efa1d7501ee0724ac28bbab75307e35cbc4e
Jon Skeet <skeet@pobox.com>
2019-03-12 13:53:25 +0000

    Use X-Robot-Tag to avoid indexing certain areas

---
+++ src/NodaTime.Web/Helpers/AddHeaderAttribute.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace NodaTime.Helpers
+{
+    // As per https://docs.microsoft.com/en-us/aspnet/core/mvc/controllers/filters,
+    // with minor modifications.
+    public class AddHeaderAttribute : ResultFilterAttribute
+    {
+        private readonly string _name;
+        private readonly string _value;
+
+        public AddHeaderAttribute(string name, string value)
+        {
+            _name = name;
+            _value = value;
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            context.HttpContext.Response.Headers.Add(_name, _value);
+            base.OnResultExecuting(context);
+        }
+    }
+}
