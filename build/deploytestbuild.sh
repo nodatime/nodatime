97e2ed32fef52cbb1fd56db923cd25e0f2def830
Jon Skeet <skeet@pobox.com>
2019-03-16 12:48:07 +0000

    Fix test build to publish and deploy the same configuration

--- build/deploytestbuild.sh
+++ build/deploytestbuild.sh
@@ -2,9 +2,9 @@
 
 ROOT=$(git rev-parse --show-toplevel)
 (cd $ROOT/src/NodaTime.Web;
- dotnet publish;
- echo "Built at $(date -u -Iseconds)" > bin/Release/netcoreapp2.2/publish/wwwroot/build.txt;
- cp $ROOT/build/nodatime.org/Dockerfile bin/Release/netcoreapp2.2/publish;
+ dotnet publish -c Debug;
+ echo "Built at $(date -u -Iseconds)" > bin/Debug/netcoreapp2.2/publish/wwwroot/build.txt;
+ cp $ROOT/build/nodatime.org/Dockerfile bin/Debug/netcoreapp2.2/publish;
  gcloud.cmd builds submit \
    --config=$ROOT/build/nodatime.org/testcloudbuild.yaml \
-   $ROOT/src/NodaTime.Web/bin/Release/netcoreapp2.2/publish)
+   $ROOT/src/NodaTime.Web/bin/Debug/netcoreapp2.2/publish)
