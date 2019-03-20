e6da1f3c1319aea085276560e38582390d8993c2
Jon Skeet <skeet@pobox.com>
2019-03-20 10:56:18 +0000

    Update .NET Core SDK to 3.0 preview 3

--- build/travis.sh
+++ build/travis.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-curl -o preview-sdk.tgz https://download.visualstudio.microsoft.com/download/pr/efa6dde9-a5ee-4322-b13c-a2a02d3980f0/dad445eba341c1d806bae5c8afb47015/dotnet-sdk-3.0.100-preview-010184-linux-x64.tar.gz
+curl -o preview-sdk.tgz https://download.visualstudio.microsoft.com/download/pr/35c9c95a-535e-4f00-ace0-4e1686e33c6e/b9787e68747a7e8a2cf8cc530f4b2f88/dotnet-sdk-3.0.100-preview3-010431-linux-x64.tar.gz
 mkdir -p $HOME/dotnet && tar zxf preview-sdk.tgz -C $HOME/dotnet
 export DOTNET_ROOT=$HOME/dotnet 
 export PATH=$HOME/dotnet:$PATH
