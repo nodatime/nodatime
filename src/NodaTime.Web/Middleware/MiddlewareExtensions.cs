a55c5176c5a3dceec30a3f3c1433e2078ba2251c
Jon Skeet <skeet@pobox.com>
2019-04-03 16:41:32 +0100

    Cleaner, more useful logging

---
+++ src/NodaTime.Web/Middleware/MiddlewareExtensions.cs
@@ -0,0 +1,17 @@
+﻿// Copyright 2019 The Noda Time Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+using Microsoft.AspNetCore.Builder;
+
+namespace NodaTime.Web.Middleware
+{
+    /// <summary>
+    /// Extension methods to configure the middleware in this namespace.
+    /// </summary>
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseSingleLineResponseLogging(this IApplicationBuilder builder) =>
+            builder.UseMiddleware<SingleLineResponseLoggingMiddleware>();
+    }
+}
