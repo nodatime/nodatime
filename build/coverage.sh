9c56559008e85f15893205b2729d9e07b30f66ac
Jon Skeet <skeet@pobox.com>
2019-05-03 19:39:02 +0100

    Update dotCover to a version which works with the .NET Core SDK preview

--- build/coverage.sh
+++ build/coverage.sh
@@ -6,7 +6,7 @@ set -e
 
 declare -r ROOT=$(realpath $(dirname $0)/..)
 declare -r TEST=$ROOT/src/NodaTime.Test
-declare -r DOTCOVER_VERSION=2018.3.3
+declare -r DOTCOVER_VERSION=2019.1.1
 declare -r REPORTGENERATOR_VERSION=4.0.12
 
 nuget.exe install -Verbosity quiet -OutputDirectory $ROOT/packages -Version $DOTCOVER_VERSION JetBrains.dotCover.CommandLineTools
@@ -16,19 +16,9 @@ declare -r DOTCOVER=$ROOT/packages/JetBrains.dotCover.CommandLineTools.$DOTCOVER
 rm -rf $ROOT/coverage
 mkdir $ROOT/coverage
 
-# dotCover fails with the current .NET Core SDK preview,
-# so we make sure we've built with the preview, then use
-# a global.json file to pin to the stable SDK
-rm -f $TEST/global.json
-dotnet build -c Release $TEST
-cp $ROOT/build/global.json.old $TEST/global.json
-
 # Run the tests under dotCover
 (cd $TEST; 
- $DOTCOVER cover coverageparams.xml /ReturnTargetExitCode)
-
-# Clean up
-rm -f $TEST/global.json
+ $DOTCOVER cover coverageparams.xml -ReturnTargetExitCode)
 
 $DOTCOVER report -Source=$ROOT/coverage/NodaTime.dvcr -Output=$ROOT/coverage/coverage.xml -ReportType=DetailedXML ""
 
