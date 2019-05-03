fe5e591c941fe2fa6997b17939bf02ccbff524a4
Jon Skeet <skeet@pobox.com>
2019-05-03 18:45:06 +0100

    Fix demo tests

--- src/NodaTime.Demo/OffsetDateDemo.cs
+++ src/NodaTime.Demo/OffsetDateDemo.cs
@@ -1,4 +1,4 @@
-﻿// Copyright 2010 The Noda Time Authors. All rights reserved.
+﻿// Copyright 2019 The Noda Time Authors. All rights reserved.
 // Use of this source code is governed by the Apache License 2.0,
 // as found in the LICENSE.txt file.
 
@@ -28,7 +28,7 @@ namespace NodaTime.Demo
                 Offset.FromHours(3));
 
             OffsetDate updated = Snippet.For(original.WithOffset(Offset.FromHours(-3)));
-            Assert.AreEqual(original.Offset, updated.Offset);
+            Assert.AreEqual(Offset.FromHours(-3), updated.Offset);
             Assert.AreEqual(original.Date, updated.Date);
         }
 
@@ -39,10 +39,10 @@ namespace NodaTime.Demo
                 new LocalDate(2019, 5, 3),
                 Offset.FromHours(3));
 
-            Func<LocalDate, LocalDate> tomorrowfier = x => x.Plus(Period.FromDays(1));
-            OffsetDate updated = Snippet.For(original.With(tomorrowfier));
+            Func<LocalDate, LocalDate> tomorrowAdjuster = x => x + Period.FromDays(1);
+            OffsetDate updated = Snippet.For(original.With(tomorrowAdjuster));
             Assert.AreEqual(original.Offset, updated.Offset);
-            Assert.AreEqual(tomorrowfier(original.Date), updated.Date);
+            Assert.AreEqual(tomorrowAdjuster(original.Date), updated.Date);
         }
 
         [Test]
@@ -54,7 +54,6 @@ namespace NodaTime.Demo
 
             OffsetDate updated = Snippet.For(original.WithCalendar(CalendarSystem.Gregorian));
             Assert.AreEqual(original.Offset, updated.Offset);
-            Assert.AreEqual(original.Date, updated.Date);
             Assert.AreEqual(CalendarSystem.Gregorian, updated.Calendar);
         }
     }
