1efaba8d28bb99f25a103fb9f4e47fd3f4271db4
Carlos Schults <carlos.schults@gmail.com>
2019-05-04 01:00:26 -0300

    add snippets for the OffsetTime type

---
+++ src/NodaTime.Demo/OffsetTimeDemo.cs
@@ -0,0 +1,50 @@
+﻿// Copyright 2019 The Noda Time Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+using NUnit.Framework;
+
+namespace NodaTime.Demo
+{
+    public class OffsetTimeDemo
+    {
+        [Test]
+        public void Construction()
+        {
+            OffsetTime offsetTime = Snippet.For(new OffsetTime(
+                new LocalTime(15, 20, 48),
+                Offset.FromHours(3)));
+
+            Assert.AreEqual(new LocalTime(15, 20, 48), offsetTime.TimeOfDay);
+            Assert.AreEqual(Offset.FromHours(3), offsetTime.Offset);
+        }
+
+        [Test]
+        public void WithOffset()
+        {
+            OffsetTime original = new OffsetTime(
+                new LocalTime(15, 20, 48),
+                Offset.FromHours(3));
+
+            OffsetTime updated = Snippet.For(
+                original.WithOffset(Offset.FromHours(-3)));
+
+            Assert.AreEqual(original.TimeOfDay, updated.TimeOfDay);
+            Assert.AreEqual(Offset.FromHours(-3), updated.Offset);
+        }
+
+        [Test]
+        public void With()
+        {
+            OffsetTime original = new OffsetTime(
+                new LocalTime(15, 20, 48),
+                Offset.FromHours(3));
+
+            OffsetTime updated = Snippet.For(
+                original.With(x => x.PlusHours(5)));
+
+            Assert.AreEqual(new LocalTime(20, 20, 48), updated.TimeOfDay);
+            Assert.AreEqual(original.Offset, updated.Offset);
+        }
+    }
+}
