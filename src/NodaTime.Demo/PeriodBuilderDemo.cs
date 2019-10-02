e8f356af9e18b0125598e95868be37f080e064f6
Dean Becker <dean.becker@nexusinds.com>
2019-10-02 16:06:03 +0100

    * Updated copyright year * Added mutation to PeriodBuilder construction demo to highlight property persistence/flow * Neatened up instantiation with object initializer

--- src/NodaTime.Demo/PeriodBuilderDemo.cs
+++ src/NodaTime.Demo/PeriodBuilderDemo.cs
@@ -1,4 +1,4 @@
-// Copyright 2010 The Noda Time Authors. All rights reserved.
+// Copyright 2019 The Noda Time Authors. All rights reserved.
 // Use of this source code is governed by the Apache License 2.0,
 // as found in the LICENSE.txt file.
 
@@ -13,17 +13,21 @@ namespace NodaTime.Demo
         {
             Period existingPeriod = Period.FromYears(5);
             PeriodBuilder periodBuilder = Snippet.For(new PeriodBuilder(existingPeriod));
+            periodBuilder.Months = 6;
             Period period = periodBuilder.Build();
             Assert.AreEqual(5, period.Years);
+            Assert.AreEqual(6, period.Months);
         }
 
         [Test]
         public void Build()
         {
-            PeriodBuilder periodBuilder = new PeriodBuilder();
-            periodBuilder.Years = 2;
-            periodBuilder.Months = 3;
-            periodBuilder.Days = 4;
+            PeriodBuilder periodBuilder = new PeriodBuilder()
+            {
+                Years = 2,
+                Months = 3,
+                Days = 4
+            };
             Period period = Snippet.For(periodBuilder.Build());
             Assert.AreEqual(2, period.Years);
             Assert.AreEqual(3, period.Months);
