a03223fe6ce05f620cd1616eb2e12ba57f7cedba
Per Westerlund <per@fastmail.com>
2019-10-22 22:57:55 +0200

    Added snippets for Interval.Deconstruct(out Instant, out Instant), Interval.Duration and Interval.ToString()

--- src/NodaTime.Demo/IntervalDemo.cs
+++ src/NodaTime.Demo/IntervalDemo.cs
@@ -36,5 +36,36 @@ namespace NodaTime.Demo
             Interval interval = new Interval(start, end);
             Assert.True(Snippet.For(interval.Contains(Instant.FromUtc(2019, 1, 1, 15, 50, 50))));
         }
+
+        [Test]
+        public void Deconstruct()
+        {
+            Instant start = Instant.FromUtc(2019, 1, 2, 3, 10, 11);
+            Instant end = Instant.FromUtc(2020, 4, 5, 6, 12, 13);
+            Interval interval = new Interval(start, end);
+            Snippet.ForAction(() => interval.Deconstruct(out var start, out var end));
+            Assert.AreEqual(Instant.FromUtc(2019, 1, 2, 3, 10, 11), start);
+            Assert.AreEqual(Instant.FromUtc(2020, 4, 5, 6, 12, 13), end);
+        }
+
+        [Test]
+        public void DurationDemo()
+        {
+            Instant start = Instant.FromUtc(2019, 1, 1, 3, 10, 20);
+            Instant end = Instant.FromUtc(2019, 1, 1, 9, 10, 20);
+            Interval interval = new Interval(start, end);
+            var duration = Snippet.For(interval.Duration);
+            Assert.AreEqual(Duration.FromHours(6), duration);
+        }
+
+        [Test]
+        public void ToStringDemo()
+        {
+            Instant start = Instant.FromUtc(2019, 1, 2, 10, 11, 12);
+            Instant end = Instant.FromUtc(2020, 2, 3, 12, 13, 14);
+            Interval interval = new Interval(start, end);
+            var stringRepresentation = Snippet.For(interval.ToString());
+            Assert.AreEqual("2019-01-02T10:11:12Z/2020-02-03T12:13:14Z", stringRepresentation);
+        }
     }
 }
