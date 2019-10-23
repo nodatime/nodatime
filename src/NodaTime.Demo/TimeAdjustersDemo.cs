369c407efc559e023526718e5f1c2688c51f23ac
Per Westerlund <per@fastmail.com>
2019-10-23 19:13:43 +0200

    Added demo snippets for TimeAdjusters class

---
+++ src/NodaTime.Demo/TimeAdjustersDemo.cs
@@ -0,0 +1,35 @@
+﻿// Copyright 2019 The Noda Time Authors. All rights reserved.
+// Use of this source code is governed by the Apache License 2.0,
+// as found in the LICENSE.txt file.
+
+using NUnit.Framework;
+
+namespace NodaTime.Demo
+{
+    public class TimeAdjustersDemo
+    {
+        [Test]
+        public void TruncateToHour()
+        {
+            var time = LocalTime.FromMinutesSinceMidnight(63);
+            var truncated = Snippet.For(TimeAdjusters.TruncateToHour(time));
+            Assert.AreEqual(LocalTime.FromHoursSinceMidnight(1), truncated);
+        }
+
+        [Test]
+        public void TruncateToMinute()
+        {
+            var time = LocalTime.FromSecondsSinceMidnight(127);
+            var truncated = Snippet.For(TimeAdjusters.TruncateToMinute(time));
+            Assert.AreEqual(LocalTime.FromMinutesSinceMidnight(2), truncated);
+        }
+
+        [Test]
+        public void TruncateToSecond()
+        {
+            var time = LocalTime.FromMillisecondsSinceMidnight(3042);
+            var truncated = Snippet.For(TimeAdjusters.TruncateToSecond(time));
+            Assert.AreEqual(LocalTime.FromSecondsSinceMidnight(3), truncated);
+        }
+    }
+}
