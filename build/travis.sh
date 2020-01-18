fb56d8ae7812095d99d9e1a99b5739e962bc37b1
Jon Skeet <skeet@pobox.com>
2020-01-18 15:07:06 +0000

    Change how we run "all but the slow tests" to use runsettings

--- build/travis.sh
+++ build/travis.sh
@@ -17,7 +17,7 @@ dotnet --info
 dotnet build -c Release src/NodaTime
 dotnet build -c Release src/NodaTime.Testing
 dotnet build -c Release src/NodaTime.Benchmarks -f netcoreapp3.0
-dotnet test -c Release src/NodaTime.Test --filter=TestCategory!=Slow
+dotnet test -c Release src/NodaTime.Test -s src/NodaTime.Test/NoSlowTests.runsettings
 dotnet test -c Release src/NodaTime.Demo
 
 dotnet build -c Release src/NodaTime.TzdbCompiler
