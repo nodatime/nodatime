81db84e16870188b94b644c52d7de1ae926d6e2a
Jon Skeet <skeet@pobox.com>
2020-03-22 18:03:21 +0000

    Fix Travis: update everything to .NET Core 3.1

--- build/travis.sh
+++ build/travis.sh
@@ -2,21 +2,9 @@
 
 set -e
 
-curl -o preview-sdk.tgz https://download.visualstudio.microsoft.com/download/pr/498b8b41-7626-435e-bea8-878c39ccbbf3/c8df08e881d1bcf9a49a9ff5367090cc/dotnet-sdk-3.0.100-preview9-014004-linux-x64.tar.gz
-mkdir -p $HOME/dotnet && tar zxf preview-sdk.tgz -C $HOME/dotnet
-export DOTNET_ROOT=$HOME/dotnet 
-export PATH=$HOME/dotnet:$PATH
-# Make older SDKs and runtimes available
-for i in /usr/share/dotnet/sdk/*; do ln -s $i $DOTNET_ROOT/sdk; done
-for i in /usr/share/dotnet/shared/Microsoft.AspNetCore.All/*; do ln -s $i $DOTNET_ROOT/shared/Microsoft.AspNetCore.All; done
-for i in /usr/share/dotnet/shared/Microsoft.AspNetCore.App/*; do ln -s $i $DOTNET_ROOT/shared/Microsoft.AspNetCore.App; done
-for i in /usr/share/dotnet/shared/Microsoft.NETCore.App/*; do ln -s $i $DOTNET_ROOT/shared/Microsoft.NETCore.App; done
-
 dotnet --info
 
-dotnet build -c Release src/NodaTime
-dotnet build -c Release src/NodaTime.Testing
-dotnet build -c Release src/NodaTime.Benchmarks -f netcoreapp3.0
+dotnet build -c Release src/NodaTime.sln
 dotnet test -c Release src/NodaTime.Test -s src/NodaTime.Test/NoSlowTests.runsettings
 dotnet test -c Release src/NodaTime.Demo
 
