ac61432fb4d6316632a1f2fba908f5c3c0a687a6
Cédric Luthi <cedric.luthi@gmail.com>
2020-04-07 17:09:40 +0200

    Enable .NET Framework 4.8 on all unit test projects
    Also enable mono testing on Travis CI

--- build/travis.sh
+++ build/travis.sh
@@ -3,10 +3,9 @@
 set -e
 
 dotnet --info
+mono --version || true
 
 dotnet build -c Release src/NodaTime.sln
-dotnet test -c Release src/NodaTime.Test -s src/NodaTime.Test/NoSlowTests.runsettings
-dotnet test -c Release src/NodaTime.Demo
-
-dotnet build -c Release src/NodaTime.TzdbCompiler
-dotnet test -c Release src/NodaTime.TzdbCompiler.Test
+dotnet test -c Release -f "${TARGET_FRAMEWORK}" src/NodaTime.Test -s src/NodaTime.Test/NoSlowTests.runsettings
+dotnet test -c Release -f "${TARGET_FRAMEWORK}" src/NodaTime.Demo
+dotnet test -c Release -f "${TARGET_FRAMEWORK}" src/NodaTime.TzdbCompiler.Test
