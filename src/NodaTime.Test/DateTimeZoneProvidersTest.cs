9c9b611c07a4d90c06608d37de02fe6b1eac213e
Jon Skeet <skeet@pobox.com>
2020-04-19 09:10:52 +0100

    Add test for delegation in obsolete property
    (This keeps the coverage of DateTimeZoneProviders at 100% :)

--- src/NodaTime.Test/DateTimeZoneProvidersTest.cs
+++ src/NodaTime.Test/DateTimeZoneProvidersTest.cs
@@ -3,6 +3,8 @@
 // as found in the LICENSE.txt file.
 
 using System.Linq;
+using NodaTime.Testing.TimeZones;
+using NodaTime.Xml;
 using NUnit.Framework;
 
 namespace NodaTime.Test
@@ -30,6 +32,29 @@ namespace NodaTime.Test
         public void BclProviderUsesTimeZoneInfoSource()
         {
             Assert.IsTrue(DateTimeZoneProviders.Bcl.VersionId.StartsWith("TimeZoneInfo: "));
+        }
+
+        [Test]
+        public void SerializationDelegatesToXmlSerializerSettings()
+        {
+            var original = XmlSerializationSettings.DateTimeZoneProvider;
+
+            try
+            {
+#pragma warning disable CS0618 // Type or member is obsolete
+                var provider1 = new FakeDateTimeZoneSource.Builder().Build().ToProvider();
+                DateTimeZoneProviders.Serialization = provider1;
+                Assert.AreSame(provider1, XmlSerializationSettings.DateTimeZoneProvider);
+
+                var provider2 = new FakeDateTimeZoneSource.Builder().Build().ToProvider();
+                XmlSerializationSettings.DateTimeZoneProvider = provider2;
+                Assert.AreSame(provider2, DateTimeZoneProviders.Serialization);
+#pragma warning restore CS0618 // Type or member is obsolete
+            }
+            finally
+            {
+                XmlSerializationSettings.DateTimeZoneProvider = original;
+            }
         }
     }
 }
