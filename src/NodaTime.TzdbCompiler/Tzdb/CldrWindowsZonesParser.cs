87603eb9173c8042ad7a6d95ed8a6278cb90bc80
Jon Skeet <skeet@pobox.com>
2021-03-13 07:15:49 +0000

    Allow the TZDB compiler to run under .NET 5.0
    .NET 5.0 has more instrumentation for null references in LINQ to
    XML, so the CLDR converter needed changes.

--- src/NodaTime.TzdbCompiler/Tzdb/CldrWindowsZonesParser.cs
+++ src/NodaTime.TzdbCompiler/Tzdb/CldrWindowsZonesParser.cs
@@ -3,6 +3,7 @@
 // as found in the LICENSE.txt file.
 
 using NodaTime.TimeZones.Cldr;
+using NodaTime.Utility;
 using System;
 using System.Collections.Generic;
 using System.IO;
@@ -19,10 +20,15 @@ namespace NodaTime.TzdbCompiler.Tzdb
     {
         internal static WindowsZones Parse(XDocument document)
         {
-            var mapZones = MapZones(document);
-            var windowsZonesVersion = FindVersion(document);
-            var tzdbVersion = document.Root.Element("windowsZones")?.Element("mapTimezones")?.Attribute("typeVersion")?.Value ?? "";
-            var windowsVersion = document.Root.Element("windowsZones")?.Element("mapTimezones")?.Attribute("otherVersion")?.Value ?? "";
+            var root = document.Root;
+            if (root is null)
+            {
+                throw new ArgumentException("XML document has no root element");
+            }
+            var mapZones = MapZones(root);
+            var windowsZonesVersion = FindVersion(root!);
+            var tzdbVersion = root.Element("windowsZones")?.Element("mapTimezones")?.Attribute("typeVersion")?.Value ?? "";
+            var windowsVersion = root.Element("windowsZones")?.Element("mapTimezones")?.Attribute("otherVersion")?.Value ?? "";
             return new WindowsZones(windowsZonesVersion, tzdbVersion, windowsVersion, mapZones);
         }
 
@@ -39,9 +45,9 @@ namespace NodaTime.TzdbCompiler.Tzdb
             }
         }
 
-        private static string FindVersion(XDocument document)
+        private static string FindVersion(XElement root)
         {
-            string revision = (string)document.Root.Element("version")?.Attribute("number");
+            string? revision = (string?) root.Element("version")?.Attribute("number");
             if (revision is null)
             {
                 return "";
@@ -63,14 +69,23 @@ namespace NodaTime.TzdbCompiler.Tzdb
         /// Reads the input XML file for the windows mappings.
         /// </summary>
         /// <returns>A lookup of Windows time zone mappings</returns>
-        private static IList<MapZone> MapZones(XDocument document) =>
-            document.Root
-                .Element("windowsZones")
-                .Element("mapTimezones")
-                .Elements("mapZone")
-                .Select(x => new MapZone(x.Attribute("other").Value,
-                                         x.Attribute("territory").Value,
-                                         x.Attribute("type").Value.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)))
-                .ToList();
+        private static IList<MapZone> MapZones(XElement root)
+        {
+            var elements = root.Element("windowsZones")?.Element("mapTimezones")?.Elements("mapZone");
+            if (elements is null)
+            {
+                throw new ArgumentException("Missing Windows time zone XML elements");
+            }
+            return elements.Select(CreateMapZone).ToList();
+
+            static MapZone CreateMapZone(XElement element)
+            {
+                var other = element.Attribute("other")?.Value ?? throw new ArgumentException("Missing 'other' attribute");
+                var territory = element.Attribute("territory")?.Value ?? throw new ArgumentException("Missing 'territory' attribute");
+                var type = element.Attribute("type")?.Value ?? throw new ArgumentException("Missing 'type' attribute");
+
+                return new MapZone(other, territory, type.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+            }
+        }
     }
 }
